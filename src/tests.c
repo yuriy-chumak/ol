5d32b10bfe528e2245c74f24ae3465c13a43c258
Yuriy Chumak <yuriy.chumak@mail.com>
2014-09-06 19:14:33 +0300

    [-] Практически полностью избавился он нейтивного win32 кода для примера opengl - перенес на olw

--- src/tests.c
+++ src/tests.c
@@ -22,16 +22,6 @@
 #include <gl\glu.h>			// Header File For The GLu32 Library
 //#include <gl\glaux.h>		// Header File For The Glaux Library
 
-HDC			hDC=NULL;		// Private GDI Device Context
-HGLRC		hRC=NULL;		// Permanent Rendering Context
-HWND		hWnd=NULL;		// Holds Our Window Handle
-
-char	keys[256];			// Array Used For The Keyboard Routine
-char	active=TRUE;		// Window Active Flag Set To TRUE By Default
-char	fullscreen=FALSE;	// Fullscreen Flag Set To Fullscreen Mode By Default
-
-//LRESULT	CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);	// Declaration For WndProc
-
 GLvoid ReSizeGLScene(GLsizei width, GLsizei height)		// Resize And Initialize The GL Window
 {
 	if (height==0)										// Prevent A Divide By Zero By
@@ -62,46 +52,6 @@ int InitGL(GLvoid)										// All Setup For OpenGL Goes Here
 	return TRUE;										// Initialization Went OK
 }
 
-__declspec(dllexport)
-int DrawGLScene(GLvoid)									// Here's Where We Do All The Drawing
-{
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Clear Screen And Depth Buffer
-	glLoadIdentity();									// Reset The Current Modelview Matrix
-
-	SwapBuffers(hDC);				// Swap Buffers (Double Buffering)
-	return TRUE;										// Everything Went OK
-}
-
-__declspec(dllexport)
-GLvoid KillGLWindow(GLvoid)								// Properly Kill The Window
-{
-	if (hRC)											// Do We Have A Rendering Context?
-	{
-		if (!wglMakeCurrent(NULL,NULL))					// Are We Able To Release The DC And RC Contexts?
-		{
-			MessageBox(NULL,"Release Of DC And RC Failed.","SHUTDOWN ERROR",MB_OK | MB_ICONINFORMATION);
-		}
-
-		if (!wglDeleteContext(hRC))						// Are We Able To Delete The RC?
-		{
-			MessageBox(NULL,"Release Rendering Context Failed.","SHUTDOWN ERROR",MB_OK | MB_ICONINFORMATION);
-		}
-		hRC=NULL;										// Set RC To NULL
-	}
-
-	if (hDC && !ReleaseDC(hWnd,hDC))					// Are We Able To Release The DC
-	{
-		MessageBox(NULL,"Release Device Context Failed.","SHUTDOWN ERROR",MB_OK | MB_ICONINFORMATION);
-		hDC=NULL;										// Set DC To NULL
-	}
-
-	if (hWnd && !DestroyWindow(hWnd))					// Are We Able To Destroy The Window?
-	{
-		MessageBox(NULL,"Could Not Release hWnd.","SHUTDOWN ERROR",MB_OK | MB_ICONINFORMATION);
-		hWnd=NULL;										// Set hWnd To NULL
-	}
-}
-
 /*	This Code Creates Our OpenGL Window.  Parameters Are:					*
  *	title			- Title To Appear At The Top Of The Window				*
  *	width			- Width Of The GL Window Or Fullscreen Mode				*
@@ -109,88 +59,17 @@ GLvoid KillGLWindow(GLvoid)								// Properly Kill The Window
  *	bits			- Number Of Bits To Use For Color (8/16/24/32)			*
  *	fullscreenflag	- Use Fullscreen Mode (TRUE) Or Windowed Mode (FALSE)	*/
 __declspec(dllexport)
-BOOL CreateGLWindow(HANDLE somehandle, int bits, char fullscreenflag)
+BOOL CreateGLWindow(HANDLE somehandle, int bits, char fullscreenflag, int dc, int rc)
 {
-	GLuint		PixelFormat;			// Holds The Results After Searching For A Match
-	DWORD		dwExStyle;				// Window Extended Style
-	DWORD		dwStyle;				// Window Style
 	RECT		WindowRect;				// Grabs Rectangle Upper Left / Lower Right Values
-	fullscreen=fullscreenflag;			// Set The Global Fullscreen Flag
 
-	hWnd = somehandle;
-	GetWindowRect(hWnd, &WindowRect);
+	GetWindowRect(somehandle, &WindowRect);
 	int width = WindowRect.right - WindowRect.left;
 	int height = WindowRect.bottom - WindowRect.top;
 
-	static	PIXELFORMATDESCRIPTOR pfd=				// pfd Tells Windows How We Want Things To Be
-	{
-		sizeof(PIXELFORMATDESCRIPTOR),				// Size Of This Pixel Format Descriptor
-		1,											// Version Number
-		PFD_DRAW_TO_WINDOW |						// Format Must Support Window
-		PFD_SUPPORT_OPENGL |						// Format Must Support OpenGL
-		PFD_DOUBLEBUFFER,							// Must Support Double Buffering
-		PFD_TYPE_RGBA,								// Request An RGBA Format
-		0,											// Select Our Color Depth
-		0, 0, 0, 0, 0, 0,							// Color Bits Ignored
-		0,											// No Alpha Buffer
-		0,											// Shift Bit Ignored
-		0,											// No Accumulation Buffer
-		0, 0, 0, 0,									// Accumulation Bits Ignored
-		16,											// 16Bit Z-Buffer (Depth Buffer)
-		0,											// No Stencil Buffer
-		0,											// No Auxiliary Buffer
-		PFD_MAIN_PLANE,								// Main Drawing Layer
-		0,											// Reserved
-		0, 0, 0										// Layer Masks Ignored
-	};
-	pfd.cColorBits = bits;
-
-	if (!(hDC=GetDC(hWnd)))							// Did We Get A Device Context?
-	{
-		KillGLWindow();								// Reset The Display
-		MessageBox(NULL,"Can't Create A GL Device Context.","ERROR",MB_OK|MB_ICONEXCLAMATION);
-		return FALSE;								// Return FALSE
-	}
-
-	if (!(PixelFormat=ChoosePixelFormat(hDC,&pfd)))	// Did Windows Find A Matching Pixel Format?
-	{
-		KillGLWindow();								// Reset The Display
-		MessageBox(NULL,"Can't Find A Suitable PixelFormat.","ERROR",MB_OK|MB_ICONEXCLAMATION);
-		return FALSE;								// Return FALSE
-	}
-
-	if(!SetPixelFormat(hDC,PixelFormat,&pfd))		// Are We Able To Set The Pixel Format?
-	{
-		KillGLWindow();								// Reset The Display
-		MessageBox(NULL,"Can't Set The PixelFormat.","ERROR",MB_OK|MB_ICONEXCLAMATION);
-		return FALSE;								// Return FALSE
-	}
-
-	if (!(hRC=wglCreateContext(hDC)))				// Are We Able To Get A Rendering Context?
-	{
-		KillGLWindow();								// Reset The Display
-		MessageBox(NULL,"Can't Create A GL Rendering Context.","ERROR",MB_OK|MB_ICONEXCLAMATION);
-		return FALSE;								// Return FALSE
-	}
-
-	if(!wglMakeCurrent(hDC,hRC))					// Try To Activate The Rendering Context
-	{
-		KillGLWindow();								// Reset The Display
-		MessageBox(NULL,"Can't Activate The GL Rendering Context.","ERROR",MB_OK|MB_ICONEXCLAMATION);
-		return FALSE;								// Return FALSE
-	}
-
-	ShowWindow(hWnd,SW_SHOW);						// Show The Window
-	SetForegroundWindow(hWnd);						// Slightly Higher Priority
-	SetFocus(hWnd);									// Sets Keyboard Focus To The Window
 	ReSizeGLScene(width, height);					// Set Up Our Perspective GL Screen
 
-	if (!InitGL())									// Initialize Our Newly Created GL Window
-	{
-		KillGLWindow();								// Reset The Display
-		MessageBox(NULL,"Initialization Failed.","ERROR",MB_OK|MB_ICONEXCLAMATION);
-		return FALSE;								// Return FALSE
-	}
+	InitGL();										// Initialize Our Newly Created GL Window
 
 	return TRUE;									// Success
 }
--- tests/opengl.scm
+++ tests/opengl.scm
@@ -26,7 +26,7 @@
 ; my temporary stubs for opengl (у меня пока ж нет структур и т.д.)
 (define kernel32_dll (load-library "kernel32" 0))
   (define GetModuleHandle (get-proc-address type-handle kernel32_dll "GetModuleHandleA"))
-  
+
 (define _exe (GetModuleHandle 0))
 ;(define wmain          (get-proc-address type-fix+ _exe "wmain@12")) ; test
 ;(wmain 1 2 3)                                                        ; test
@@ -34,10 +34,7 @@
 ;(cmain 1 2 3)                                                        ; test
 
 
-;(define WinMain        (get-proc-address type-fix+ _exe "WinMain@16"))
 (define CreateGLWindow (get-proc-address-c type-fix+ _exe "CreateGLWindow"))
-(define KillGLWindow   (get-proc-address-c type-fix+ _exe "KillGLWindow"))
-(define DrawGLScene    (get-proc-address-c type-fix+ _exe "DrawGLScene"))
 
          
 ; real code
@@ -67,8 +64,33 @@
     (define WS_OVERLAPPEDWINDOW  (OR #x00000000 #x00C00000 #x00080000 #x00040000 #x00020000 #x00010000))
     (define WS_CLIPSIBLINGS      #x04000000)
     (define WS_CLIPCHILDREN      #x02000000)
+  (define DestroyWindow    (get-proc-address type-fix+   user32 "DestroyWindow"))
+    
+  (define GetDC               (get-proc-address type-handle user32 "GetDC"))
+  (define ReleaseDC           (get-proc-address type-fix+   user32 "ReleaseDC"))
+  (define ShowWindow          (get-proc-address type-fix+   user32 "ShowWindow"))
+    (define SW_SHOW 5)
+  (define SetForegroundWindow (get-proc-address type-fix+   user32 "SetForegroundWindow"))
+  (define SetFocus            (get-proc-address type-fix+   user32 "SetFocus"))
   
   
+  
+(define gdi32 (load-library "gdi32" 0))
+  (define ChoosePixelFormat (get-proc-address type-fix+ gdi32 "ChoosePixelFormat"))
+  (define SetPixelFormat    (get-proc-address type-fix+ gdi32 "SetPixelFormat"))
+  (define SwapBuffers       (get-proc-address type-fix+ gdi32 "SwapBuffers"))
+
+
+(define opengl32 (load-library "opengl32" 0))
+  (define wglCreateContext  (get-proc-address type-handle opengl32 "wglCreateContext"))
+  (define wglMakeCurrent    (get-proc-address type-fix+   opengl32 "wglMakeCurrent"))
+  (define wglDeleteContext  (get-proc-address type-fix+   opengl32 "wglDeleteContext"))
+  
+  (define glClear           (get-proc-address type-fix+ opengl32 "glClear"))
+    (define GL_COLOR_BUFFER_BIT #x00004000)
+    (define GL_DEPTH_BUFFER_BIT #x00000100)
+  (define glLoadIdentity    (get-proc-address type-fix+ opengl32 "glLoadIdentity"))
+
 
 ; проверка, что все запустилось.
 
@@ -94,22 +116,43 @@
     0 ; instance
     0)) ; don't pass anything to WM_CREATE
     
-(CreateGLWindow window 16 0)
+; PIXELFORMATDESCRIPTOR
+(define pfd (list->vector '(#x28 00  1  00  #x25 00 00 00 00 #x10 00 00 00 00 00 00
+                                              00 00 00 00 00 00 00 #x10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00)))                        
+(define hDC (GetDC window))
+(define PixelFormat (ChoosePixelFormat hDC pfd))
+(SetPixelFormat hDC PixelFormat pfd)
+(define hRC (wglCreateContext hDC))
+
+(wglMakeCurrent hDC hRC)
+(ShowWindow window SW_SHOW)
+(SetForegroundWindow window)
+(SetFocus window)
+
+(CreateGLWindow window 16 0 hDC hRC)
+
 ;(WinMain 0 0 0 0)
 
 (define MSG (make-vector 28 0)) ; sizeof(MSG)=28
-(call/cc (lambda (return)
-  (define (cycle)   ;MSG
-    (if (not (= (GetAsyncKeyState 27) 0)) (return))
-    
-    (if (= 1 (PeekMessage MSG 0 0 0 PM_REMOVE))
-      (begin  
-        (TranslateMessage MSG)
-        (DispatchMessage MSG))
-      (DrawGLScene))
-    (cycle))
-  (cycle)))
+;(call/cc (lambda (return)
+(define (cycle)   ;MSG
+  (if (= 1 (PeekMessage MSG 0 0 0 PM_REMOVE))
+    (begin  
+      (TranslateMessage MSG)
+      (DispatchMessage MSG))
+      
+    (begin ; DrawGLScene
+      (glClear GL_COLOR_BUFFER_BIT)
+      (glLoadIdentity)
+      (SwapBuffers hDC)))
+  (if (= (GetAsyncKeyState 27) 0) (cycle)))
+(cycle)
 
-(KillGLWindow)
+; KillGLWindow
+(wglMakeCurrent 0 0)
+(wglDeleteContext hRC)
+(ReleaseDC window hDC)
+(DestroyWindow window)
+
 (print "@")
 
