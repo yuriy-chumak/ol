a243ad1bd118bcab6bf6fcfbdf326c0d6a4e3c58
Yuriy Chumak <yuriy.chumak@mail.com>
2014-12-17 17:37:42 +0200

    [*] Переделал сопрограмму главного opengl рисовального цикла под концевую рекурсию. Круто, теперь бегает как надо без выгребания всего стека.

--- lib/opengl.scm
+++ lib/opengl.scm
@@ -25,119 +25,132 @@
    (list->byte-vector (repeat elem n)))
 ;(define ?? (lambda (function arguments) (if function (apply function arguments) arguments)))
 
+
 ; главный цикл оконной работы:
 ;(define (main-loop)
 (fork-server 'opengl (lambda ()
+; обработчики
+(define (tuple-processor this  sender message  window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard)
+   (tuple-case message
+      ; задать окно для рендеринга, проинициализировать в нем opengl контекст 
+      ((set-main-window new-window) ; создать окно
+         ; если у нас уже есть окно - удалим его и освободим ресурсы
+         (if window (begin
+            (wglMakeCurrent hDC 0)
+            (if hRC
+               (wglDeleteContext hRC))
+            (if hDC
+               (ReleaseDC window hDC))))
+         (if new-window
+            (let* ((window new-window)
+                   (pfd (list->byte-vector '(#x28 00  1 00  #x25 00 00 00 ; size, version, flags
+                                                            00 #x10 00 00 00 00 00 00 00 00 ; pixelType, colorBits, r,g,b,a bits+shift
+                                                            00 00 00 00 00 #x10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00))) ; accum, depth, etc.
+                   (hDC (GetDC window)))
+            (let* ((PixelFormat (ChoosePixelFormat hDC pfd)))
+               (print "Pixel Format: " PixelFormat)
+               (print "    options: " (if (> (bor (ref pfd 4) 4) 0) "PFD_DRAW_TO_WINDOW " "")
+                                      (if (> (bor (ref pfd 4) 1) 0) "PFD_DOUBLEBUFFER " "")
+                                      (if (> (bor (ref pfd 4) 32) 0) "PFD_SUPPORT_OPENGL " ""))
+               (print "    color bits: " (refb pfd 9))
+               (print "    depth bits: " (refb pfd 25))
+               (print "SetPixelFormat: " (if (=
+               (SetPixelFormat hDC PixelFormat pfd) 1) "Ok" "Failed")))
+            (let* ((hRC (wglCreateContext hDC)))
+               (print "Make GL context Current: " (if (=
+                  (wglMakeCurrent hDC hRC) 1) "Ok" "Failed"))
+               (print)
+               (print "OpenGL version: " (glGetString GL_VERSION))
+               (print "OpenGL vendor: " (glGetString GL_VENDOR))
+               (print "OpenGL renderer: " (glGetString GL_RENDERER))
+            
+               (ShowWindow window SW_SHOW)
+               (SetForegroundWindow window)
+               (SetFocus window)
+
+               (mail sender window)
+               (this window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard)))
+            ; else
+            (begin
+               (mail sender 'ok)
+               (this #false #f #f  ss ms  userdata  renderer renderer-args  keyboard))))
+            
+      ; простые сеттеры и геттеры:
+      ; задать функцию рендерера, swap-buffers будет происходить автоматически
+      ((register-renderer renderer renderer-args)
+         (this window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard))
+      ; set user data
+      ((set-userdata userdata)
+         (this window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard))
+      ((set-keyboard keyboard)
+         (this window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard))
+      ; error on invalid command
+      (else
+         (print "Unknown opengl server request: " message)
+         (this window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard))))
+
+
+(define (msg-processor this MSG  window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard)
+; тут можно обработать сообщения к окну, если надо.
+; Например, такое:
+   (begin ; атомарный обработчик-диспетчер
+      (TranslateMessage MSG)
+      (DispatchMessage MSG))
+
+   (case (+ (refb MSG 4) (* (refb MSG 5) 256))
+      (WM_LBUTTONDOWN ;WM_SIZING
+         (print "Left mouse button pressed")
+         (this window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard))
+      (WM_PAINT
+         (print "paint")
+         (this window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard))
+      (WM_KEYDOWN
+         (print "WM_KEYDOWN: " (refb MSG 8) "-" (refb MSG 9))
+         (if keyboard
+            (let* ((userdata (keyboard userdata (refb MSG 8))))
+               (print "new userdata: " userdata)
+               (this window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard))
+            (this window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard)))
+      (else
+         (this window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard))))
+
+(define (opengl-processor this  window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard)
+(let* ((ss2 ms2 (clock))
+       (dms (mod (+ 1000 (- ms2 ms)) 1000))) ; todo: add seconds here too
+   (if (and window renderer)
+      (if (> dms 0)
+         (let* ((result (apply renderer (append (list dms userdata) renderer-args)))
+                (userdata (car result))
+                (renderer-args (cdr result)))   ;(apply renderer (append (list dms userdata) renderer-args))))
+            (SwapBuffers hDC)
+            (this window hDC hRC  ss2 ms2  userdata  renderer renderer-args  keyboard))
+         ;else
+         (this window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard))
+     ;else
+     (begin
+        (wait 1)
+        (this window hDC hRC  ss2 ms2  userdata  renderer renderer-args  keyboard)))))
+
 ; внутренние переменные
 (let* ((MSG "1234567890123456789012345678") ; sizeof(MSG)=28
        (ss ms (clock)))
 ;; главный оконный цикл фреймворка
 (let this ((window #f) (hDC #f) (hRC #f)  (ss ss) (ms ms)  (userdata #f) (renderer #f) (renderer-args #f)  (keyboard #f))
-   ; обработаем команды фреймворка
-   (let ((envelope (check-mail)))
-      (if envelope
-         (let* ((sender message envelope))
-            (print "sender: " sender)
-            ;(print "opengl server got message " message)
-            (if (tuple? message)
-               (tuple-case message
-                  ; задать окно для рендеринга, проинициализировать в нем opengl контекст 
-                  ((set-main-window new-window) ; создать окно
-                     (if window (begin
-                        (wglMakeCurrent hDC 0)
-                        (if hRC
-                           (wglDeleteContext hRC))
-                        (if hDC
-                           (ReleaseDC window hDC))))
-                     (if new-window
-                        (let* ((window new-window)
-                               (pfd (list->byte-vector '(#x28 00  1 00  #x25 00 00 00
-                                                         00 #x10 00 00 00 00 00 00
-                                                         00 00 00 00 00 00 00 #x10 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00)))
-                               (hDC (GetDC window)))
-                        (let* ((PixelFormat (ChoosePixelFormat hDC pfd)))
-                           (print "Pixel Format: " PixelFormat)
-                           (print "    options: " (if (> (bor (ref pfd 4) 4) 0) "PFD_DRAW_TO_WINDOW " "")
-                                                  (if (> (bor (ref pfd 4) 1) 0) "PFD_DOUBLEBUFFER " "")
-                                                  (if (> (bor (ref pfd 4) 32) 0) "PFD_SUPPORT_OPENGL " ""))
-                           (print "    color bits: " (refb pfd 9))
-                           (print "    depth bits: " (refb pfd 25))
-                           (print "SetPixelFormat: " (if (=
-                           (SetPixelFormat hDC PixelFormat pfd) 1) "Ok" "Failed")))
-                        (let* ((hRC (wglCreateContext hDC)))
-                           (print "Make GL context Current: " (if (=
-                           (wglMakeCurrent hDC hRC) 1) "Ok" "Failed"))
-                           (print)
-                           (print "OpenGL version: " (glGetString GL_VERSION))
-                           (print "OpenGL vendor: " (glGetString GL_VENDOR))
-                           (print "OpenGL renderer: " (glGetString GL_RENDERER))
-                        
-                           (ShowWindow window SW_SHOW)
-                           (SetForegroundWindow window)
-                           (SetFocus window)
-
-                           (mail sender 'ok)
-                           (this window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard)))
-                        ; else
-                     (begin
-                        (mail sender 'ok)
-                        (this #false #f #f  ss ms  userdata  renderer renderer-args  keyboard))))
-                        
-                  ; простые сеттеры и геттеры:
-                  ; задать функцию рендерера, swap-buffers будет происходить автоматически
-                  ((register-renderer renderer renderer-args)
-                     (this window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard))
-                  ; set user data
-                  ((set-userdata userdata)
-                     (this window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard))
-                  ((set-keyboard keyboard)
-                     (this window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard))
-                  ; error on invalid command
-                  (else
-                     (print "Unknown opengl server request: " message)
-                     (this window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard)))
-               (this window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard))))
-   ; если окно уже есть - обработаем окно
-   (if window (begin
-      (if (= 1 (PeekMessage MSG 0 0 0 PM_REMOVE))
-         (begin
-         ; тут можно обработать сообщения к окну, если надо.
-         ; Например, такое:
-            (case (+ (refb MSG 4) (* (refb MSG 5) 256))
-               (WM_LBUTTONDOWN ;WM_SIZING
-                  (print "Left mouse button pressed"))
-               (WM_PAINT
-                  (print "paint"))
-               (WM_KEYDOWN
-                  (print "WM_KEYDOWN: " (refb MSG 8) "-" (refb MSG 9))
-                  (if keyboard
-                     (let* ((userdata (keyboard userdata (refb MSG 8))))
-                        (print "new userdata: " userdata)
-                        (this window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard)))))
-            (TranslateMessage MSG)
-            (DispatchMessage MSG)
-            (this window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard))
-         ; no windows system events - let's draw
-         (begin
-            (let* ((ss2 ms2 (clock))
-                   (dms (mod (+ 1000 (- ms2 ms)) 1000))) ; todo: add seconds here too
-               (if (> dms 0)
-                  (if renderer
-                     (let* ((result (apply renderer (append (list dms userdata) renderer-args)))
-                            (userdata (car result))
-                            (renderer-args (cdr result)))   ;(apply renderer (append (list dms userdata) renderer-args))))
-;                        (print "userdata: " userdata)
-;                        (print "renderer-args: " renderer-args)
-                        (SwapBuffers hDC)
-                        (this window hDC hRC  ss2 ms2  userdata  renderer renderer-args  keyboard))))
-                  ; no renderer
-               (this window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard)))))
-
-   (begin
-   ; если окна не было, то доберемся сюда
-   (_yield)
-   (let* ((ss ms (clock))) ; обновим часы
-      (this window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard)))))))))
+; обработаем команды фреймворка
+(let ((envelope (check-mail)))
+   (if envelope
+      (let* ((sender message envelope))
+         (print "sender: " sender)
+         ;(print "opengl server got message " message)
+         (if (tuple? message)
+            (tuple-processor this sender message  window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard)
+            ;else
+            (this window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard)))
+   (if (= 1 (PeekMessage MSG 0 0 0 PM_REMOVE))
+      (msg-processor this MSG  window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard)
+   ; else
+   (opengl-processor this window hDC hRC  ss ms  userdata  renderer renderer-args  keyboard))))))))
+   
 
 ;(fork-server 'opengl main-loop)
 
