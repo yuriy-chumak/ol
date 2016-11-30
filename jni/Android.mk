1816c93586789ff860b53d04a397593425b373f0
Yuriy Chumak <yuriy.chumak@mail.com>
2016-11-30 21:48:18 +0200

    fixed "pie" error for android build

--- jni/Android.mk
+++ jni/Android.mk
@@ -8,4 +8,10 @@ LOCAL_SRC_FILES := ../src/olvm.c ../src/boot.c
 LOCAL_CFLAGS    += -std=c99 -O2 -s
 LOCAL_LDFLAGS   := -Xlinker --export-dynamic
 
+# for solving the
+# "error: only position independent executables (PIE) are supported."
+# uncomment next lines:
+#LOCAL_CFLAGS    += -fPIC
+#LOCAL_LDFLAGS   += -Xlinker -pie
+
 include $(BUILD_EXECUTABLE)
