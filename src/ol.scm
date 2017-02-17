73ebc5b2c9fd260d9623ce55d2a9328be949ad57
yuriy-chumak <ext-yuriy.chumak@here.com>
2017-02-17 17:50:13 +0200

    license update

--- src/ol.scm
+++ src/ol.scm
@@ -2,26 +2,20 @@
 ;;; ol.scm: an Otus Lisp read-eval-print loop (REPL) binary image compiler.
 ;;;
 
-#| Copyright (c) 2012 Aki Helin
- | Copyright (c) 2014, 2015 Yuriy Chumak
+#| Copyright(c) 2012 Aki Helin
+ | Copyright(c) 2014 - 2017 Yuriy Chumak
  |
- | Permission is hereby granted, free of charge, to any person obtaining a
- | copy of this software and associated documentation files (the "Software"),
- | to deal in the Software without restriction, including without limitation
- | the rights to use, copy, modify, merge, publish, distribute, sublicense,
- | and/or sell copies of the Software, and to permit persons to whom the
- | Software is furnished to do so, subject to the following conditions
- |
- | The above copyright notice and this permission notice shall be included
- | in all copies or substantial portions of the Software.
- |
- | THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- | IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- | FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- | THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- | LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- | FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- | DEALINGS IN THE SOFTWARE.
+ | This program is free software;  you can redistribute it and/or
+ | modify it under the terms of the GNU General Public License as
+ | published by the Free Software Foundation; either version 2 of
+ | the License, or (at your option) any later version.
+ | 
+ | This program is distributed in the hope that it will be useful,
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ | 
+ | You should have received a copy of the GNU GPL along with this
+ | program.           If not, see <http://www.gnu.org/licenses/>.
  |#
 
 (print "Loading code...")
