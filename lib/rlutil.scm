124bd75b3951835264a383af8c473308c67e3ccd
yuriy-chumak <ext-yuriy.chumak@here.com>
2017-03-13 19:02:20 +0200

    rlutil goes standalone

--- lib/rlutil.scm
+++ lib/rlutil.scm
@@ -1,31 +1,35 @@
 ; https://github.com/yuriy-chumak/rlutil
 (define-library (lib rlutil)
    (import (r5rs core)
+           (owl string)
+           (owl math) (owl io) (owl list)
            (otus pinvoke))
    (export
-      cls locate setConsoleTitle)
+      locate)
 (begin
+   (define uname (syscall 63 #f #f #f))                 ; uname -a
+;   (define TERM (syscall 1016 (c-string "TERM") #f #f)) ; getenv('TERM')
 
-   (define rlutil (or (dlopen "rlutil.dll")
-                      (dlopen "librlutil.so")
-                      (runtime-error "Can't load rlutil" #f)))
-   (define gotoxy (dlsym rlutil type-void "gotoxy" type-int+ type-int+))
-   ;getkey
-   ;nb_getch
-   ;getANSIColor
-   ;getANSIBackgroundColor
-   ;setColor
-   ;setBackgroundColor
-   ;saveDefaultColor
-   ;resetColor
-   (define cls (dlsym rlutil type-void "cls"))
-   (define locate (dlsym rlutil type-void "locate" type-int+ type-int+))
-   (define setString (dlsym rlutil type-void "setString" type-string))
-   (define setChar   (dlsym rlutil type-void "setChar"   type-fix+  ))
-   ;...
-   (define trows (dlsym rlutil type-int+ "trows"))
-   (define tcols (dlsym rlutil type-int+ "tcols"))
-   ;...
-   (define setConsoleTitle (dlsym rlutil type-void "setConsoleTitle" type-string))
+   (define win32? (string-ci=? (ref uname 1) "Windows"))
+   (define linux? (string-ci=? (ref uname 1) "Linux"))
+
+   ; this check based on https://gist.github.com/ssbarnea/1316877
+   ; (define ansi? (not (and win32?
+   ;                        (not (and TERM
+   ;                                  (string-ci=? TERM "ANSI"))))))
+
+(define locate (cond
+   (win32?
+      (let*((kernel32 (dlopen "kernel32.dll"))
+            (STD_OUTPUT_HANDLE -11)
+            (GetStdHandle               (dlsym kernel32 type-vptr "GetStdHandle" type-fix+))
+            (SetConsoleCursorPosition   (dlsym kernel32 type-vptr "SetConsoleCursorPosition" type-vptr type-int+)))
+      (lambda (x y)
+         (let ((COORD (+ (band x #xFFFF)
+                         (<< (band y #xFFFF) 16))))
+            (SetConsoleCursorPosition (GetStdHandle STD_OUTPUT_HANDLE) COORD)))))
+   (linux?
+      (lambda (x y)
+         (for-each display `("\x1B;[" ,y ";" ,x "f"))))))
 
 ))
