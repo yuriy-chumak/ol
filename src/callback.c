31b183ff37460912daaee1cb7d418526fcf8652e
yuriy-chumak <yuriy.chumak@mail.com>
2017-05-23 18:33:04 +0300

    callbacks support cleanup

--- src/callback.c
+++ src/callback.c
@@ -2,30 +2,30 @@
 //long long callback(OL* ol, int id, long long* argi, double* argf, long long* others) // linux
 long callback(OL* ol, int id, int_t* argi
 #if __amd64__
-		, double* argf, int_t* rest
+		, double* argf, int_t* rest //win64
 #endif
-		) // win64
+		)
 {
 // http://stackoverflow.com/questions/11270588/variadic-function-va-arg-doesnt-work-with-float
 //	__asm("int $3");
 	word* R = ol->R;
 
-	ol->this = (word*)cdr (ol->R[NR + id]); // lambda ��� ��������� ������
-//	ol->ticker = ol->bank ? ol->bank : 999; // ����� ���? � �� ����, ��� ��� ��� ������� ��������
+	ol->this = (word*)cdr (ol->R[NR + id]); // lambda для обратного вызова
+//	ol->ticker = ol->bank ? ol->bank : 999; // зачем это? а не надо, так как без потоков работаем
 //	ol->bank = 0;
 	assert (is_reference(ol->this));
 	assert (reftype (ol->this) != TTHREAD);
 
-	// ���� ��������� ��������, ����� �� ��������� GC
-	// todo: ���������� �� � ������! � ��������������� ������ ��
+	// надо сохранить значения, иначе их уничтожит GC
+	// todo: складывать их в память! и восстанавливать оттуда же
 	R[NR + 0] = R[0]; // mcp?
-//	R[NR + 1] = R[1]; // �� ����
-//	R[NR + 2] = R[2]; // �� ����
+//	R[NR + 1] = R[1]; // не надо
+//	R[NR + 2] = R[2]; // не надо
 	R[NR + 3] = R[3]; // continuation
 
-	// ������� ������:
-	R[0] = IFALSE;  // �������� mcp, �� ���� �� �������� � �������� �� callback �������
-	R[3] = IRETURN; // ������� ������ �� �������
+	// вызовем колбек:
+	R[0] = IFALSE;  // отключим mcp, мы пока не работаем с потоками из callback функций
+	R[3] = IRETURN; // команда выхода из колбека
 	ol->arity = 1;
 
 	word types = car(ol->R[NR + id]);
@@ -107,7 +107,7 @@ long callback(OL* ol, int id, int_t* argi
 			#endif
 			long n = value * 10000; // todo: change to something like ftosn
 			long d = 10000;
-			// ������������ �������������?
+			// максимальная читабельность?
 			R[a] = (word)new_pair(TRATIONAL, itosv(n), itouv(d));
 			break;
 		}
@@ -130,7 +130,7 @@ long callback(OL* ol, int id, int_t* argi
 			#endif
 			long n = value * 10000; // todo: change to something like ftosn
 			long d = 10000;
-			// ������������ �������������?
+			// максимальная читабельность?
 			R[a] = (word)new_pair(TRATIONAL, itosv(n), itouv(d));
 			break;
 		}
@@ -205,13 +205,13 @@ long callback(OL* ol, int id, int_t* argi
 
 	//word* r =
 	runtime(ol);
-	// ������� �� �������,
-	// R, NR ����� ����������
+	// возврат из колбека,
+	// R, NR могли измениться
 	R = ol->R;
 	R[3] = R[NR + 3];
-//	R[1] = R[NR + 2]; // �� ����
-//	R[1] = R[NR + 1]; // �� ����
-	R[0] = R[NR + 0]; // ??? ����� ����� IFALSE, ���� ������� R0 ��� ��� ����� ����������������?
+//	R[1] = R[NR + 2]; // не надо
+//	R[1] = R[NR + 1]; // не надо
+	R[0] = R[NR + 0]; // ??? может лучше IFALSE, ведь прежний R0 уже мог стать недействительным?
 
 	// if result must be float or double,
 	// do the __ASM__ with loading the result into fpu/xmm register
