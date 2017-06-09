c2f68d30a75786e6446555dccbf55f36b21acd00
Yuriy Chumak <yuriy.chumak@mail.com>
2017-06-09 21:07:08 +0300

    new json printer

--- lib/json.scm
+++ lib/json.scm
@@ -1,6 +1,6 @@
 (define-library (lib json)
    (export
-      jsonify guid rand!)
+      print-json-with guid rand!)
    (import (otus lisp))
 
 (begin
@@ -71,6 +71,7 @@
                           (string-ref ss (band (>> c4  0) 15))
 ))))
 
+#|
 ; smart display that prints inexact numbers in dot-notaion:
 (define :display display)
 (define (display i)
@@ -136,6 +137,47 @@
          (else
             (display "?")
             (if (not (null? (cdr L))) (display ","))))
-      (jsonify tab (cdr L)))))
+      (jsonify tab (cdr L)))))|#
+
+(define (print-json-with display object)
+   (let jsonify ((L object))
+      (cond
+         ((symbol? L)
+            (for-each display `("\"" ,L "\"")))
+         ((string? L)
+            (for-each display `("\"" ,L "\"")))
+         ((boolean? L)
+            (display (if L "true" "false")))
+
+         ((integer? L)
+            (display L))
+         ((rational? L)
+            (let*((int (floor L))
+                  (frac (floor (* (- L int) 10000))))
+            (display int) (display ".")
+            (let loop ((i frac) (n 1000))
+               (display (floor (/ i n)))
+               (if (less? 1 n)
+                  (loop (mod i n) (/ n 10))))))
+         ((vector? L)
+            (display "[")
+            (let ((len (vec-len L)))
+               (let loop ((n 0))
+                  (if (less? n len) (begin
+                     (jsonify (vector-ref L n))
+                     (if (less? (+ n 1) len)
+                        (display ","))
+                     (loop (+ n 1))))))
+            (display "]"))
+         ((list? L)
+            (display "{")
+            (let loop ((L L))
+               (if (not (null? L)) (begin
+                  (for-each display `("\"" ,(caar L) "\":"))
+                  (jsonify (cdar L))
+                  (if (not (null? (cdr L)))
+                     (display ","))
+                  (loop (cdr L)))))
+            (display "}")))))
 
 ))
