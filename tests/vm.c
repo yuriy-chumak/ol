0b6109b7aa8afa7cc92609f588deae0592421bbb
yuriy-chumak <yuriy.chumak@mail.com>
2017-07-17 16:51:40 +0300

    more internal VM tests

--- tests/vm.c
+++ tests/vm.c
@@ -6,7 +6,7 @@ typedef uintptr_t word;
 
 #define BEGIN(x) \
 { \
-	printf("Testing " x " instructions... ");
+	printf("Testing " x " instruction... ");
 #define END() \
 	OL_free(olvm); \
 }
@@ -33,18 +33,42 @@ typedef uintptr_t word;
 		ASSERT(output assertion) \
 	END()
 
-
+#if EMBEDDED_VM
 int main(int argc, char** argv)
 {
 	int failed = 0;
 	int succeeded = 0;
 	int total = 0;
 
-	/// LD+RET
-	TESTCASE("LD+RET", == 2,
+	/// LDI
+	TESTCASE("LDE",  == 0x336, // #empty
+		//lambda(x)  LDE[6]  RET[6]
+		{ 2, 16, 4,  13, 6,  24, 6,  0 }
+	);
+	TESTCASE("LDN",  == 0x236, // #null
+		//lambda(x)  LDN[6]  RET[6]
+		{ 2, 16, 4,  77, 6,  24, 6,  0 }
+	);
+	TESTCASE("LDT",  == 0x136, // #true
+		//lambda(x)  LDN[6]  RET[6]
+		{ 2, 16, 4, 141, 6,  24, 6,  0 }
+	);
+	TESTCASE("LDF",  == 0x036, // #false
+		//lambda(x)  LDN[6]  RET[6]
+		{ 2, 16, 4, 205, 6,  24, 6,  0 }
+	);
+
+	/// LD
+	TESTCASE("LD",   == 0x202,
 		//lambda(x)  LD  2>[6]  RET[6]
 		{ 2, 16, 5,  14, 2, 6,  24, 6,  0 }
 	);
+	/// MOVE
+	TESTCASE("MOVE", == 0x202,
+		//lambda(x)  LD  2>[6]  MOVE [6]>[7]  RET[7]
+		{ 2, 16, 8,  14, 2, 6,  9, 6, 7,      24, 7,  0 }
+	);
+
 
 	/// GOTO, APPLY, RET, RUN, ARITY_ERROR, SYS, LDI, LD, REFI, MOVE, MOV2
 	/// JEQ, JP, JAF, VMNEW, VMRAW, RAWQ, CONS, TYPE, SIZE, CAST, CAR, CDR
@@ -57,3 +81,4 @@ int main(int argc, char** argv)
 	///
 	return (failed != 0);
 }
+#endif
