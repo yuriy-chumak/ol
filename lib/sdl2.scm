8bf453afb9695e340ce2bd6207a68c9bb22eab05
Yuriy Chumak <yuriy.chumak@mail.com>
2017-11-21 23:56:26 +0200

    added basic render+event loops

--- lib/sdl2.scm
+++ lib/sdl2.scm
@@ -45,6 +45,15 @@
       ((a) a)
       ((a . bs) (fold bor a bs))))
 
+(define int32->ol (case (vm:endianness)
+   (1 (lambda (vector offset)
+         (+     (ref vector offset)
+            (<< (ref vector (+ offset 1)) 8)
+            (<< (ref vector (+ offset 2)) 16)
+            (<< (ref vector (+ offset 3)) 24))))
+   (else
+      (print "Unknown endianness")
+      #false)))
 
 ; ------------------------
 ; SDL_error
@@ -74,8 +83,24 @@
 
 ; ------------------------
 ; SDL_render
+(define SDL_Renderer* type-vptr)
 
+(define SDL_CreateRenderer (dlsym % SDL_Renderer* "SDL_CreateRenderer" SDL_Window* type-int+ type-int+))
+   (define SDL_RENDERER_SOFTWARE #x00000001)          ;/**< The renderer is a software fallback */
+   (define SDL_RENDERER_ACCELERATED #x00000002)       ;/**< The renderer uses hardware acceleration */
+   (define SDL_RENDERER_PRESENTVSYNC #x00000004)      ;/**< Present is synchronized with the refresh rate */
+   (define SDL_RENDERER_TARGETTEXTURE #x00000008)     ;/**< The renderer supports
 
+(define SDL_RenderClear (dlsym % type-int+ "SDL_RenderClear" SDL_Renderer*))
+(define SDL_RenderPresent (dlsym % type-void "SDL_RenderPresent" SDL_Renderer*))
+
+; ------------------------
+; SDL_event
+(define SDL_Event* type-vector-raw)
+(define (make-SDL_Event) (vm:new-raw-object type-vector-raw 56))
+
+(define SDL_PollEvent (dlsym % type-fix+ "SDL_PollEvent" SDL_Event*))
+   (define SDL_QUIT #x100)
 ;))
 
 ; test:
@@ -89,7 +114,28 @@
    SDL_WINDOWPOS_UNDEFINED SDL_WINDOWPOS_UNDEFINED
    640 480 SDL_WINDOW_SHOWN))
 
-(define surface (SDL_GetWindowSurface window))
+;(define surface (SDL_GetWindowSurface window))
+(define renderer (SDL_CreateRenderer window -1 (bor SDL_RENDERER_ACCELERATED SDL_RENDERER_PRESENTVSYNC)))
 
 
-(print "ok.")
+(call/cc (lambda (break)
+   (let loop ()
+      (let ((event (make-SDL_Event)))
+         (let event-loop ()
+            (unless (eq? (SDL_PollEvent event) 0)
+               (let ((type (int32->ol event 0)))
+                  (case type
+                     (SDL_QUIT (break))
+                     (else
+                        #f))
+                  (event-loop)))))
+      (SDL_RenderClear renderer)
+      (SDL_RenderPresent renderer)
+      (loop))))
+
+
+;(SDL_DestroyRenderer renderer)
+;(SDL_DestroyWindow window)
+;(SDL_Quit)
+
+(print "done.")
