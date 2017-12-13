be66111b823eeaa5f1fbd1c8e394bc897bb5cde9
Yuriy Chumak <yuriy.chumak@mail.com>
2017-12-13 22:34:10 +0200

    sqlite sample fixed

--- tutorial/Sqlite/sqlite.lisp
+++ tutorial/Sqlite/sqlite.lisp
@@ -12,88 +12,33 @@
 
 ; todo: move this test into separate script (tests/sqlite3.scm)
 (define database (make-sqlite3))
-(print "open: "     (sqlite3_open (c-string ":memory:") database))
-;(print "close: "    (sqlite3-close database))
-;(print "open: "     (sqlite3_open (c-string "x.sqlite") database))
-
-
-
-(define (execute query . args)
-   (let ((statement (make-sqlite3-stmt)))
-      (if (less? 0 (sqlite3-prepare-v2 database (c-string query) -1 statement #f))
-         (runtime-error "error query preparation" query))
-      (let loop ((n 1) (args args))
-         (if (not (null? args))
-            (let ((arg (car args)))
-               (print "type-arg: " (type arg))
-               (cond
-                  ((integer? arg)
-                     (print "integer")
-                     ;todo: if > max-int-value use sqlite3_bind_int64
-                     (sqlite3-bind-int    statement n arg))
-                  ((rational? arg)
-                     (sqlite3-bind-double statement n arg))
-                  (else
-                     (runtime-error "Unsupported parameter type" arg)))
-               (loop (+ n 1) (cdr args)))))
-      (let ((result (sqlite3-step statement)))
-         (sqlite3-finalize statement)
-         result)))
-
-(define (select query)
-   (let ((statement (make-sqlite3-stmt)))
-      (if (less? 0 (sqlite3-prepare-v2 database (c-string query) -1 statement #f))
-         (print "error query [" query "] preparation"))
-      (sqlite3-step statement)
-      statement))
-
-(define (sqlite:for-each query handler)
-   (let ((statement (make-sqlite3-stmt)))
-      (if (less? 0 (sqlite3-prepare-v2 database (c-string query) -1 statement #f))
-         (runtime-error "error query preparation" query))
-      (let loop ((x null))
-         (if (= (sqlite3-step statement) SQLITE_ROW)
-            (loop (handler statement))))
-      (sqlite3-finalize statement)))
+(print "open: "  (sqlite3_open (c-string ":memory:") database))
 
 ; todo: add sqlite3-fold function:
-(define (with-sql-query string processor)
-   (define query (select string))
-   (processor query)
-   (sqlite3-finalize query))
+;(define (with-sql-query string processor)
+;   (define query (select string))
+;   (processor query)
+;   (sqlite3-finalize query))
 
-(define statement (make-sqlite3-stmt))
-
-(execute "CREATE TABLE test (id INTEGER)")
-(sqlite:exec database "INSERT INTO test VALUES (1)")
-(sqlite:exec database "INSERT INTO test VALUES (2)")
-(sqlite:exec database "INSERT INTO test VALUES (7)")
-
+(sqlite:query database "CREATE TABLE test (id INTEGER)")
+(sqlite:query database "INSERT INTO test VALUES (1)")
+(sqlite:query database "INSERT INTO test VALUES (2)")
+(sqlite:query database "INSERT INTO test VALUES (7)")
 
 (let ((db (make-sqlite3)))
    (sqlite3_open (c-string "db.sqlite") db)
-   (sqlite:exec db "CREATE TABLE IF NOT EXISTS T (id INTEGER PRIMARY KEY, text STRING)")
-   (sqlite:exec db "INSERT INTO T (text) VALUES (?)" "one")
-   (sqlite:exec db "INSERT INTO T (text) VALUES (?)" "two")
-   (sqlite:exec db "INSERT INTO T (text) VALUES (?)" "three")
-   (sqlite:exec db "INSERT INTO T (text) VALUES (?)" "four")
-   (sqlite3-close db))
+   (sqlite:query db "CREATE TABLE IF NOT EXISTS T (id INTEGER PRIMARY KEY, text STRING)")
+   (sqlite:query db "INSERT INTO T (text) VALUES (?)" "one")
+   (sqlite:query db "INSERT INTO T (text) VALUES (?)" "two")
+   (sqlite:query db "INSERT INTO T (text) VALUES (?)" "three")
+   (sqlite:query db "INSERT INTO T (text) VALUES (?)" "four")
+   (sqlite3_close db))
 
+(print "query: " (sqlite:query database "INSERT INTO test VALUES (?)" 11))
 
-(print "exec: " (sqlite:exec database "INSERT INTO test VALUES (?)" 11))
+(sqlite:for-each (sqlite:query database "SELECT * FROM test")
+   (lambda (row)
+      (print "values: " row)))
 
-;(print "prepare: "  (sqlite3_prepare_v2 database (c-string "CREATE TABLE test ( id INTEGER )") -1 statement #f))
-;(print "step: " (sqlite3_step statement))
-
-;(print "1: " (sqlite3_column_count statement))
-;(print "name: " (sqlite3_column_name statement 1))
-(define query (select "SELECT COUNT(id) FROM test"))
-(print "count(id) = " (sqlite3_column_int query 0))
-(sqlite3-finalize query)
-
-(sqlite:for-each "SELECT * FROM test" (lambda (statement)
-   (print "value: " (sqlite3_column_int statement 0))))
-
-;(print "finalize: " (sqlite3-finalize statement))
-(print "close: "    (sqlite3-close database))
+(print "close: "    (sqlite3_close database))
 
