252e878f6cb13c15da216499b6943b9cb7a25b29
yuriy-chumak <yuriy.chumak@mail.com>
2018-02-07 14:27:29 +0200

    added color to simple opengl triangle sample

--- samples/OpenGL/202. Drawing simple triangle.lisp
+++ samples/OpenGL/202. Drawing simple triangle.lisp
@@ -15,7 +15,12 @@
 
    (glColor3f 0.2 0.5 0.2)
    (glBegin GL_TRIANGLES)
-      (glVertex2f -0.2 -0.2)
-      (glVertex2f +0.2 -0.2)
-      (glVertex2f -0.0 +0.3)
+      (glColor3f 1 0 0)
+      (glVertex2f -0.6 -0.6)
+
+      (glColor3f 0 1 0)
+      (glVertex2f +0.6 -0.6)
+
+      (glColor3f 0 0 1)
+      (glVertex2f -0.0 +0.7)
    (glEnd)))
