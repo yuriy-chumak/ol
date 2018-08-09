35fc7721da401d973cffb39a28866137c29893b1
Yuriy Chumak <yuriy.chumak@mail.com>
2018-08-09 19:03:14 +0300

    pacman updated to last speedup embed changes

--- samples/pacman/main.c
+++ samples/pacman/main.c
@@ -27,6 +27,8 @@ struct timeval blinkytimestamp = {0, 0}; // for fps
 unsigned frames = 0;
 // unsigned ticks = 0; // todo: up every 100 ms
 
+uintptr_t eat_the_point;
+uintptr_t get_level;
 
 // olvm:
 ol_t ol;
@@ -34,19 +36,48 @@ ol_t ol;
 // just code simplification, some kind of magic to not manually write 'new_string' and 'make_integer':
 // we automatically will call new_string and make_integer depends on argument type
 // but in general case you should do this manually. or not.
+// C preprocessor trick, some kind of "map":
+// https://github.com/swansontec/map-macro
+///*
+#define EVAL0(...) __VA_ARGS__
+#define EVAL1(...) EVAL0(EVAL0(EVAL0(__VA_ARGS__)))
+#define EVAL2(...) EVAL1(EVAL1(EVAL1(__VA_ARGS__)))
+#define EVAL3(...) EVAL2(EVAL2(EVAL2(__VA_ARGS__)))
+#define EVAL4(...) EVAL3(EVAL3(EVAL3(__VA_ARGS__)))
+#define EVAL(...)  EVAL4(EVAL4(EVAL4(__VA_ARGS__)))
+
+#define MAP_END(...)
+#define MAP_OUT
+#define MAP_COMMA ,
+
+#define MAP_GET_END2() 0, MAP_END
+#define MAP_GET_END1(...) MAP_GET_END2
+#define MAP_GET_END(...) MAP_GET_END1
+#define MAP_NEXT0(test, next, ...) next MAP_OUT
+#define MAP_NEXT1(test, next) MAP_NEXT0(test, next, 0)
+#define MAP_NEXT(test, next)  MAP_NEXT1(MAP_GET_END test, next)
+
+#define MAP0(f, x, peek, ...) f(x) MAP_NEXT(peek, MAP1)(f, peek, __VA_ARGS__)
+#define MAP1(f, x, peek, ...) f(x) MAP_NEXT(peek, MAP0)(f, peek, __VA_ARGS__)
+
+#define MAP_LIST_NEXT1(test, next) MAP_NEXT0(test, MAP_COMMA next, 0)
+#define MAP_LIST_NEXT(test, next)  MAP_LIST_NEXT1(MAP_GET_END test, next)
+
+#define MAP_LIST0(f, x, peek, ...) f(x) MAP_LIST_NEXT(peek, MAP_LIST1)(f, peek, __VA_ARGS__)
+#define MAP_LIST1(f, x, peek, ...) f(x) MAP_LIST_NEXT(peek, MAP_LIST0)(f, peek, __VA_ARGS__)
+
+#define MAP(f, ...) EVAL(MAP1(f, __VA_ARGS__, ()()(), ()()(), ()()(), 0))
+#define MAP_LIST(f, ...) EVAL(MAP_LIST1(f, __VA_ARGS__, ()()(), ()()(), ()()(), 0))
+//*/  end of C preprocessor trick
+
 #define _Q(x) \
-	__builtin_choose_expr( __builtin_types_compatible_p (typeof(x), char[]), \
-		new_string(&ol, (char*)(uintptr_t)x), \
-	__builtin_choose_expr( __builtin_types_compatible_p (typeof(x), char*), \
-		new_string(&ol, (char*)(uintptr_t)x), \
-	__builtin_choose_expr( __builtin_types_compatible_p (typeof(x), int), \
-		make_integer((int)(uintptr_t)x), \
-	__builtin_choose_expr( __builtin_types_compatible_p (typeof(x), unsigned), \
-		make_integer((int)(uintptr_t)x), \
-	__builtin_choose_expr( __builtin_types_compatible_p (typeof(x), long), \
-		make_integer((int)(uintptr_t)x), \
-	/*else*/ \
-		IFALSE)))))
+	__builtin_choose_expr( __builtin_types_compatible_p (__typeof__(x), char[]),   new_string(&ol, (char*)(uintptr_t)x), \
+	__builtin_choose_expr( __builtin_types_compatible_p (__typeof__(x), char*),    new_string(&ol, (char*)(uintptr_t)x), \
+	__builtin_choose_expr( __builtin_types_compatible_p (__typeof__(x), int),      make_integer((int)(uintptr_t)x), \
+	__builtin_choose_expr( __builtin_types_compatible_p (__typeof__(x), unsigned), make_integer((int)(uintptr_t)x), \
+	__builtin_choose_expr( __builtin_types_compatible_p (__typeof__(x), long),     make_integer((long)(uintptr_t)x), \
+	__builtin_choose_expr( __builtin_types_compatible_p (__typeof__(x), uintptr_t),(uintptr_t)x, \
+	IFALSE))))))
 
 #define eval(...) embed_eval(&ol, MAP_LIST(_Q, __VA_ARGS__), 0)
 
@@ -142,6 +173,12 @@ void init(void)
 	// initial vm communication
 	eval("(import (main))");
 	eval("eat-the-point", mainx, mainy);
+
+	// compile script functions for speedup
+	// but this is not required - we still can call this functions
+	// by name, not by pinned id
+	eat_the_point = eval("(vm:pin eat-the-point)");
+	get_level = eval("(vm:pin get-level)");
 }
 
 void idle() {
@@ -178,11 +215,11 @@ void keys(int key, int x, int y) {
 		return;
 	}
 
-	word p = eval("get-level", mainx+dx, mainy+dy);      assert(is_small(p));
+	word p = eval(get_level, mainx+dx, mainy+dy);      assert(is_small(p));
 	if (ol2small(p) == 1) {
 		mainx += dx;
 		mainy += dy;
-		eval("eat-the-point", mainx, mainy);
+		eval(eat_the_point, mainx, mainy);
 	}
 	glutPostRedisplay();
 }
