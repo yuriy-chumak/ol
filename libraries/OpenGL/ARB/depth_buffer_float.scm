5610969510ca7baf2818be531503165d2b5abcbc
Yuriy Chumak <yuriy.chumak@mail.com>
2018-09-07 16:24:45 +0300

    one more opengl extension

---
+++ libraries/OpenGL/ARB/depth_buffer_float.scm
@@ -0,0 +1,48 @@
+; ==========================================================================
+; ARB_depth_buffer_float
+;
+;     https://www.khronos.org/registry/OpenGL/extensions/ARB/ARB_depth_buffer_float.txt
+;
+(define-library (OpenGL ARB depth_buffer_float)
+
+; --------------------------------------------------------------------------
+; Dependencies
+;     None
+   (import
+      (scheme core) (owl io)
+      (OpenGL version-1-0))
+
+; --------------------------------------------------------------------------
+   (export  ARB_depth_buffer_float
+
+; --------------------------------------------------------------------------
+; New Procedures and Functions
+   
+; --------------------------------------------------------------------------
+; New Tokens
+;
+
+;    Accepted by the <internalformat> parameter of TexImage1D, TexImage2D,
+;    TexImage3D, CopyTexImage1D, CopyTexImage2D, and RenderbufferStorageEXT,
+;    and returned in the <data> parameter of GetTexLevelParameter and
+;    GetRenderbufferParameterivEXT:
+   DEPTH_COMPONENT32F
+   DEPTH32F_STENCIL8
+
+;    Accepted by the <type> parameter of DrawPixels, ReadPixels, TexImage1D,
+;    TexImage2D, TexImage3D, TexSubImage1D, TexSubImage2D, TexSubImage3D, and
+;    GetTexImage:
+
+   FLOAT_32_UNSIGNED_INT_24_8_REV
+
+)
+
+; --------------------------------------------------------------------------
+(begin
+   (define ARB_depth_buffer_float (gl:QueryExtension "GL_ARB_depth_buffer_float"))
+
+   (define DEPTH_COMPONENT32F              #x8CAC)
+   (define DEPTH32F_STENCIL8               #x8CAD)
+   (define FLOAT_32_UNSIGNED_INT_24_8_REV  #x8DAD)
+
+))
