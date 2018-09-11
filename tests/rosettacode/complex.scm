04fa3eaa003a2a593c54932b49f810668cee576c
Yuriy Chumak <yuriy.chumak@mail.com>
2018-09-11 18:34:22 +0300

    more rosettacode

---
+++ tests/rosettacode/complex.scm
@@ -0,0 +1,12 @@
+(define A 0+1i) ; manually entered numbers
+(define B 1+0i)
+
+(print (+ A B))
+(print (- A B))
+(print (* A B))
+(print (/ A B))
+
+(define C (complex 2/7 -3)) ; functional way
+
+(print "real part of " C " is " (car C))
+(print "imaginary part of " C " is " (cdr C))
