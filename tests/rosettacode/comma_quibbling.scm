85ca907a0f38b954cec87cebca23a83a68cd1624
Yuriy Chumak <yuriy.chumak@mail.com>
2018-09-12 17:00:10 +0300

    more rosettacode

---
+++ tests/rosettacode/comma_quibbling.scm
@@ -0,0 +1,19 @@
+; http://www.rosettacode.org/wiki/Comma_quibbling
+(define (quibble . args)
+   (display "{")
+   (let loop ((args args))
+      (unless (null? args) (begin
+         (display (car args))
+         (cond
+            ((= 1 (length args)) #t)
+            ((= 2 (length args))
+               (display " and "))
+            (else
+               (display ", ")))
+         (loop (cdr args)))))
+   (print "}"))
+
+(quibble)
+(quibble "ABC")
+(quibble "ABC" "DEF")
+(quibble "ABC" "DEF" "G" "H")
