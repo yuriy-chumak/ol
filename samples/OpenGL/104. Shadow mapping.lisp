08a4413b0c7ac513b08130fbe52289cb9bda88d2
Yuriy Chumak <yuriy.chumak@comodo.com>
2018-09-21 19:47:26 +0300

    shadows example

--- samples/OpenGL/104. Shadow mapping.lisp
+++ samples/OpenGL/104. Shadow mapping.lisp
@@ -4,9 +4,10 @@
 (gl:set-window-title "4. Shadow mapping")
 ;(gl:set-window-size 854 480)
 
-(import (OpenGL version-1-1))
+(import (OpenGL version-1-3))
 (import (OpenGL ARB depth_texture))
 (import (OpenGL ARB shadow))
+;(import (OpenGL ARB multitexture))
 
 (unless (and ARB_depth_texture ARB_shadow)
    (begin
@@ -14,6 +15,7 @@
       (halt 1)))
 
 (import (lib math))
+(define (make-mat4x4) (map (lambda (_) (inexact 0)) (iota 16)))
 
 ; load the model
 (import (lang sexp))
@@ -77,22 +79,36 @@
 
 ; ---------------------------------------------
 ; setup
-(glMatrixMode GL_MODELVIEW)
-(glLoadIdentity)
+;; (glMatrixMode GL_MODELVIEW)
+;; (glLoadIdentity)
 
+;; (glClearColor 0 0 0 0)
+;; (glColor4f 1 1 1 1)
 (glShadeModel GL_SMOOTH)
-(glClearColor 0 0 0 0)
-(glColor4f 1 1 1 1)
 (glHint GL_PERSPECTIVE_CORRECTION_HINT GL_NICEST)
 
-; depth states
-(glClearDepth 1)
-(glDepthFunc GL_LEQUAL)
+;; ; depth states
+;; (glClearDepth 1)
+;; (glDepthFunc GL_LEQUAL)
 (glEnable GL_DEPTH_TEST)
+
+(glEnable GL_LIGHTING)
+(glLightModelf GL_LIGHT_MODEL_TWO_SIDE GL_TRUE)
+(glEnable GL_LIGHT0)
+;(glLightfv GL_LIGHT0 GL_POSITION '(0 3 0  1))
+(glLightfv GL_LIGHT0 GL_AMBIENT '(0.2 0.2 0.2 1)) ; white * 0.2
+(glLightfv GL_LIGHT0 GL_DIFFUSE '(0.2 0.2 0.2 1)) ; white * 0.2
+(glLightfv GL_LIGHT0 GL_SPECULAR '(0 0 0 1)) ; black
+
+;? (glEnable GL_NORMALIZE)
+
+(glColorMaterial GL_FRONT GL_AMBIENT_AND_DIFFUSE)
+;(glMaterialf GL_FRONT GL_SHININESS 16)
+(glEnable GL_COLOR_MATERIAL)
 
 (glEnable GL_CULL_FACE)
 
-; We use glScale when drawing the scene (? maybe not required)
+;; ; We use glScale when drawing the scene (? maybe not required)
 (glEnable GL_NORMALIZE)
 
 ; Create the shadow map texture
@@ -104,68 +120,37 @@
 (define shadowMapSize 512)
 
 (glBindTexture GL_TEXTURE_2D shadowMapTexture)
-(glTexImage2D  GL_TEXTURE_2D 0 GL_DEPTH_COMPONENT shadowMapSize shadowMapSize 0 GL_DEPTH_COMPONENT GL_UNSIGNED_BYTE NULL)
-(glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MIN_FILTER GL_NEAREST)
-(glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MAG_FILTER GL_NEAREST)
-(glTexParameteri GL_TEXTURE_2D GL_TEXTURE_WRAP_S GL_CLAMP)
-(glTexParameteri GL_TEXTURE_2D GL_TEXTURE_WRAP_T GL_CLAMP)
+(glTexParameteri GL_TEXTURE_2D GL_TEXTURE_WRAP_S GL_CLAMP_TO_EDGE)
+(glTexParameteri GL_TEXTURE_2D GL_TEXTURE_WRAP_T GL_CLAMP_TO_EDGE)
+(glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MAG_FILTER GL_LINEAR)
+(glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MIN_FILTER GL_LINEAR)
 
-; Use the color as the ambient and diffuse material
-(glColorMaterial GL_FRONT GL_AMBIENT_AND_DIFFUSE)
-(glEnable GL_COLOR_MATERIAL)
+(glTexParameteri GL_TEXTURE_2D GL_TEXTURE_COMPARE_MODE_ARB GL_COMPARE_R_TO_TEXTURE_ARB)
+(glTexParameteri GL_TEXTURE_2D GL_TEXTURE_COMPARE_FUNC_ARB GL_LEQUAL)
 
-(define white '(1 1 1 1))
-(glMaterialfv GL_FRONT GL_SPECULAR white)
-(glMaterialf GL_FRONT GL_SHININESS 16)
+(glTexGeni GL_S GL_TEXTURE_GEN_MODE GL_EYE_LINEAR)
+(glTexGeni GL_T GL_TEXTURE_GEN_MODE GL_EYE_LINEAR)
+(glTexGeni GL_R GL_TEXTURE_GEN_MODE GL_EYE_LINEAR)
+(glTexGeni GL_Q GL_TEXTURE_GEN_MODE GL_EYE_LINEAR)
 
-; prepare matrices
-(glPushMatrix)
-
-(define (make-mat4x4) (map (lambda (_) (inexact 0)) (iota 16)))
-
-(define lightProjectionMatrix (make-mat4x4))
-(define lightViewMatrix (make-mat4x4))
-(define cameraProjectionMatrix (make-mat4x4))
-(define cameraViewMatrix (make-mat4x4))
-
-(glLoadIdentity)
-(gluPerspective 45.0 (/ 854 480) 1.0 100)
-(glGetFloatv GL_MODELVIEW_MATRIX cameraProjectionMatrix)
-
-(glLoadIdentity)
-(gluLookAt 6 9 6 ;cameraPosition
-           1 3 0
-           0 1 0)
-(glGetFloatv GL_MODELVIEW_MATRIX cameraViewMatrix)
-
-(glLoadIdentity)
-(gluPerspective 45.0 1.0 2.0 32.0)
-(glGetFloatv GL_MODELVIEW_MATRIX lightProjectionMatrix)
-;(print "lightProjectionMatrix: " lightProjectionMatrix)
-
-(glLoadIdentity)
-(gluLookAt 0 9 3 ;lightPosition
-           1 3 0
-           0 1 0)
-(glGetFloatv GL_MODELVIEW_MATRIX lightViewMatrix)
-
-(glPopMatrix)
+(glTexImage2D GL_TEXTURE_2D 0 GL_DEPTH_COMPONENT shadowMapSize shadowMapSize 0
+   GL_DEPTH_COMPONENT GL_UNSIGNED_BYTE NULL)
 
 (define (DrawScene)
    ; floor
-   (glColor4f '(0.2 0.2 0.2 1))
+   (glColor3f 0 1 0)
    (glBegin GL_QUADS)
       (glNormal3f 0 1 0)
-      (glVertex3f -5 0 -5)
       (glVertex3f  5 0 -5)
-      (glVertex3f  5 0  5)
+      (glVertex3f -5 0 -5)
       (glVertex3f -5 0  5)
+      (glVertex3f  5 0  5)
    (glEnd)
 
    ; show model
    (for-each (lambda (o)
          (let ((diffuse-color (get (cdr o) 'kd #f)))
-            (glColor3f diffuse-color))
+            (glColor3fv diffuse-color))
          (glCallList (car o)))
       obj)
 )
@@ -175,192 +160,143 @@
       (skip (cdr l) (- n 1))
       l))
 
+(define mv (make-mat4x4))
+(define pr (make-mat4x4))
 
 ; draw
 (gl:set-renderer (lambda ()
+   (let*((ss ms (clock))
+         (x (* 3 (sin (+ ss (/ ms 1000)))))
+         (y (+ 8 (* 1 (sin (/ (+ ss (/ ms 1000)) 8)))))
+         (z (* 3 (cos (+ ss (/ ms 1000))))))
+
    ; First pass - from light's point of view
-   (glClear (vm:or GL_COLOR_BUFFER_BIT GL_DEPTH_BUFFER_BIT))
+   ;#|
+   ; (render-to-shadow-map):
+   ; //Use viewport the same size as the shadow map
+   (glPushAttrib GL_VIEWPORT_BIT)
+   (glViewport 0 0 shadowMapSize shadowMapSize)
+   (glClear GL_DEPTH_BUFFER_BIT)
+
+   (glColorMask GL_FALSE GL_FALSE GL_FALSE GL_FALSE)
+
+   (glEnable GL_POLYGON_OFFSET_FILL)
+   (glPolygonOffset 4 4)
 
    (glMatrixMode GL_PROJECTION)
-   (glLoadMatrixf lightProjectionMatrix)
- 
+   (glLoadIdentity)
+   (gluPerspective 90.0 1 1 30)
+   (gluLookAt x y z ; lightPosition
+              0 0 0
+              0 1 0)
+
    (glMatrixMode GL_MODELVIEW)
-   (glLoadMatrixf lightViewMatrix)
+   (glLoadIdentity)
+
+   (glGetFloatv GL_MODELVIEW_MATRIX mv)
+   (glGetFloatv GL_PROJECTION_MATRIX pr)
  
-   ; //Use viewport the same size as the shadow map
-   (glViewport 0 0 shadowMapSize shadowMapSize)
- 
-   ; //Draw back faces into the shadow map
-   (glCullFace GL_FRONT)
- 
-   ; //Disable color writes, and use flat shading for speed
-   (glShadeModel GL_FLAT)
-   (glColorMask 0 0 0 0)
-     
    ; //Draw the scene
+   (glDisable GL_TEXTURE_2D)
+   (glDisable GL_LIGHTING)
    (DrawScene)
  
    ; //Read the depth buffer into the shadow map texture
    (glBindTexture GL_TEXTURE_2D shadowMapTexture)
-   (glCopyTexSubImage2D GL_TEXTURE_2D 0 0 0 0 0 shadowMapSize shadowMapSize)
+   (glCopyTexImage2D GL_TEXTURE_2D 0 GL_DEPTH_COMPONENT 0 0 shadowMapSize shadowMapSize 0)
  
    ; //restore states
-   (glColorMask 1 1 1 1)
-   (glShadeModel GL_SMOOTH)
-   (glCullFace GL_BACK)
+   (glDisable GL_POLYGON_OFFSET_FILL)
+   (glColorMask GL_TRUE GL_TRUE GL_TRUE GL_TRUE)
+   (glPopAttrib)
+   #|
 
    ; DEBUG:
-   ;; (glViewport 0 0 854 480)
-   ;; (glClear GL_DEPTH_BUFFER_BIT)
-
-   ;; (glMatrixMode GL_PROJECTION)
-   ;; (glLoadIdentity)
-   ;; (glMatrixMode GL_MODELVIEW)
-   ;; (glLoadIdentity)
-
-   ;; (glEnable GL_TEXTURE_2D)
-   ;; (glBindTexture GL_TEXTURE_2D shadowMapTexture)
-
-   ;; (glColor3f 0.8 0.8 0.8)
-   ;; (glBegin GL_QUADS)
-   ;; (glTexCoord2f  0  0)
-   ;; (glVertex2f   -1 -1)
-   ;; (glTexCoord2f  1  0)
-   ;; (glVertex2f    1 -1)
-   ;; (glTexCoord2f  1  1)
-   ;; (glVertex2f    1  1)
-   ;; (glTexCoord2f  0  1)
-   ;; (glVertex2f   -1  1)
-   ;; (glEnd)
-
-   ;; (glDisable GL_TEXTURE_2D)
- 
-   ;; ; end of DEBUG
-
-   ;#|
-   ;; ; //2nd pass - Draw from camera's point of view
-   (glViewport 0 0 854 480)
    (glClear GL_DEPTH_BUFFER_BIT)
- 
    (glMatrixMode GL_PROJECTION)
-   ;(glLoadMatrixf cameraProjectionMatrix)
    (glLoadIdentity)
-   (gluPerspective 45.0 (/ 854 480) 1.0 100)
-     
    (glMatrixMode GL_MODELVIEW)
-   ;(glLoadMatrixf cameraViewMatrix)
+   (glLoadIdentity)
+
+   (glEnable GL_TEXTURE_2D)
+   (glBindTexture GL_TEXTURE_2D shadowMapTexture)
+
+   (glColor3f 1 1 1)
+   (glBegin GL_QUADS)
+      (glTexCoord2f  0  0)
+      (glVertex2f   -1 -1)
+      (glTexCoord2f  1  0)
+      (glVertex2f    1 -1)
+      (glTexCoord2f  1  1)
+      (glVertex2f    1  1)
+      (glTexCoord2f  0  1)
+      (glVertex2f   -1  1)
+   (glEnd)
+   ; end of DEBUG
+   #||#
+
+   ; let's start draw scene with shadows:
+   (glClear GL_DEPTH_BUFFER_BIT)
+   (glClear GL_COLOR_BUFFER_BIT)
+
+   (glMatrixMode GL_PROJECTION)
+   (glLoadIdentity)
+   (gluPerspective 45.0 (/ 854 480) 1 60)
+
+   (glMatrixMode GL_MODELVIEW)
    (glLoadIdentity)
    (gluLookAt 6 9 6 ;cameraPosition
-            1 3 0
+            1 3 0 ; 1 3 0
             0 1 0)
- 
-   ; //Use dim light to represent shadowed areas
-   (glLightfv GL_LIGHT1 GL_POSITION '(0 9 3 1)) ; lightPosition
-   (glLightfv GL_LIGHT1 GL_AMBIENT '(0.2 0.2 0.2 1)) ; white * 0.2
-   (glLightfv GL_LIGHT1 GL_DIFFUSE '(0.2 0.2 0.2 1)) ; white * 0.2
-   (glLightfv GL_LIGHT1 GL_SPECULAR '(0 0 0 1)) ; black
-   (glEnable GL_LIGHT1)
-   (glEnable GL_LIGHTING)
-
-   ;; (DrawScene)
 
    ;#|
-   ;; ; //3rd pass
-   ;; ; //Draw with bright light
-   (glLightfv GL_LIGHT1 GL_DIFFUSE white)
-   (glLightfv GL_LIGHT1 GL_SPECULAR white)
+   ;(glActiveTextureARB GL_TEXTURE1_ARB)
+   (glBindTexture GL_TEXTURE_2D shadowMapTexture)
 
-   (glMatrixMode GL_MODELVIEW)
-   (glPushMatrix)
-   (glLoadIdentity)
-   (glLoadMatrixf '( ; biasMatrix
-      0.5  0.0  0.0  0.0
-      0.0  0.5  0.0  0.0
-      0.0  0.0  0.5  0.0
-      0.5  0.5  0.5  1.0)) ; //bias from [-1, 1] to [0, 1]
-   (glMultMatrixf lightProjectionMatrix)
-   (glMultMatrixf lightViewMatrix)
-
-   (define textureMatrix (make-mat4x4))
-   (glGetFloatv GL_MODELVIEW_MATRIX textureMatrix)
-
-   (glPopMatrix)
-
-   (glTexGeni GL_S GL_TEXTURE_GEN_MODE GL_EYE_LINEAR)
-   (glTexGenfv GL_S GL_EYE_PLANE (take (skip textureMatrix 0) 4)) ;GetRow(0))
+   (glEnable GL_TEXTURE_2D)
    (glEnable GL_TEXTURE_GEN_S)
- 
-   (glTexGeni GL_T GL_TEXTURE_GEN_MODE GL_EYE_LINEAR)
-   (glTexGenfv GL_T GL_EYE_PLANE (take (skip textureMatrix 4) 4)) ;GetRow(1))
    (glEnable GL_TEXTURE_GEN_T)
- 
-   (glTexGeni GL_R GL_TEXTURE_GEN_MODE GL_EYE_LINEAR)
-   (glTexGenfv GL_R GL_EYE_PLANE (take (skip textureMatrix 8) 4)) ;GetRow(2))
    (glEnable GL_TEXTURE_GEN_R)
- 
-   (glTexGeni GL_Q GL_TEXTURE_GEN_MODE GL_EYE_LINEAR)
-   (glTexGenfv GL_Q GL_EYE_PLANE (take (skip textureMatrix 12) 4)) ;GetRow(3))
    (glEnable GL_TEXTURE_GEN_Q)
 
-   (glBindTexture GL_TEXTURE_2D shadowMapTexture)
-   (glEnable GL_TEXTURE_2D)
+   (glTexGenfv GL_S GL_EYE_PLANE '(1 0 0 0))
+   (glTexGenfv GL_T GL_EYE_PLANE '(0 1 0 0))
+   (glTexGenfv GL_R GL_EYE_PLANE '(0 0 1 0))
+   (glTexGenfv GL_Q GL_EYE_PLANE '(0 0 0 1))
 
-   ; Enable shadow comparison
-   (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_COMPARE_MODE_ARB GL_COMPARE_R_TO_TEXTURE_ARB)
+   (glTexEnvi GL_TEXTURE_ENV GL_TEXTURE_ENV_MODE GL_MODULATE)
 
-   ; Shadow comparison should be true (ie not in shadow) if r<=texture
-   (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_COMPARE_FUNC_ARB GL_LEQUAL)
+   (glMatrixMode GL_TEXTURE)
+   (glPushMatrix)
 
-   ; Shadow comparison should generate an INTENSITY result
-   (glTexParameteri GL_TEXTURE_2D GL_DEPTH_TEXTURE_MODE_ARB GL_INTENSITY)
+   (glLoadIdentity)
+   (glTranslatef 0.5 0.5 0.5)
+   (glScalef 0.5 0.5 0.5)
+   (glMultMatrixf pr)
+   (glMultMatrixf mv)
 
-   ; Set alpha test to discard false comparisons
-   (glAlphaFunc GL_GEQUAL 0.99)
-   (glEnable GL_ALPHA_TEST)
+   ;(glActiveTextureARB GL_TEXTURE_0_ARB)
+
+   ; draw
+   ;#|
+   ;(glDisable GL_TEXTURE_2D)
+   (glEnable GL_LIGHTING)
+   (glLightfv GL_LIGHT0 GL_POSITION (list x y z 1))
 
    (DrawScene)
 
+   (glMatrixMode GL_TEXTURE)
+   (glPopMatrix)
+
+   ; draw the point
    (glDisable GL_TEXTURE_2D)
- 
-   (glDisable GL_TEXTURE_GEN_S)
-   (glDisable GL_TEXTURE_GEN_T)
-   (glDisable GL_TEXTURE_GEN_R)
-   (glDisable GL_TEXTURE_GEN_Q)
- 
    (glDisable GL_LIGHTING)
-   (glDisable GL_ALPHA_TEST)
+   (glPointSize 5)
+   (glBegin GL_POINTS)
+   (glColor3f #xff/255 #xbf/255 0)
+   (glVertex3f x y z)
+   (glEnd)
 
    #||#
 
-;  OLD: regular 
-   ;; (glViewport 0 0 854 480)
-
-   ;; (glMatrixMode GL_PROJECTION)
-   ;; (glLoadIdentity)
-   ;; (gluPerspective 45 (/ 854 480) 0.1 100)
-
-
-   ;; (glMatrixMode GL_MODELVIEW)
-   ;; (glLoadIdentity)
-   ;; (gluLookAt 6 9 6
-   ;;    1 3 0
-   ;;    0 1 0)
-
-   ;; ; show lighting point
-   ;; (let*((ss ms (clock))
-   ;;       (x 0) ;(* 3 (sin (+ ss (/ ms 1000)))))
-   ;;       (y 4) ;(+ 4 (* 3 (sin (/ (+ ss (/ ms 1000)) 8)))))
-   ;;       (z 3));(* 3 (cos (+ ss (/ ms 1000))))))
-   ;;    (glDisable GL_LIGHTING)
-   ;;    (glPointSize 5)
-   ;;    (glBegin GL_POINTS)
-   ;;    (glColor3f #xff/255 #xbf/255 0)
-   ;;    (glVertex3f x y z)
-   ;;    (glEnd)
-
-   ;;    (glEnable GL_LIGHTING)
-   ;;    (glLightfv GL_LIGHT0 GL_POSITION (list x y z 0)))
-
-   ;; (DrawScene)
-
-))
+)))
