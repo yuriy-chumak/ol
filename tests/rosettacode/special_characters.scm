a9cf750d9913b93193fe90eb202387f3d631651e
Yuriy Chumak <yuriy.chumak@comodo.com>
2018-10-10 21:18:47 +0300

    more rosetacode

---
+++ tests/rosettacode/special_characters.scm
@@ -0,0 +1,8 @@
+; https://rosettacode.org/wiki/Special_characters#Ol
+
+(print #\|) (print #\#)
+(define |I'm the ,`stra[]ge symbol :))| 123)
+ 
+(print (put #empty '|I'm the ,`stra[]ge symbol :))| 444))
+ 
+(print (+ |I'm the ,`stra[]ge symbol :))| 17))
