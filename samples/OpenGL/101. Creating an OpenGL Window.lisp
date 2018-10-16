aa64c02d77deeff213d07e41d5c439bc2b7d4295
Yuriy Chumak <yuriy.chumak@mail.com>
2018-10-16 18:07:17 +0300

    typos

--- samples/OpenGL/101. Creating an OpenGL Window.lisp
+++ samples/OpenGL/101. Creating an OpenGL Window.lisp
@@ -16,7 +16,7 @@
 ; no more steps required,
 ; just wait for window closing
 
-; BTW, you can start ol session, execute this file using ',load "this-file-name"' command
+; BTW, you can start ol session, execute this file using ',load "101. Creating an OpenGL Window.lisp"' command
 ; and still have interactive console (repl) while OpenGL window renders itself in background.
 ; Just type '(glClearColor 1 0 0 1)' and you will see that OpenGL window immediately will
-; change the background color. Cool, yeh?
+; change the background color. Cool, yeah?
