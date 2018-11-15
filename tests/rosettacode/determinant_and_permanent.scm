2286c64c5cdd5dfb77e18dc4f922b8da0517c80f
Yuriy Chumak <yuriy.chumak@mail.com>
2018-11-15 17:01:10 +0200

    rosettacode updated

--- tests/rosettacode/determinant_and_permanent.scm
+++ tests/rosettacode/determinant_and_permanent.scm
@@ -7,31 +7,50 @@
       (map exclude1
          matrix (repeat i (length matrix)))
       j))
- 
-; det calculator
-(define (det matrix)
+
+; superfunction for determinant and permanent
+(define (super matrix math)
    (let loop ((n (length matrix)) (matrix matrix))
       (if (eq? n 1)
          (caar matrix)
          (fold (lambda (x a j)
-                  (+ x (* a (lref '(-1 1) (mod j 2)) (det (rest matrix j 1)))))
+                  (+ x (* a (lref math (mod j 2)) (super (rest matrix j 1) math))))
             0
             (car matrix)
             (iota n 1)))))
- 
+
+
+; det/per calculators
+(define (det matrix) (super matrix '(-1 1)))
+(define (per matrix) (super matrix '( 1 1)))
+
 ; ---=( testing )=---------------------
 (print (det '(
    (1 2)
    (3 4))))
 ; ==> -2
- 
+
+(print (per '(
+   (1 2)
+   (3 4))))
+; ==> 10
+
+
 (print (det '(
    ( 1  2  3  1)
    (-1 -1 -1  2)
    ( 1  3  1  1)
    (-2 -2  0 -1))))
 ; ==> 26
- 
+
+(print (per '(
+   ( 1  2  3  1)
+   (-1 -1 -1  2)
+   ( 1  3  1  1)
+   (-2 -2  0 -1))))
+; ==> -10
+
+
 (print (det '(
    ( 0  1  2  3  4)
    ( 5  6  7  8  9)
@@ -39,3 +58,11 @@
    (15 16 17 18 19)
    (20 21 22 23 24))))
 ; ==> 0
+
+(print (per '(
+   ( 0  1  2  3  4)
+   ( 5  6  7  8  9)
+   (10 11 12 13 14)
+   (15 16 17 18 19)
+   (20 21 22 23 24))))
+; ==> 6778800
