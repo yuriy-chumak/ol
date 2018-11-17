393bc01f5a97677f719a70b50e01627228b2db3d
Yuriy Chumak <yuriy.chumak@comodo.com>
2018-11-17 13:13:48 +0200

    new lib - ini parser

---
+++ libraries/file/ini.scm
@@ -0,0 +1,68 @@
+(define-library (file ini)
+(import (otus lisp)
+      (only (lang intern) string->symbol)
+      (owl parse))
+(export
+   ini-parse-file)
+(begin
+
+   (define (whitespace? x)
+      (has? '(#\tab #\space #\newline #\return) x))
+
+   (define skip-whitespaces
+      (get-kleene* (get-rune-if whitespace?)))
+
+   (define get-rest-of-line
+      (let-parses
+         ((chars (get-kleene* (get-byte-if (lambda (x) (not (has? '( #\newline #\return) x))))))
+         (skip  (get-kleene+ (get-byte-if (lambda (x) (has? '( #\newline #\return) x))))))
+         chars))
+
+   (define (between? lo x hi) ; fast version of (<= lo x hi)), where x is rune
+      (and (or (less? lo x) (eq? lo x))
+         (or (less? x hi) (eq? x hi))))
+
+   (define (character? n) (or
+      (between? #\a n #\z)
+      (between? #\0 n #\9)
+      (between? #\A n #\Z)))
+      ;(between? #x7F n #xD7FF)
+      ;(between? #xE000 n #xFFFD)
+      ;(between? #x10000 n #x10FFFF)))
+
+   (define get-keyvalue
+      (let-parses (
+            (key (get-kleene+ (get-rune-if character?)))
+            (* (get-imm #\=))
+            (value get-rest-of-line)
+            (* skip-whitespaces))
+         (cons
+            (string->symbol (bytes->string key))
+            (bytes->string value))))
+
+
+   (define get-section
+      (let-parses (
+            (* (get-imm #\[))
+            (name (get-kleene+ (get-rune-if character?)))
+            (* (get-imm #\]))
+            (* skip-whitespaces)
+            (pairs (get-kleene* get-keyvalue))
+            (* skip-whitespaces))
+         (cons
+            (string->symbol (bytes->string name))
+            (list->ff pairs))))
+
+   (define ini-parser
+      (let-parses (
+            (sections (get-greedy+ get-section))
+            (* skip-whitespaces))
+         sections))
+
+   (define (ini-parse-file filename)
+      (let ((file (open-input-file filename)))
+         (if file
+            (let ((o (try-parse ini-parser (port->byte-stream file) filename "ini parse error" #false)))
+               (if o o
+                  (begin (close-port file) #false)))))) ; no automatic port closing on error
+))
