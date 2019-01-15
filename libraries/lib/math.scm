e9759273e5273bf35066c07b44dd3e3f2d3ebd44
Yuriy Chumak <yuriy.chumak@mail.com>
2019-01-15 19:12:26 +0200

    more math in lib/math.

--- libraries/lib/math.scm
+++ libraries/lib/math.scm
@@ -1,20 +1,31 @@
 (define-library (lib math)
-   (import (scheme core)
-           (otus ffi))
+   (import
+      (scheme core)
+      (otus ffi))
    (export
-      sin cos sqrt
-)
+      exp log sin cos tan
+      asin acos atan sqrt)
 
 (begin
-
    (define libm (or (load-dynamic-library "libm.so.6")
                     (load-dynamic-library "ntdll.dll")
                     (runtime-error "Can't load libm" #f)))
 
-   ; sin / cos
+   (define exp (libm fft-double "exp" fft-double))
+   (define log (libm fft-double "log" fft-double))
    (define sin (libm fft-double "sin" fft-double))
    (define cos (libm fft-double "cos" fft-double))
-
-   ; sqrt
+   (define tan (libm fft-double "tan" fft-double))
+   (define asin (libm fft-double "asin" fft-double))
+   (define acos (libm fft-double "acos" fft-double))
+   (define atan (libm fft-double "atan" fft-double))
+      ; procedure: atan y x
    (define sqrt (libm fft-double "sqrt" fft-double))
+      ; procedure: expt z1 z2
+      ; procedure: make-rectangular x1 x2
+      ; procedure: make-polar x3 x4
+      ; procedure: real-part z
+      ; procedure: imag-part z
+      ; procedure: magnitude z
+      ; procedure: angle z
 ))
