5f13b911bebad15dc1d9e24648bd61758e1b0fea
Yuriy Chumak <yuriy.chumak@comodo.com>
2019-02-07 11:26:54 +0200

    more rosettacode

--- tests/rosettacode/pythagorean_triples.scm
+++ tests/rosettacode/pythagorean_triples.scm
@@ -2,7 +2,7 @@
 (define (euclid-formula max)
    (let loop ((a 3) (b 4) (c 5) (tail #null))
       (if (<= (+ a b c) max)
-         (cons (tuple a b c) (lambda ()
+         (cons (list a b c) (lambda ()
             (let ((d (- b)) (z (- a)))
             (loop (+ a d d c c) (+ a a d c c) (+ a a d d c c c) (lambda ()
                (loop (+ a b b c c) (+ a a b c c) (+ a a b b c c c) (lambda ()
@@ -20,7 +20,7 @@
          (else
             (let ((triple (car ll)))
                (loop (+ p 1) (+ t (div max (apply + triple)))
-               (cdr ll)))))))
+                  (cdr ll)))))))
  
 ; print values for 10..100000
 (for-each (lambda (max)
