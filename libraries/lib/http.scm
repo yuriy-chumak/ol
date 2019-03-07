ae514e74eff800056ec7daeba606d1cb45270fc1
Yuriy Chumak <yuriy.chumak@mail.com>
2019-03-07 11:32:03 +0200

    http library updated (according to latest sexp parser changes)

--- libraries/lib/http.scm
+++ libraries/lib/http.scm
@@ -7,9 +7,7 @@
   (import (scheme base) (scheme srfi-1)
       (owl parse) (owl vector)
       (owl math) (owl list) (owl io) (owl string) (owl ff) (owl list-extra) (owl interop)
-      (only (lang intern) string->symbol)
-      (only (lang sexp) fd->exp-stream)
-     )
+      (only (lang intern) string->symbol))
 
 (begin
    (define (upper-case? x) (<= #\A x #\Z))
@@ -37,154 +35,154 @@
 (define (timestamp) (syscall 201 "%c" #f #f))
 (define (set-ticker-value n) (syscall 1022 n #false #false))
 
+; -------------------------------------------
 ; http parser
-   (define (syntax-fail pos info lst)
-      (print-to stderr "http parser fail: " info)
-      ;(print-to stderr ">>> " pos "-" (runes->string lst) " <<<")
-      '(() (())))
 
-      (define HT #\tab)      ; 9
-      (define CTL (append (iota 32) '(127))) ; 0 .. 31, 127
-      (define CR #\return)   ; 13
-      (define LF #\newline)  ; 10
-      (define SP #\space)    ; 32
-      (define tspecials '(#\( #\) #\< #\> #\@ #\, #\; #\: #\\ #\" #\/ #\[ #\] #\? #\= #\{ #\} #\space #\tab))
+(define HT #\tab)      ; 9
+(define CTL (append (iota 32) '(127))) ; 0 .. 31, 127
+(define CR #\return)   ; 13
+(define LF #\newline)  ; 10
+(define SP #\space)    ; 32
+(define tspecials '(#\( #\) #\< #\> #\@ #\, #\; #\: #\\ #\" #\/ #\[ #\] #\? #\= #\{ #\} #\space #\tab))
 
-      (define (token-char? x)
-         (and (less? 31 x) (not (eq? 127 x))    ; not CTL
-              (not (has? tspecials x))))
+(define (token-char? x)
+   (and (less? 31 x) (not (eq? 127 x))    ; not CTL
+         (not (has? tspecials x))))
 
-      (define (a-z? x)
-         (<= #\a x #\z))
-      (define (A-Z? x)
-         (<= #\A x #\Z))
-      (define (uri? x)
-         (or (<= #\a x #\z)
-             (<= #\A x #\Z)
-             (<= #\0 x #\9)
-             (has? '(#\- #\. #\_ #\~ #\: #\/ #\? #\# #\@ #\! #\$ #\& #\' #\( #\) #\* #\+ #\, #\; #\= #\. #\%) x)
-             (has? '(#\{ #\} #\| #\\ #\^ #\[ #\] #\`) x))) ;unwise characters are allowed but may cause problems
-      (define (xml? x)
-         (or (<= #\0 x #\9)
-             (<= #\A x #\Z)
-             (<= #\a x #\z)
-             (has? '(#\< #\/ #\> #\-) x)))
+(define (a-z? x)
+   (<= #\a x #\z))
+(define (A-Z? x)
+   (<= #\A x #\Z))
+(define (uri? x)
+   (or (<= #\a x #\z)
+         (<= #\A x #\Z)
+         (<= #\0 x #\9)
+         (has? '(#\- #\. #\_ #\~ #\: #\/ #\? #\# #\@ #\! #\$ #\& #\' #\( #\) #\* #\+ #\, #\; #\= #\. #\%) x)
+         (has? '(#\{ #\} #\| #\\ #\^ #\[ #\] #\`) x))) ;unwise characters are allowed but may cause problems
+(define (xml? x)
+   (or (<= #\0 x #\9)
+         (<= #\A x #\Z)
+         (<= #\a x #\z)
+         (has? '(#\< #\/ #\> #\-) x)))
 
 
-      (define (space-char? x) (= x #\space))
+(define (space-char? x) (= x #\space))
 
-      (define (digit-char? x)
-         (or (<= #\0 x #\9)
-             (<= #\A x #\F)
-             (<= #\a x #\f)))
-      (define (alpha-char? x)
-         (or (<= #\0 x #\9)
-             (<= #\A x #\Z)
-             (<= #\a x #\z)))
+(define (digit-char? x)
+   (or (<= #\0 x #\9)
+         (<= #\A x #\F)
+         (<= #\a x #\f)))
+(define (alpha-char? x)
+   (or (<= #\0 x #\9)
+         (<= #\A x #\Z)
+         (<= #\a x #\z)))
 
-      (define (header-char? x)
-         (or (<= #\A x #\Z)
-             (<= #\a x #\z)
-             (has? '(#\-) x)))
+(define (header-char? x)
+   (or (<= #\A x #\Z)
+         (<= #\a x #\z)
+         (has? '(#\-) x)))
 
-      ;(define (separator-char?))
+;(define (separator-char?))
 
-      (define digit-values
-         (list->ff
-            (foldr append null
-               (list
-                  (map (lambda (d) (cons d (- d 48))) (lrange 48 1 58))  ;; 0-9
-                  (map (lambda (d) (cons d (- d 55))) (lrange 65 1 71))  ;; A-F
-                  (map (lambda (d) (cons d (- d 87))) (lrange 97 1 103)) ;; a-f
-                  ))))
+(define digit-values
+   (list->ff
+      (foldr append null
+         (list
+            (map (lambda (d) (cons d (- d 48))) (lrange 48 1 58))  ;; 0-9
+            (map (lambda (d) (cons d (- d 55))) (lrange 65 1 71))  ;; A-F
+            (map (lambda (d) (cons d (- d 87))) (lrange 97 1 103)) ;; a-f
+            ))))
 
-      (define hex-table
-         (list->ff (fold append '() (list
-            (zip cons (iota 10 #\0) (iota 10 0))     ; 0-9
-            (zip cons (iota  6 #\a) (iota 6 10))     ; a-f
-            (zip cons (iota  6 #\A) (iota 6 10)))))) ; A-F
-
-
-      (define get-rest-of-line
-         (let-parses
-            ((chars (get-greedy* (get-byte-if (lambda (x) (not (eq? x #\newline))))))
-             (skip (get-imm #\newline))) ;; <- note that this won't match if line ends to eof
-            chars))
-      (define get-a-whitespace
-         (get-any-of
-            ;get-hashbang   ;; actually probably better to make it a symbol as above
-            (get-byte-if (lambda (x) (has? '(#\space #\tab #\newline #\return) x)))
-            (let-parses
-               ((skip (get-imm #\;))
-                (skip get-rest-of-line))
-               'comment)))
-      (define maybe-whitespace (get-kleene* get-a-whitespace))
-
-      (define get-request-line
-         (let-parses ((method  (get-greedy+ (get-rune-if A-Z?))) ; GET/POST/etc.
-                      (-          (get-imm #\space))
-                      (uri     (get-greedy+ (get-rune-if uri?))) ; URI
-                      (-          (get-imm #\space))
-                      (version (get-greedy+ (get-rune-if uri?))) ; HTTP/x.x
-                      (-          (get-imm #\return)) (skip (get-imm #\newline))) ; end of request line
-            (tuple
-               (runes->string method)
-               (runes->string uri)
-               (runes->string version))))
-
-      (define get-header-line
-         (let-parses ((name  (get-greedy+ (get-rune-if token-char?)))
-                      (-        (get-imm #\:))
-                      (-        (get-rune-if space-char?)) ; todo: should be LWS
-                      (value (get-greedy+ (get-rune-if (lambda (x) (not (eq? x CR)))))) ; until CRLF
-                      (- (get-imm CR)) (- (get-imm LF)))   ; CRLF
-            (cons
-               (string->symbol (runes->string name))
-               (runes->string value))))
+(define hex-table
+   (list->ff (fold append '() (list
+      (zip cons (iota 10 #\0) (iota 10 0))     ; 0-9
+      (zip cons (iota  6 #\a) (iota 6 10))     ; a-f
+      (zip cons (iota  6 #\A) (iota 6 10)))))) ; A-F
 
 
-      (define http-parser
-         (let-parses ((request-line get-request-line)
-                      (headers-array (get-greedy* get-header-line))
-                      (- (get-imm CR)) (- (get-imm LF)))   ; final CRLF
-            (tuple
-               request-line (list->ff headers-array))))
+(define get-rest-of-line
+   (let-parses
+      ((chars (get-greedy* (get-byte-if (lambda (x) (not (eq? x #\newline))))))
+         (skip (get-imm #\newline))) ;; <- note that this won't match if line ends to eof
+      chars))
+(define get-a-whitespace
+   (get-any-of
+      ;get-hashbang   ;; actually probably better to make it a symbol as above
+      (get-byte-if (lambda (x) (has? '(#\space #\tab #\newline #\return) x)))
+      (let-parses
+         ((skip (get-imm #\;))
+            (skip get-rest-of-line))
+         'comment)))
+(define maybe-whitespace (get-kleene* get-a-whitespace))
+
+(define get-request-line
+   (let-parses ((method  (get-greedy+ (get-rune-if A-Z?))) ; GET/POST/etc.
+                  (-          (get-imm #\space))
+                  (uri     (get-greedy+ (get-rune-if uri?))) ; URI
+                  (-          (get-imm #\space))
+                  (version (get-greedy+ (get-rune-if uri?))) ; HTTP/x.x
+                  (-          (get-imm #\return)) (skip (get-imm #\newline))) ; end of request line
+      (tuple
+         (runes->string method)
+         (runes->string uri)
+         (runes->string version))))
+
+(define get-header-line
+   (let-parses ((name  (get-greedy+ (get-rune-if token-char?)))
+                  (-        (get-imm #\:))
+                  (-        (get-rune-if space-char?)) ; todo: should be LWS
+                  (value (get-greedy+ (get-rune-if (lambda (x) (not (eq? x CR)))))) ; until CRLF
+                  (- (get-imm CR)) (- (get-imm LF)))   ; CRLF
+      (cons
+         (string->symbol (runes->string name))
+         (runes->string value))))
+
+
+(define http-parser
+   (let-parses ((request-line get-request-line)
+                  (headers-array (get-greedy* get-header-line))
+                  (- (get-imm CR)) (- (get-imm LF)))   ; final CRLF
+      (tuple
+         request-line (list->ff headers-array))))
 ;)
 
-
+; ----
 ; todo: use atomic counter to check count of clients and do appropriate timeout on select
 ;  (1 for count > 0, 100 for count = 0)
-
 (define (on-accept id fd onRequest)
 (lambda ()
-   (print "an-accept " id)
-   (let*((ss1 ms1 (clock))
-         (send (lambda args
-                  (for-each (lambda (arg)
-                     (display-to fd arg)) args) #t)))
+   (define (ok data fail val pos)
+      ;(print-to stderr "request: " val)
+      ;(print-to stderr "data: " data)
+      (values val data))
+   (define (fail pos reason)
+      (print-to stderr "http parser fail: \e[0;31m" reason "\e[0;0m")
+      (values #false #null))
+   (define (send . args)
+      (for-each (lambda (arg)
+         (display-to fd arg)) args) #t)
 
-      (let loop ((request (fd->exp-stream fd "" http-parser syntax-fail)))
-         ;(print id " loop:" request)
-         (if (call/cc (lambda (close)
-                         (if (null? request)
-                            #t
-                            (let ((Request-Line (ref (car request) 1))
-                                  (Headers-Line (ref (car request) 2)))
-                               (print id " Request-Line: \e[0;34m" Request-Line "\e[0;0m")
-                               (print id " Headers-Line: " Headers-Line)
-                               (if (null? Request-Line)
-                                  (close (send "HTTP/1.0 400 Bad Request\r\n\r\n400"))
-                                  (onRequest fd Request-Line Headers-Line send close))
-                            (print "ok.")
-                            #f)) ))
-            (begin
-               (display id)
-               (display (if (syscall 3 fd #f #f) " socket closed, " " can't close socket, ")))
-            ;else
-            (loop (force (cdr request)))))
-      (print "on-accept done." )
+   (print "on-accept :" id)
+   (let*((ss1 ms1 (clock)))
+      (let loop ((stream (port->byte-stream fd)))
+         ;(print "stream: " stream)
+         (let* ((request stream (http-parser stream ok fail 0)))
+            (if request
+               (unless (call/cc (lambda (close)
+                              (let ((Request-Line (ref request 1))
+                                    (Headers-Line (ref request 2)))
+                                 (print id ": Request-Line: \e[0;34m" Request-Line "\e[0;0m")
+                                 (print id ": Headers-Line: " Headers-Line)
+                                 (if (null? Request-Line)
+                                    (close (send "HTTP/1.0 400 Bad Request\r\n\r\n400"))
+                                    (onRequest fd Request-Line Headers-Line send close))
+                                 (print "ok."))
+                              #false))
+                  (loop stream)))))
+      (print id (if (syscall 3 fd #f #f) ": socket closed" ": can't close socket"))
+      (print "on-accept :" id " done.")
       (let*((ss2 ms2 (clock)))
-         (print id " # " (timestamp) ": request processed in "  (+ (* (- ss2 ss1) 1000) (- ms2 ms1)) "ms.")))
+         (print "# " (timestamp) ": request " id " processed in "  (+ (* (- ss2 ss1) 1000) (- ms2 ms1)) "ms.")))
 ))
 
 
