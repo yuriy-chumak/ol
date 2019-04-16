88fe0618de1e65670065f390cdc1ed914ce5bc0c
Yuriy Chumak <yuriy.chumak@mail.com>
2019-04-16 12:51:19 +0300

    Added srfi-8 with (receive) macro

---
+++ libraries/scheme/srfi-8.scm
@@ -0,0 +1,20 @@
+(define-library (scheme srfi-8)
+; http://srfi.schemers.org/srfi-8/srfi-8.html
+
+; Title
+
+; Abstract
+;
+; -----
+(export
+   receive)
+(import
+   (scheme core))
+
+(begin
+   (define-syntax receive
+      (syntax-rules ()
+         ((receive formals expression body ...)
+            (call-with-values (lambda () expression)
+                              (lambda formals body ...)))))
+))
