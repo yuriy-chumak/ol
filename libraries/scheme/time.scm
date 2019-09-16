6f7697db0adde58efd509ed2a5bf0fc44578bcf9
Yuriy Chumak <yuriy.chumak@mail.com>
2019-09-17 00:28:29 +0300

    more (scheme time) functions

--- libraries/scheme/time.scm
+++ libraries/scheme/time.scm
@@ -1,10 +1,23 @@
 (define-library (scheme time)
    (export 
-      (exports (scheme core)) )
+      current-jiffy
+      current-second
+      jiffies-per-second
+      
+   )
 
    (import
       (scheme core))
 
    (begin
-      #true
+
+      (define (current-jiffy)
+         (cdr (syscall 96)))
+
+      (define (current-second)
+         (car (syscall 96)))
+
+      (define (jiffies-per-second)
+         1000000)
+      
 ))
