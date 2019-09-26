c3fbb296b1adef4153e9a64d98ef830160b3e373
Yuriy Chumak <yuriy.chumak@mail.com>
2019-09-26 10:46:04 +0300

    more scheme libraries

--- libraries/scheme/file.scm
+++ libraries/scheme/file.scm
@@ -1,10 +1,24 @@
 (define-library (scheme file)
-   (export 
-      (exports (scheme core)) )
+   (version 1.0)
+   (license MIT/LGPL3)
+   (keywords (scheme file))
+   (description "
+      Scheme file library.")
 
    (import
       (scheme core))
 
+   (export 
+      file-exists?
+      delete-file)
+
    (begin
-      #true
+
+      (define (file-exists? filename)
+         (if (syscall 4 (c-string filename))
+            #true))
+
+      (define (delete-file filename)
+         (syscall 87 (c-string filename)))
+
 ))
