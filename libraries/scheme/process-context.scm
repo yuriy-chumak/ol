debcd1502f4cd9b816f33b28942fa607970835af
Yuriy Chumak <yuriy.chumak@mail.com>
2019-09-26 10:47:21 +0300

    more scheme libraries

--- libraries/scheme/process-context.scm
+++ libraries/scheme/process-context.scm
@@ -1,10 +1,35 @@
 (define-library (scheme process-context)
    (export 
-      (exports (scheme core)) )
+      command-line
+      exit
+      emergency-exit
+      get-environment-variable
+      get-environment-variables
+      )
 
    (import
       (scheme core))
 
    (begin
-      #true
+      (define (command-line)
+         *vm-args*)
+
+      (define exit (case-lambda
+         ((code)
+            (vm:exit (case code
+               (#true 0)
+               (#false -1)
+               (else code))))
+         (()
+            (vm:exit 0))))
+      
+      (define emergency-exit exit)
+
+      (define (get-environment-variable name)
+         (let ((name (c-string name)))
+            (if name
+               (syscall 1016 name))))
+
+      (define (get-environment-variables)
+         (runtime-error 'get-environment-variables #f))
 ))
--- libraries/scheme/time.scm
+++ libraries/scheme/time.scm
@@ -1,13 +1,20 @@
+; 
 (define-library (scheme time)
+   (version 1.0)
+   (license MIT/LGPL3)
+   (keywords (scheme time))
+   (description "
+      Scheme time library.")
+
+   (import
+      (scheme core))
+
    (export 
       current-jiffy
       current-second
       jiffies-per-second
       
    )
-
-   (import
-      (scheme core))
 
    (begin
 
