27a9b6c4e7ed03169bc3737d8ac9968dc5ce69af
Yuriy Chumak <yuriy.chumak@mail.com>
2019-10-30 22:27:30 +0200

    more rosettacode

---
+++ tests/rosettacode/currying.scm
@@ -0,0 +1,8 @@
+; http://rosettacode.org/wiki/Currying#Ol
+(define (addN n)
+   (lambda (x) (+ x n)))
+
+(let ((add10 (addN 10))
+      (add20 (addN 20)))
+   (print "(add10 4) ==> " (add10 4))
+   (print "(add20 4) ==> " (add20 4)))
