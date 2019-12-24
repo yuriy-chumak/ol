82812eb4294059cbf3ccf5de6a0202cf07dc8382
Yuriy Chumak <yuriy.chumak@mail.com>
2019-12-24 13:50:09 +0200

    more rosettacode

---
+++ tests/rosettacode/huffman_coding.scm
@@ -0,0 +1,53 @@
+; https://www.rosettacode.org/wiki/Huffman_coding#Ol
+
+(define phrase "this is an example for huffman encoding")
+
+; prepare initial probabilities table
+(define table (ff->list
+   (fold (lambda (ff x)
+            (put ff x (+ (ff x 0) 1)))
+      {}
+      (string->runes phrase))))
+
+; just sorter...
+(define (resort l)
+   (sort (lambda (x y) (< (cdr x) (cdr y))) l))
+; ...to sort table
+(define table (resort table))
+
+; build huffman tree
+(define tree
+   (let loop ((table table))
+      (if (null? (cdr table))
+         (car table)
+         (loop (resort (cons
+            (cons
+               { 1 (car table) 0 (cadr table)}
+               (+ (cdar table) (cdadr table)))
+            (cddr table)))))))
+
+; huffman codes
+(define codes
+   (map (lambda (i)
+         (call/cc (lambda (return)
+            (let loop ((prefix #null) (tree tree))
+               (if (ff? (car tree))
+                  (begin
+                     (loop (cons 0 prefix) ((car tree) 0))
+                     (loop (cons 1 prefix) ((car tree) 1)))
+                  (if (eq? (car tree) i)
+                     (return (reverse prefix))))))))
+      (map car table)))
+
+; debug print:
+; --------------------------------------------
+(print "weights: ---------------------------")
+(for-each (lambda (ch)
+      (print (string (car ch)) ": " (cdr ch)))
+   (reverse table))
+
+(print "codes: -----------------------------")
+(map (lambda (char code)
+      (print (string char) ": " code))
+   (reverse (map car table))
+   (reverse codes))
