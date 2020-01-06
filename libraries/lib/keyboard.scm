01a431626ac1971c09ebc0c8c7429003cf32d422
Yuriy Chumak <yuriy.chumak@mail.com>
2020-01-06 16:15:15 +0200

    more keys in keyboard lib

--- libraries/lib/keyboard.scm
+++ libraries/lib/keyboard.scm
@@ -9,6 +9,8 @@
    KEY_ENTER KEY_ESC
    KEY_LEFTCTRL KEY_LEFTALT KEY_LEFTSHIFT
    KEY_UP KEY_DOWN KEY_LEFT KEY_RIGHT
+   KEY_1 KEY_2 KEY_3 KEY_4 KEY_5 KEY_6 KEY_7 KEY_8 KEY_9 KEY_0
+   KEY_MINUS KEY_EQUAL KEY_BACKSPACE KEY_TAB
 
    ;vkQ vkW vkE vkR vkT vkY vkU vkI vkO vkP
    ;vkA vkS vkD vkF vkG vkH vkJ vkK vkL
@@ -31,6 +33,11 @@
          (define KEY_ENTER #x0D) (define KEY_ESC #x1B)
          (define KEY_LEFTCTRL #x11) (define KEY_LEFTALT #x12) (define KEY_LEFTSHIFT #x10)
          (define KEY_UP #x26) (define KEY_DOWN #x28) (define KEY_LEFT #x25) (define KEY_RIGHT #x27)
+
+         (define KEY_1 #x31) (define KEY_2 #x32) (define KEY_3 #x33) (define KEY_4 #x34) (define KEY_5 #x35)
+         (define KEY_6 #x36) (define KEY_7 #x37) (define KEY_8 #x38) (define KEY_9 #x39) (define KEY_0 #x30)
+         (define KEY_MINUS #xBD) (define KEY_EQUAL #xBB) (define KEY_BACKSPACE #x08) (define KEY_TAB #x09)
+
          ;; (define vkQ 24) (define vkW 25) (define vkE 26) (define vkR 27) (define vkT 28) (define vkY 29) (define vkU 30) (define vkI 31) (define vkO 32) (define vkP 33)
          ;; (define vkA 38) (define vkS 39) (define vkD 40) (define vkF 41) (define vkG 42) (define vkH 43) (define vkJ 44)(define vkK 45) (define vkL 46)
          ;; (define vkZ 52) (define vkX 53) (define vkC 54) (define vkV 55) (define vkB 56) (define vkN 57) (define vkM 58)
@@ -59,6 +66,11 @@
          (define KEY_ENTER #xff52) (define KEY_ESC #xff1b)
          (define KEY_LEFTCTRL #xffe3) (define KEY_LEFTALT #xffe9) (define KEY_LEFTSHIFT #xffe1)
          (define KEY_UP #xff52) (define KEY_DOWN #xff54) (define KEY_LEFT #xff51) (define KEY_RIGHT #xff53)
+
+         (define KEY_1 #x02) (define KEY_2 #x03) (define KEY_3 #x04) (define KEY_4 #x05) (define KEY_5 #x06)
+         (define KEY_6 #x07) (define KEY_7 #x08) (define KEY_8 #x09) (define KEY_9 #x0A) (define KEY_0 #x0B)
+         (define KEY_MINUS #x0C) (define KEY_EQUAL #x0D) (define KEY_BACKSPACE #x0E) (define KEY_TAB #x0F)
+
          ;; (define vkQ 24) (define vkW 25) (define vkE 26) (define vkR 27) (define vkT 28) (define vkY 29) (define vkU 30) (define vkI 31) (define vkO 32) (define vkP 33)
          ;; (define vkA 38) (define vkS 39) (define vkD 40) (define vkF 41) (define vkG 42) (define vkH 43) (define vkJ 44)(define vkK 45) (define vkL 46)
          ;; (define vkZ 52) (define vkX 53) (define vkC 54) (define vkV 55) (define vkB 56) (define vkN 57) (define vkM 58)
