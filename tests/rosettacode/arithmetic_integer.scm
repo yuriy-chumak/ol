f3ce343e6cdc6e59748325f7da9c6c0a379a35a1
Yuriy Chumak <yuriy.chumak@mail.com>
2020-07-11 09:34:24 +0300

    more rosettacode

--- tests/rosettacode/arithmetic_integer.scm
+++ tests/rosettacode/arithmetic_integer.scm
@@ -1,4 +1,4 @@
-; 
+; http://www.rosettacode.org/wiki/Arithmetic/Integer#Ol
 (define a 8)
 (define b 12)
 
@@ -11,11 +11,12 @@
 (print "(remainder " a " " b ") => " (rem  a b)) ; same as (remainder a b)
 (print "(modulo " a " " b ") => "    (mod  a b)) ; same as (modulo a b)
 
-(import (owl math-extra))
 (print "(expt " a " " b ") => " (expt a b))
+(print "(gcd " a " " b ") => " (gcd a b))
+(print "(lcm " a " " b ") => " (lcm a b))
 
 ; you can use more than two arguments for +,-,*,/ functions
-(print (+ 1 3 5 7 9)) ; ==> 25
-(print (- 1 3 5 7 9)) ; ==> -23
-(print (* 1 3 5 7 9)) ; ==> 945 - same as (1*3*5*7*9)
-(print (/ 1 3 5 7 9)) ; ==> 1/945 - same as (((1/3)/5)/7)/9
+(print (+ 1 3 5 7 9))
+(print (- 1 3 5 7 9))
+(print (* 1 3 5 7 9)) ; same as (1*3*5*7*9)
+(print (/ 1 3 5 7 9)) ; same as (((1/3)/5)/7)/9
