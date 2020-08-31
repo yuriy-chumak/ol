057c4b5c7f7680ca9b62d80ae74cd8be4ae741a4
Yuriy Chumak <yuriy.chumak@mail.com>
2020-08-31 17:10:49 +0300

    backport from owl-lisp 'sort' function example

--- libraries/owl/sort.scm
+++ libraries/owl/sort.scm
@@ -104,4 +104,9 @@
             ((null? (cdr lst)) lst)
             (else (mergesort op lst))))
 
+      (assert (sort < '(1 4 2))    ===> '(1 2 4))
+
+      ;; sort stability test
+      (assert (sort (λ (a b) (< (>> a 1) (>> b 1)))
+         '(2 8 4 9 0 5 1 6 7 3 5)) ===> '(0 1 2 3 4 5 5 6 7 8 9))
 ))
