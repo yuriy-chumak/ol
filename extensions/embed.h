19a53badd1c3871104da3d44f9ff947d507b9010
Yuriy Chumak <yuriy.chumak@mail.com>
2021-01-07 18:51:23 +0200

    internal type cleanup

--- extensions/embed.h
+++ extensions/embed.h
@@ -33,7 +33,7 @@ typedef uintptr_t word;
 typedef struct ol_t
 {
 	struct olvm_t* vm;  // ol virtual machine instance
-	uintptr_t eval; // embed pinned 'eval' function id
+	word eval; // embed pinned 'eval' function id
 } ol_t;
 
 
@@ -43,80 +43,80 @@ typedef struct ol_t
 // ====================================================================
 
 //! returns not 0 if argument is value
-#define is_value(x) (((uintptr_t)(x)) & 2)
+#define is_value(x) (((word)(x)) & 2)
 
 //! returns not 0 if argument is reference
 #define is_reference(x) (!is_value(x))
 
 //! returns the type of provided ol value
-#define vtype(x) ({ uintptr_t p = (uintptr_t)(x);\
+#define vtype(x) ({ word p = (word)(x);\
 		assert (is_value(p) && "argument should be value");\
-		(unsigned char)((( (uintptr_t )(p)) >> 2) & 0x3F); })
+		(unsigned char)((( (word )(p)) >> 2) & 0x3F); })
 
 //! returns the type of provided ol reference
-#define reftype(x) ({ uintptr_t p = (uintptr_t)(x);\
+#define reftype(x) ({ word p = (word)(x);\
 		assert (is_reference(p) && "argument should be reference");\
-		(unsigned char)(((*(uintptr_t*)(p)) >> 2) & 0x3F); })
+		(unsigned char)(((*(word*)(p)) >> 2) & 0x3F); })
 
 //! returns not 0 if argument is a small number (type-enum+ or type-enum-)
-#define is_enum(x) ({ uintptr_t s = (uintptr_t)(x);\
+#define is_enum(x) ({ word s = (word)(x);\
 		is_value(s) ?\
 			vtype(s) == 0 || vtype(s) == 32\
 		: 0; })
 
 //! returns not 0 if argument is a number (type-int+ or type-int-)
-#define is_number(x) ({ uintptr_t n = (uintptr_t)(x);\
+#define is_number(x) ({ word n = (word)(x);\
 		is_enum(n) ? 1 \
 		: is_reference(n) ? \
 			reftype(n) == 40 || reftype(n) == 41\
 			: 0; })
 
 //! returns not 0 if argument is a string (type-string or type-string-wide)
-#define is_string(x) ({ uintptr_t s = (uintptr_t)(x);\
+#define is_string(x) ({ word s = (word)(x);\
 		is_reference(s) ?\
 			reftype(s) == 3 || reftype(s) == 5\
 		: 0; })
 //! returns length of ol string
-#define string_length(x) ({ uintptr_t o = (uintptr_t)(x);\
+#define string_length(x) ({ word o = (word)(x);\
 		assert (is_string(o) && "argument should be a string");\
-		(int)(((*(uintptr_t*)o >> 16) - 1) * sizeof(uintptr_t) -\
-		      ((*(uintptr_t*)o >> 8) & 7)); })
+		(int)(((*(word*)o >> 16) - 1) * sizeof(word) -\
+		      ((*(word*)o >> 8) & 7)); })
 //! returns address of ol string body, this is NOT null terminated string!
-#define string_value(x) ({ uintptr_t o = (uintptr_t)(x);\
+#define string_value(x) ({ word o = (word)(x);\
 		assert (is_string(o) && "argument should be a string");\
-		(const char*)(o+sizeof(uintptr_t)); })
+		(const char*)(o+sizeof(word)); })
 
 //! returns not 0 if argument is a bytevector (type-bytevector)
-#define is_bytevector(x) ({ uintptr_t s = (uintptr_t)(x);\
+#define is_bytevector(x) ({ word s = (word)(x);\
 		is_reference(s) ?\
 			reftype(s) == 19 : 0; })
 //! returns length of ol bytevector
-#define bytevector_length(x) ({ uintptr_t o = (uintptr_t)(x);\
+#define bytevector_length(x) ({ word o = (word)(x);\
 		assert (is_bytevector(o) && "argument should be a bytevector");\
-		(int)(((*(uintptr_t*)o >> 16) - 1) * sizeof(uintptr_t) -\
-		      ((*(uintptr_t*)o >> 8) & 7)); })
+		(int)(((*(word*)o >> 16) - 1) * sizeof(word) -\
+		      ((*(word*)o >> 8) & 7)); })
 //! return address of ol bytevector body
-#define bytevector_value(x) ({ uintptr_t o = (uintptr_t)(x);\
+#define bytevector_value(x) ({ word o = (word)(x);\
 		assert (is_bytevector(o) && "argument should be a bytevector");\
-		(const char*)(o+sizeof(uintptr_t)); })
+		(const char*)(o+sizeof(word)); })
 
 //! returns not 0 if argument is a cons (and maybe a list)
-#define is_pair(x) ({ uintptr_t s = (uintptr_t)(x);\
+#define is_pair(x) ({ word s = (word)(x);\
 		is_reference(s) ?\
 			reftype(s) == 1\
 		: 0; })
 
 //! converts ol small number into C signed integer
-#define ol2small(x) ({ uintptr_t m = (uintptr_t)(x);\
+#define ol2small(x) ({ word m = (word)(x);\
 		assert (is_enum(m) && "argument should be a small number");\
 		int v = m >> 8;\
 		(m & 0x80) ? -v : v;})
 
 //! converts OL number into C signed integer
-#define ol2int(x) ({ uintptr_t u = (uintptr_t)(x);\
+#define ol2int(x) ({ word u = (word)(x);\
 		assert (is_number(u) && "argument should be a number");\
 		is_enum(u) ? ol2small(u)\
-			: (uintptr_t)ol2small(car(u)) | (uintptr_t)ol2small(cadr(u)) << ((sizeof (uintptr_t) * 8) - 8)/*FBITS*/;})
+			: (word)ol2small(car(u)) | (word)ol2small(cadr(u)) << ((sizeof (word) * 8) - 8)/*FBITS*/;})
 
 //#pragma GCC diagnostic pop
 
@@ -129,7 +129,7 @@ typedef struct ol_t
 // "new_" prefix do the allocation (in olvm heap, sure).
 
 // same as lisp (ref object n), (car object) and (cdr object) functions
-#define ref(ob, n) (uintptr_t)(((uintptr_t*)(ob))[n+1]) // +1 is for header skipping
+#define ref(ob, n) (word)(((word*)(ob))[n+1]) // +1 is for header skipping
 #define car(ob) ref(ob, 0)
 #define cdr(ob) ref(ob, 1)
 
@@ -140,9 +140,9 @@ typedef struct ol_t
 #define cddr(o) cdr(cdr(o))
 
 // commonly used olvm constants, value type
-#define INULL  (uintptr_t)((2 << 8) | (13 << 2) | 2) // #null
-#define IFALSE (uintptr_t)((0 << 8) | (13 << 2) | 2) // #false
-#define ITRUE  (uintptr_t)((1 << 8) | (13 << 2) | 2) // #true
+#define INULL  (word)((2 << 8) | (13 << 2) | 2) // #null
+#define IFALSE (word)((0 << 8) | (13 << 2) | 2) // #false
+#define ITRUE  (word)((1 << 8) | (13 << 2) | 2) // #true
 
 // --------------------------------------------
 
@@ -275,11 +275,11 @@ word embed_eval(ol_t* embed, ...)
 	va_end(vl);
 
 	va_start(vl, embed);
-	uintptr_t* args = __builtin_alloca((count+1) * sizeof(uintptr_t)); // just one for sanity zero
+	word* args = __builtin_alloca((count+1) * sizeof(word)); // just one for sanity zero
 
 	args[0] = OL_deref(embed->vm, embed->eval); // deref right now, cause GC possibly moved the object
 	int i = 0;
-	while ((args[++i] = (uintptr_t)va_arg(vl, void*)) != 0) ;
+	while ((args[++i] = (word)va_arg(vl, void*)) != 0) ;
 	va_end(vl);
 
 	word r = OL_continue(embed->vm, i, (void**)args);
