f348a83d3e35f31e14f52f69fec2cdcda107c346
Yuriy Chumak <yuriy.chumak@mail.com>
2021-05-14 17:32:01 +0300

    big-endian dependent code precompiler fix

--- src/olvm.c
+++ src/olvm.c
@@ -5022,8 +5022,15 @@ word* deserialize(word *ptrs, int nobjs, unsigned char *bootstrap, word* fp)
 			int pads = words * W - size;//(W - (size % W));
 
 			unsigned char *p = (unsigned char*)&car(new (type, words, pads));
-			// неточные числа
-#if __BYTE_ORDER == __BIG_ENDIAN
+
+			// inexact numbers
+// is it a big-endian target architecture?
+#if defined(__BYTE_ORDER) && __BYTE_ORDER == __BIG_ENDIAN || \
+    defined(__BIG_ENDIAN__) || \
+    defined(__ARMEB__) || \
+    defined(__THUMBEB__) || \
+    defined(__AARCH64EB__) || \
+    defined(_MIBSEB) || defined(__MIBSEB) || defined(__MIBSEB__)
 #	ifdef OLVM_INEXACTS
 			if (type == TINEXACT) {
 				int s = size;
@@ -5033,6 +5040,16 @@ word* deserialize(word *ptrs, int nobjs, unsigned char *bootstrap, word* fp)
 			}
 			else
 #	endif
+// is it a little-endian target architecture?
+#elif defined(__BYTE_ORDER) && __BYTE_ORDER == __LITTLE_ENDIAN || \
+    defined(__LITTLE_ENDIAN__) || \
+    defined(__ARMEL__) || \
+    defined(__THUMBEL__) || \
+    defined(__AARCH64EL__) || \
+    defined(_MIPSEL) || defined(__MIPSEL) || defined(__MIPSEL__)
+			// nothing to do
+#else
+#	error "Unknown target endianness arcitecture"
 #endif
 			// обычные байтовые последовательности
 				while (size--)
