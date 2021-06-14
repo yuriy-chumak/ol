8acd12be5bd21b9f804719be93eaebd2704b7533
Yuriy Chumak <yuriy.chumak@mail.com>
2021-06-14 22:04:26 +0300

    rosettacode

--- tests/rosettacode/enumerations.scm
+++ tests/rosettacode/enumerations.scm
@@ -1,6 +1,5 @@
 ; https://www.rosettacode.org/wiki/Enumerations#Ol
 
-(define *interactive* #true)
 (define fruits '{
    apple  0
    banana 1
@@ -12,11 +11,11 @@
    'cherry 2})
 
 ; getting enumeration value:
-(get fruits 'apple -1) ; ==> 0
+(print (get fruits 'apple -1)) ; ==> 0
 ; or simply
-(fruits 'apple)        ; ==> 0
+(print (fruits 'apple))        ; ==> 0
 ; or simply with default (for non existent enumeration key) value
-(fruits 'carrot -1)    ; ==> -1
+(print (fruits 'carrot -1))    ; ==> -1
 
 ; simple function to create enumeration with autoassigning values
 (define (make-enumeration . args)
@@ -26,4 +25,4 @@
       args
       (iota (length args))))
 
-(make-enumeration 'apple 'banana 'cherry)
+(print (make-enumeration 'apple 'banana 'cherry))
