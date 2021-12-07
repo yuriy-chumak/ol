08e380bc6af3da68945689d251de152ba01e2507
Yuriy Chumak <yuriy.chumak@mail.com>
2021-12-07 14:09:11 +0200

    more rosettacode

---
+++ tests/rosettacode/bubble_sort.scm
@@ -0,0 +1,19 @@
+; http://rosettacode.org/wiki/Sorting_algorithms/Bubble_sort#Ol
+(define (bubble-sort x ??)
+   (define (sort-step l)
+      (if (or (null? l) (null? (cdr l)))
+         l
+         (if (?? (car l) (cadr l))
+            (cons (cadr l) (sort-step (cons (car l) (cddr l))))
+            (cons (car  l) (sort-step (cdr l))))))
+   (let loop ((i x))
+      (if (equal? i (sort-step i))
+         i
+         (loop (sort-step i)))))
+
+(print 
+   (bubble-sort (list 1 3 5 9 8 6 4 2) >))
+(print 
+   (bubble-sort (iota 100) >))
+(print 
+   (bubble-sort (iota 100) <))
