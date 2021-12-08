4bfd67d8025d0c4fcf5fc691520fe2141124f85a
Yuriy Chumak <yuriy.chumak@mail.com>
2021-12-08 12:21:10 +0200

    more rosettacode

---
+++ tests/rosettacode/sorting_algorithms/sleep_sort.scm
@@ -0,0 +1,10 @@
+; http://rosettacode.org/wiki/Sorting_algorithms/Sleep_sort#Ol
+
+(define (sleep-sort lst)
+   (for-each (lambda (timeout)
+         (async (lambda ()
+            (sleep timeout)
+            (print timeout))))
+      lst))
+
+(sleep-sort '(5 8 2 7 9 10 5))
