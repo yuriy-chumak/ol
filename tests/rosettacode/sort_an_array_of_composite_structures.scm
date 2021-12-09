4b38572eb4c78537acd93a9acf4609fcb69f4896
Yuriy Chumak <yuriy.chumak@mail.com>
2021-12-09 22:03:39 +0200

    more rosettacode

---
+++ tests/rosettacode/sort_an_array_of_composite_structures.scm
@@ -0,0 +1,20 @@
+; http://www.rosettacode.org/wiki/Sort_an_array_of_composite_structures#Ol
+
+(define (comp a b)
+   (string-ci<? (a 'name #f) (b 'name #f)))
+
+(for-each print
+   (sort comp (list
+      { 'name "David"
+        'value "Manager" }
+      { 'name "Alice"
+        'value "Sales" }
+      { 'name "Joanna"
+        'value "Director" }
+      { 'name "Henry"
+        'value "Admin" }
+      { 'name "Tim"
+        'value "Sales" }
+      { 'name "Juan"
+        'value "Admin" })))
+
---
+++ tests/rosettacode/sort_an_integer_array.scm
@@ -0,0 +1,5 @@
+; http://www.rosettacode.org/wiki/Sort_an_integer_array#Ol
+
+
+(print
+   (sort < '(9 -2 1 2 8 0 1 2)))
