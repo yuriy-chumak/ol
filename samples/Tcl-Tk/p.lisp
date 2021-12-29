a74f419197314bb4c7b336111d34108e8a36d2ef
Yuriy Chumak <yuriy.chumak@mail.com>
2021-12-29 19:57:53 +0200

    cleanup

--- samples/Tcl-Tk/p.lisp
+++ samples/Tcl-Tk/p.lisp
@@ -9,18 +9,17 @@
 
 ; let's declare function
 (define calculate (vm:pin (cons
-   ;     userdata  tcl       argc       argv in reverse order
-   (list fft-int type-vptr type-vptr fft-int  (list type-string type-string))
+   (cons fft-int
+   ;     userdata  tcl       argc      arguments in reverse order
+   (list type-vptr type-vptr fft-int  (list type-string type-string)))
    (lambda (userdata tcl argc argv)
       (let ((argv (reverse argv)))
-         (print "argc: " argc)
-         (print "argv: " argv)
-
          (let*((feets (string->number (cadr argv) 10))
                (meters (* feets 0.3048))
                (result
-                  (fold string-append "" (list "{" (number->string (inexact meters)) "}"))))
-            (print "result: " result)
+                  (number->string (inexact meters))))
+            (print "feets: " feets)
+            (print "meters: " meters)
             (Tcl_SetResult tcl result #f)))
       TCL_OK
 ))))
@@ -29,16 +28,6 @@
 ; and send it to the tcl
 (define cmd (Tcl_CreateCommand tcl "calculate" calculateCallback #f #f))
 
-
-
-;(Tcl_Eval tcl "calculate 123")
-;
-;
-; run
-;(Tk_Init tcl)
-;(Tcl_Eval tcl "
-;   grid [ttk::button .b -text \"---\"]
-;")
 (Tcl_EvalFile tcl "p.tcl")
 (Tk_MainLoop)
 (Tcl_DeleteCommand tcl "calculate")
