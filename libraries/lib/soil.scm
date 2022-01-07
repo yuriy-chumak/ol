6e547000030051867958fecffc498032a0918e67
Yuriy Chumak <yuriy.chumak@mail.com>
2022-01-07 15:57:53 +0200

    libsoil cubemaps enabled

--- libraries/lib/soil.scm
+++ libraries/lib/soil.scm
@@ -6,6 +6,9 @@
       SOIL_load_OGL_texture
       SOIL_load_OGL_texture_from_memory
 
+      SOIL_load_OGL_cubemap
+      SOIL_load_OGL_single_cubemap
+
       SOIL_load_image
       SOIL_load_image_from_memory
       SOIL_free_image_data
@@ -17,6 +20,8 @@
       SOIL_LOAD_RGBA
 
       SOIL_CREATE_NEW_ID
+      
+      SOIL_DDS_CUBEMAP_FACE_ORDER
 
       SOIL_FLAG_INVERT_Y
    )
@@ -63,7 +68,7 @@
    ;SOIL_SAVE_TYPE_BMP = 1,
    ;SOIL_SAVE_TYPE_DDS = 2
    
-   ;SOIL_DDS_CUBEMAP_FACE_ORDER "EWUDNS"
+   (define SOIL_DDS_CUBEMAP_FACE_ORDER "EWUDNS")
    
    ;SOIL_HDR_RGBE = 0,
    ;SOIL_HDR_RGBdivA = 1,
@@ -76,9 +81,12 @@
    
    (define SOIL_load_OGL_texture (libsoil fft-unsigned-int "SOIL_load_OGL_texture" type-string fft-int fft-unsigned-int fft-unsigned-int))
    (define SOIL_load_OGL_texture_from_memory (libsoil fft-unsigned-int "SOIL_load_OGL_texture_from_memory" type-bytevector fft-int fft-int fft-unsigned-int fft-unsigned-int))
-   (define SOIL_load_OGL_cubemap (libsoil fft-unsigned-int "SOIL_load_OGL_cubemap" type-string type-string type-string type-string type-string type-string fft-int fft-unsigned-int fft-unsigned-int))
+   (define SOIL_load_OGL_cubemap (libsoil fft-unsigned-int "SOIL_load_OGL_cubemap"
+      type-string type-string type-string type-string type-string type-string
+      fft-int fft-unsigned-int fft-unsigned-int))
    ;SOIL_load_OGL_cubemap_from_memory
-   (define SOIL_load_OGL_single_cubemap (libsoil fft-unsigned-int "SOIL_load_OGL_single_cubemap" type-string type-string fft-int fft-unsigned-int fft-unsigned-int))
+   (define SOIL_load_OGL_single_cubemap (libsoil fft-unsigned-int "SOIL_load_OGL_single_cubemap"
+      type-string type-string fft-int fft-unsigned-int fft-unsigned-int))
    ;SOIL_load_OGL_single_cubemap_from_memory
    ;SOIL_load_OGL_HDR_texture
 
