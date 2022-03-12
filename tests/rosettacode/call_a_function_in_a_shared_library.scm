effc6131d83e817e475b8eff74441731e2744661
Yuriy Chumak <yuriy.chumak@mail.com>
2022-03-12 10:38:09 +0200

    removed memory leak in rosettacode example

--- tests/rosettacode/call_a_function_in_a_shared_library.scm
+++ tests/rosettacode/call_a_function_in_a_shared_library.scm
@@ -3,10 +3,17 @@
 (import (otus ffi))
 
 (define self (load-dynamic-library #f))
-(define strdup (or
-   (self type-string "strdup" type-string)
-   (lambda (str)
-      (list->string (string->list str)))))
+(define strdup
+   (let ((strdup (self type-vptr "strdup" type-string))
+         (free   (self fft-void "free" type-vptr)))
+      (if (and strdup free)
+         (lambda (str)
+            (let*((dupped (strdup str))
+                  (result (vptr->string dupped)))
+               (free dupped)
+               result))
+         (lambda (str)
+            (list->string (string->list str))))))
 
 (define strduX (or
    (self type-string "strduX" type-string)
