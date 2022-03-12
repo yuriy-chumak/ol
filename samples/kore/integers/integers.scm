de311a83f6daa6ec4771c3e372e2aa4c2df2d8f5
Yuriy Chumak <yuriy.chumak@mail.com>
2022-03-12 11:48:06 +0200

    kore sample updated

--- samples/kore/integers/integers.scm
+++ samples/kore/integers/integers.scm
@@ -9,24 +9,49 @@
 
    (let*((out "analyze result:\n")
 
+         ;; byte
          (byte (box 0))
          (out (if (eq? (http_argument_get_byte req "id" byte) 1)
                (string-append out "byte: " (number->string (unbox byte)) "\n")
                out))
-         ;; (- (print byte))
 
-         (int16 (box 0))
-         (out (if (eq? (http_argument_get_int16 req "id" int16) 1)
-               (string-append out "int16: " (number->string (unbox int16)) "\n")
+         ;; int16/uint16
+         (int (box 0))
+         (out (if (eq? (http_argument_get_int16 req "id" int) 1)
+               (string-append out "int16: " (number->string (unbox int)) "\n")
                out))
-         ;; (- (print int16))
-
-         (int32 (box 0))
-         (out (if (eq? (http_argument_get_int32 req "id" int32) 1)
-               (string-append out "int32: " (number->string (unbox int32)) "\n")
+         (out (if (eq? (http_argument_get_uint16 req "id" int) 1)
+               (string-append out "uint16: " (number->string (unbox int)) "\n")
                out))
-         ;; (- (print int32))
 
+         ;; int32/uint32
+         (int (box 0))
+         (out (if (eq? (http_argument_get_int32 req "id" int) 1)
+               (string-append out "int32: " (number->string (unbox int)) "\n")
+               out))
+         (out (if (eq? (http_argument_get_uint32 req "id" int) 1)
+               (string-append out "uint32: " (number->string (unbox int)) "\n")
+               out))
+
+         ;; int64/uint64
+         (int (box 0)) ; we should allocate large number
+         (out (if (eq? (http_argument_get_int64 req "id" int) 1)
+               (string-append out "int64: " (number->string (unbox int)) "\n")
+               out))
+         (out (if (eq? (http_argument_get_uint64 req "id" int) 1)
+               (string-append out "uint64: " (number->string (unbox int)) "\n")
+               out))
+
+         ;; float/double
+         (int (box (inexact 0))) ; we should allocate large number
+         (out (if (eq? (http_argument_get_float req "id" int) 1)
+               (string-append out "float: " (number->string (unbox int)) "\n")
+               out))
+         (out (if (eq? (http_argument_get_double req "id" int) 1)
+               (string-append out "double: " (number->string (unbox int)) "\n")
+               out))
+
+         ;; end with newline
          (out (string-append out "\n")))
       
       (print "out: " out)
