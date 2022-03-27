1cd25d0bcce49f398461f3d9f55aa5c73c7bd684
Yuriy Chumak <yuriy.chumak@mail.com>
2022-03-27 18:00:18 +0300

    leveldb iterators

--- libraries/lib/leveldb.scm
+++ libraries/lib/leveldb.scm
@@ -32,13 +32,13 @@
    ;; typedef struct leveldb_env_t leveldb_env_t;
    ;; typedef struct leveldb_filelock_t leveldb_filelock_t;
    ;; typedef struct leveldb_filterpolicy_t leveldb_filterpolicy_t;
-   ;; typedef struct leveldb_iterator_t leveldb_iterator_t;
+   leveldb_iterator_t*
    ;; typedef struct leveldb_logger_t leveldb_logger_t;
    leveldb_options_t*
    ;; typedef struct leveldb_randomfile_t leveldb_randomfile_t;
    leveldb_readoptions_t*
    ;; typedef struct leveldb_seqfile_t leveldb_seqfile_t;
-   ;; typedef struct leveldb_snapshot_t leveldb_snapshot_t;
+   leveldb_snapshot_t*
    ;; typedef struct leveldb_writablefile_t leveldb_writablefile_t;
    leveldb_writebatch_t*
    leveldb_writeoptions_t*
@@ -51,9 +51,9 @@
    leveldb_write
    leveldb_get
 
-   ;; leveldb_create_iterator
-   ;; leveldb_create_snapshot
-   ;; leveldb_release_snapshot
+   leveldb_create_iterator
+   leveldb_create_snapshot
+   leveldb_release_snapshot
    ;; leveldb_property_value
    ;; leveldb_approximate_sizes
    ;; leveldb_compact_range
@@ -61,16 +61,16 @@
    leveldb_destroy_db
    ;; leveldb_repair_db
 
-   ;; leveldb_iter_destroy
-   ;; leveldb_iter_valid
-   ;; leveldb_iter_seek_to_first
-   ;; leveldb_iter_seek_to_last
-   ;; leveldb_iter_seek
-   ;; leveldb_iter_next
-   ;; leveldb_iter_prev
-   ;; leveldb_iter_key
-   ;; leveldb_iter_value
-   ;; leveldb_iter_get_error
+   leveldb_iter_destroy
+   leveldb_iter_valid
+   leveldb_iter_seek_to_first
+   leveldb_iter_seek_to_last
+   leveldb_iter_seek
+   leveldb_iter_next
+   leveldb_iter_prev
+   leveldb_iter_key
+   leveldb_iter_value
+   leveldb_iter_get_error
 
    ;; ; writebatch
    ;; leveldb_writebatch_create
@@ -83,7 +83,7 @@
 
    ; options
    leveldb_options_create
-   ;; leveldb_options_destroy
+   leveldb_options_destroy
    ;; leveldb_options_set_comparator
    ;; leveldb_options_set_filter_policy
    leveldb_options_set_create_if_missing
@@ -98,9 +98,9 @@
    ;; leveldb_options_set_block_restart_interval
    ;; leveldb_options_set_max_file_size
 
-   ;; leveldb_options_set_compression
-   ;;    leveldb_no_compression
-   ;;    leveldb_snappy_compression
+   leveldb_options_set_compression
+      leveldb_no_compression
+      leveldb_snappy_compression
 
    ;; leveldb_comparator_create
    ;; leveldb_comparator_destroy
@@ -112,15 +112,15 @@
 
    ; read options
    leveldb_readoptions_create
-   ;; leveldb_readoptions_destroy
-   ;; leveldb_readoptions_set_verify_checksums
-   ;; leveldb_readoptions_set_fill_cache
-   ;; leveldb_readoptions_set_snapshot
+   leveldb_readoptions_destroy
+   leveldb_readoptions_set_verify_checksums
+   leveldb_readoptions_set_fill_cache
+   leveldb_readoptions_set_snapshot
 
    ; write options
    leveldb_writeoptions_create
-   ;; leveldb_writeoptions_destroy
-   ;; leveldb_writeoptions_set_sync
+   leveldb_writeoptions_destroy
+   leveldb_writeoptions_set_sync
 
    ;; ; cache
    ;; leveldb_cache_create_lru
@@ -138,12 +138,22 @@
    leveldb_minor_version
    
    ; smart interface
+   ; -----------------------
    leveldb:open
    leveldb:close
+   leveldb:make-options
 
    leveldb:get
+   leveldb:get-string  ; todo
+   leveldb:get-integer ; todo
+   leveldb:make-readoptions
+;   leveldb:get-boolean ;?
    leveldb:delete
    leveldb:put
+   leveldb:make-writeoptions
+
+   encode
+   decode
 )
 
 ; ============================================================================
@@ -175,13 +185,13 @@
    ;; typedef struct leveldb_env_t leveldb_env_t;
    ;; typedef struct leveldb_filelock_t leveldb_filelock_t;
    ;; typedef struct leveldb_filterpolicy_t leveldb_filterpolicy_t;
-   ;; typedef struct leveldb_iterator_t leveldb_iterator_t;
+   (define leveldb_iterator_t* fft-void*)
    ;; typedef struct leveldb_logger_t leveldb_logger_t;
    (define leveldb_options_t* fft-void*)
    ;; typedef struct leveldb_randomfile_t leveldb_randomfile_t;
    (define leveldb_readoptions_t* fft-void*)
    ;; typedef struct leveldb_seqfile_t leveldb_seqfile_t;
-   ;; typedef struct leveldb_snapshot_t leveldb_snapshot_t;
+   (define leveldb_snapshot_t* fft-void*)
    ;; typedef struct leveldb_writablefile_t leveldb_writablefile_t;
    (define leveldb_writebatch_t* fft-void*)
    (define leveldb_writeoptions_t* fft-void*)
@@ -195,6 +205,7 @@
    (define size_t fft-size-t)
 
    (define uint8_t fft-uint8)
+   ;(define bool fft-unsigned-char)
 
    ; -----------------------------------------------
 
@@ -211,9 +222,9 @@
    (define leveldb_write (leveldb void "leveldb_write" leveldb_t* leveldb_writeoptions_t* leveldb_writebatch_t* char**))
    (define leveldb_get (leveldb fft-void* "leveldb_get" leveldb_t* leveldb_readoptions_t* fft-any size_t (fft& fft-uint32) char**)) ; (fft& size_t)
 
-   ;; leveldb_create_iterator
-   ;; leveldb_create_snapshot
-   ;; leveldb_release_snapshot
+   (define leveldb_create_iterator (leveldb leveldb_iterator_t* "leveldb_create_iterator" leveldb_t* leveldb_readoptions_t*))
+   (define leveldb_create_snapshot (leveldb leveldb_snapshot_t* "leveldb_create_snapshot" leveldb_t*))
+   (define leveldb_release_snapshot (leveldb void "leveldb_release_snapshot" leveldb_t* leveldb_snapshot_t*))
    ;; leveldb_property_value
    ;; leveldb_approximate_sizes
    ;; leveldb_compact_range
@@ -221,16 +232,16 @@
    (define leveldb_destroy_db (leveldb void "leveldb_destroy_db" leveldb_options_t* char* fft-void**))
    ;; leveldb_repair_db
 
-   ;; leveldb_iter_destroy
-   ;; leveldb_iter_valid
-   ;; leveldb_iter_seek_to_first
-   ;; leveldb_iter_seek_to_last
-   ;; leveldb_iter_seek
-   ;; leveldb_iter_next
-   ;; leveldb_iter_prev
-   ;; leveldb_iter_key
-   ;; leveldb_iter_value
-   ;; leveldb_iter_get_error
+   (define leveldb_iter_destroy (leveldb void "leveldb_iter_destroy" leveldb_iterator_t*))
+   (define leveldb_iter_valid (leveldb fft-unsigned-char "leveldb_iter_valid" leveldb_iterator_t*))
+   (define leveldb_iter_seek_to_first (leveldb void "leveldb_iter_seek_to_first" leveldb_iterator_t*))
+   (define leveldb_iter_seek_to_last (leveldb void "leveldb_iter_seek_to_last" leveldb_iterator_t*))
+   (define leveldb_iter_seek (leveldb void "leveldb_iter_seek" leveldb_iterator_t* fft-any size_t))
+   (define leveldb_iter_next (leveldb void "leveldb_iter_next" leveldb_iterator_t*))
+   (define leveldb_iter_prev (leveldb void "leveldb_iter_prev" leveldb_iterator_t*))
+   (define leveldb_iter_key (leveldb fft-void* "leveldb_iter_key" leveldb_iterator_t* (fft& fft-uint32)))
+   (define leveldb_iter_value (leveldb fft-void* "leveldb_iter_value" leveldb_iterator_t* (fft& fft-uint32)))
+   (define leveldb_iter_get_error (leveldb void "leveldb_iter_get_error" leveldb_iterator_t* char**))
 
    ;; ; writebatch
    ;; leveldb_writebatch_create
@@ -243,7 +254,7 @@
 
    ; options
    (define leveldb_options_create (leveldb leveldb_options_t* "leveldb_options_create"))
-   ;; leveldb_options_destroy
+   (define leveldb_options_destroy (leveldb void "leveldb_options_destroy" leveldb_options_t*))
    ;; leveldb_options_set_comparator
    ;; leveldb_options_set_filter_policy
    (define leveldb_options_set_create_if_missing (leveldb void "leveldb_options_set_create_if_missing" leveldb_options_t* uint8_t))
@@ -258,9 +269,9 @@
    ;; leveldb_options_set_block_restart_interval
    ;; leveldb_options_set_max_file_size
 
-   ;; leveldb_options_set_compression
-   ;;    leveldb_no_compression
-   ;;    leveldb_snappy_compression
+   (define leveldb_options_set_compression (leveldb void "leveldb_options_set_compression" leveldb_options_t* int))
+      (define leveldb_no_compression 0)
+      (define leveldb_snappy_compression 1)
 
    ;; leveldb_comparator_create
    ;; leveldb_comparator_destroy
@@ -272,15 +283,15 @@
 
    ; read options
    (define leveldb_readoptions_create (leveldb leveldb_readoptions_t* "leveldb_readoptions_create"))
-   ;; leveldb_readoptions_destroy
-   ;; leveldb_readoptions_set_verify_checksums
-   ;; leveldb_readoptions_set_fill_cache
-   ;; leveldb_readoptions_set_snapshot
+   (define leveldb_readoptions_destroy (leveldb void "leveldb_readoptions_destroy" leveldb_readoptions_t*))
+   (define leveldb_readoptions_set_verify_checksums (leveldb void "leveldb_readoptions_set_verify_checksums" leveldb_readoptions_t* fft-unsigned-char))
+   (define leveldb_readoptions_set_fill_cache (leveldb void "leveldb_readoptions_set_fill_cache" leveldb_readoptions_t* fft-unsigned-char))
+   (define leveldb_readoptions_set_snapshot (leveldb void "leveldb_readoptions_set_snapshot" leveldb_readoptions_t* leveldb_snapshot_t*))
 
    ; write options
    (define leveldb_writeoptions_create (leveldb leveldb_writeoptions_t* "leveldb_writeoptions_create"))
-   ;; leveldb_writeoptions_destroy
-   ;; leveldb_writeoptions_set_sync
+   (define leveldb_writeoptions_destroy (leveldb void "leveldb_writeoptions_destroy" leveldb_writeoptions_t*))
+   (define leveldb_writeoptions_set_sync (leveldb void "leveldb_writeoptions_set_sync" leveldb_writeoptions_t* fft-unsigned-char))
 
    ;; ; cache
    ;; leveldb_cache_create_lru
@@ -296,17 +307,56 @@
 ;; High-level Interface
 (begin
 
-   (define (leveldb:open filename) ; todo: options
-      (define options (leveldb_options_create))
-      (leveldb_options_set_create_if_missing options 1)
-      (define err (make-vptr))
-      (define database (leveldb_open options "testdb" err))
-      (if (equal? err NULL)
-         database
-      else
-         (print-to stderr "leveldb open error: " (vptr->string err))
-         (leveldb_free err)
-         #false))
+   (define (leveldb:make-options options)
+      (define op (leveldb_options_create))
+      ; apply options:
+      (ff-fold (lambda (? key value)
+                  (case key
+                     ('create_if_missing
+                        (leveldb_options_set_create_if_missing op (if value 1 0)))
+                     ;'comparator
+                     ;'filter_policy
+                     ;'error_if_exists
+                     ;'paranoid_checks
+                     ;'env
+                     ;'info_log
+                     ;'write_buffer_size
+                     ;'max_open_files
+                     ;'cache
+                     ;'block_size
+                     ;'block_restart_interval
+                     ;'max_file_size
+                     ('compression ; leveldb_no_compression | leveldb_snappy_compression
+                        (leveldb_options_set_compression op value))
+                     (else
+                        (print-to stderr "leveldb: unknown option " key " with value " value))))
+         #f
+         options)
+      op)
+
+   (define leveldb:open
+      ; options can be ff or previously created options
+      (define (leveldb:open filename options)
+         (define op (if (ff? options)
+            (leveldb:make-options options)
+            options))
+         
+         (define err (make-vptr))
+         (define database (leveldb_open op filename err))
+         (if (ff? options) ; cleanup
+            (leveldb_options_destroy op))
+
+         (if (equal? err NULL)
+            database
+         else
+            (print-to stderr "leveldb open error:\n   " (vptr->string err))
+            (leveldb_free err)
+            #false))
+      (case-lambda
+         ((filename)
+            (leveldb:open filename {}))
+         ((filename options)
+            (leveldb:open filename options))))
 
    (define (leveldb:close database)
       (leveldb_close database)
@@ -314,7 +364,7 @@
 
 
    ; * internal
-   (define (encode-arg arg)
+   (define (encode arg)
       (cond
          ((and (integer? arg) (< arg #x10000000000000000)) ; 64-bit
             (list (cons (fft* fft-long-long) (box arg)) (sizeof fft-long-long)))
@@ -329,27 +379,53 @@
             (define len (length bin))
             (list (cons (fft* fft-char) bin) len))))
 
+   (define (leveldb:make-writeoptions options)
+      (define op (leveldb_writeoptions_create))
+      ; apply options:
+      (ff-fold (lambda (? key value)
+                  (case key
+                     ('sync
+                        (leveldb_writeoptions_set_sync op (if value 1 0)))
+                     (else
+                        (print-to stderr "leveldb: unknown write option " key " with value " value))))
+         #f
+         options)
+      op)
+
    ; put
-   (define (leveldb:put database key value)
-      (define err (make-vptr))
-      (apply leveldb_put (append
-         (list database (leveldb_writeoptions_create))
-         (encode-arg key)
-         (encode-arg value)
-         (list err)))
-      (if (equal? err NULL)
-         #true
-      else
-         (print-to stderr "leveldb put error: " (vptr->string err))
-         (leveldb_free err)
-         #false))
+   (define leveldb:put
+      (define (leveldb:put database key value options)
+         (define op (if (ff? options)
+            (leveldb:make-writeoptions options)
+            options))
+
+         (define err (make-vptr))
+         (apply leveldb_put (append
+            (list database op)
+            (encode key)
+            (encode value)
+            (list err)))
+         (if (ff? options) ; cleanup
+            (leveldb_writeoptions_destroy op))
+
+         (if (equal? err NULL)
+            #true
+         else
+            (print-to stderr "leveldb put error: " (vptr->string err))
+            (leveldb_free err)
+            #false))
+      (case-lambda
+         ((database key value)
+            (leveldb:put database key value {}))
+         ((database key value options)
+            (leveldb:put database key value options))))
 
    ; delete
    (define (leveldb:delete database key)
       (define err (make-vptr))
       (apply leveldb_delete (append
          (list database (leveldb_writeoptions_create))
-         (encode-arg key)
+         (encode key)
          (list err)))
       (if (equal? err NULL)
          #true
@@ -358,29 +434,81 @@
          (leveldb_free err)
          #false))
 
-   ; get
-   (define (leveldb:get database key . key-type)
-      (define err (make-vptr))
-      (define len (box 0))
-      (define out
-         (apply leveldb_get (append
-            (list database (leveldb_readoptions_create))
-            (encode-arg key)
-            (list len err))))
-      (if (equal? err NULL)
-         (if (> (unbox len) 0)
-         then
-            (case (unless (null? key-type) (car key-type))
+   ; -------------------------------------------
+   (define (leveldb:make-readoptions options)
+      (define op (leveldb_readoptions_create))
+      ; apply options:
+      (ff-fold (lambda (? key value)
+                  (case key
+                     ('verify_checksums
+                        (leveldb_readoptions_set_verify_checksums op (if value 1 0)))
+                     ('fill_cache
+                        (leveldb_readoptions_set_fill_cache op (if value 1 0)))
+                     ('snapshot
+                        (leveldb_readoptions_set_snapshot op value))
+                     (else
+                        (print-to stderr "leveldb: unknown read option " key " with value " value))))
+         #f
+         options)
+      op)
+
+   (define decode
+      (case-lambda
+         ((ptr len)
+            (fasl-decode (bytevector->list (vptr->bytevector ptr len)) #false))
+         ((ptr len returntype)
+            (case returntype
                (string?   ; ignore string length, we assume that string is null-terminated
-                  (vptr->string out))
-               (integer?
-                  (assert (eq? (car len) 8))
-                  (bytevector->int64 (vptr->bytevector out 8) 0))
+                  (vptr->string ptr))
+               (integer?  ; we assume that integer is 64-bit width
+                  (assert (eq? len 8))
+                  (bytevector->int64 (vptr->bytevector ptr 8) 0))
+               (bytevector?
+                  ((vptr->bytevector ptr len)))
                (else
-                  (fasl-decode (bytevector->list (vptr->bytevector out (car len))) #false))))
-      else
-         (print-to stderr "leveldb get error: " (vptr->string err))
-         (leveldb_free err)
-         #false))
+                  (fasl-decode (bytevector->list (vptr->bytevector ptr len)) #false))))))
+
+   ; get
+   (define leveldb:get
+      (define (leveldb:get database key options returntype)
+         (define op (if (ff? options)
+            (leveldb:make-readoptions options)
+            options))
+
+         (define err (make-vptr))
+         (define len (box 0))
+         (define out
+            (apply leveldb_get (append
+               (list database op)
+               (encode key)
+               (list len err))))
+         (if (ff? options) ; cleanup
+            (leveldb_readoptions_destroy op))
+
+         (if (equal? err NULL)
+         then
+            (when (> (unbox len) 0)
+               (decode out (unbox len) returntype))
+         else
+            (print-to stderr "leveldb get error: " (vptr->string err))
+            (leveldb_free err)
+            #false))
+      (case-lambda
+         ((database key)
+            (leveldb:get database key {} #false))
+         ((database key opt/type)
+            (if (or (ff? opt/type) (eq? (type opt/type) type-vptr))
+               (leveldb:get database key opt/type #false)
+               (leveldb:get database key {} opt/type)))
+         ((database key opt/type type/opt)
+            (if (or (ff? opt/type) (eq? (type opt/type) type-vptr))
+               (leveldb:get database key opt/type type/opt)
+               (leveldb:get database key type/opt opt/type)))))
+
+   (define (leveldb:get-string database key)
+      (leveldb:get database key string?))
+
+   (define (leveldb:get-integer database key)
+      (leveldb:get database key integer?))
 
 ))
--- samples/leveldb/main.lisp
+++ samples/leveldb/main.lisp
@@ -4,38 +4,114 @@
 
 (print "leveldb version: " (leveldb_major_version) "." (leveldb_minor_version))
 
-;; OPEN
+;; --------------------------------
+;;   OPEN
+;; --------------------------------
+
+; simple open/close
 (define db (leveldb:open "testdb"))
+(if db (leveldb_close db))
+
+; smart open
+(define db (leveldb:open "testdb" {
+   'create_if_missing #true
+   'compression leveldb_no_compression
+}))
+
+
+;; --------------------------------
+;;   READ
+;; --------------------------------
+(define (do_read db)
+   (print "--- read ---")
+
+   ; any ol objects
+   (print "object: "
+      (leveldb:get db "i-want-to-read-an-object"))
+
+   (display "reading internal object... ")
+   (define oread (leveldb:get db "+"))
+   (print "internal object: " oread)
+   (print "try to use loaded object: (oread 1 2 3): " (if oread (oread 1 2 3)))
+
+
+   ; integer
+   (print "integer: "
+      (leveldb:get db "i-want-to-read-an-integer" integer?))
+
+   (print "integer with options 1: "
+      (leveldb:get db "i-want-to-read-an-integer" integer? {}))
+
+   (print "integer with options 2: "
+      (leveldb:get db "i-want-to-read-an-integer" {
+         'verify_checksums #false
+         'fill_cache #true
+      } integer?))
+
+   ; string
+   (print "reading string by string key: "
+      (leveldb:get db "string-key-string-value" string?))
+   (print "reading string by integer key: "
+      (leveldb:get db 123777 string?))
+)
+
+; negative test, try to read non existent data if database just created and empty
+(do_read db)
+
+;; --------------------------------
+;;   WRITE
+;; --------------------------------
 
 ;; SMART WRITE
-(display "writing string value... ")
-(if (leveldb:put db "a-string-key" "a-string-value")
-   (print "ok."))
+; writing string key and string value
+(leveldb:put db "string-key-string-value" "a-string-value")
 
-(display "writing integer value... ")
-(if (leveldb:put db 1234567 7654321)
-   (print "ok."))
+; writing integer key and string value
+(leveldb:put db 123777 "a-string-123777-value")
 
-(display "writing internal object (a '+' function)... ")
-(if (leveldb:put db "+" +)
-   (print "ok."))
+; writing string key and string value
+(leveldb:put db 1234567 7654321)
 
-;; SMART READ
-(display "reading string value... ")
-(define sread (leveldb:get db "a-string-key" string?))
-(print "string value: " sread)
+; writing internal object (a '+' function)...
+(leveldb:put db "+" +)
 
-(display "reading integer value... ")
-(define iread (leveldb:get db 1234567 integer?))
-(print "integer value: " iread)
 
-(display "reading internal object... ")
-(define oread (leveldb:get db "+"))
-(print "internal object: " oread)
-(print "try to use loaded object: (oread 1 2 3): " (oread 1 2 3))
+; reread
+(do_read db)
 
-;; CLOSE
 (leveldb_close db)
+; -=( iterators )=--------------------------
 
-; done.
+(define db (leveldb:open "intint" {
+   'create_if_missing #true
+   'compression leveldb_no_compression
+}))
+
+(for-each (lambda (x)
+      (leveldb:put db x (* x x)))
+   (iota 8))
+
+
+(define iter (leveldb_create_iterator db (leveldb:make-readoptions {})))
+(leveldb_iter_seek_to_first iter)
+(let loop ()
+   (when (eq? (leveldb_iter_valid iter) 1)
+      (define kl (box 0))
+      (define key (leveldb_iter_key iter kl))
+      (display "key: ")
+      (display (decode key (unbox kl) integer?))
+
+      (define vl (box 0))
+      (define value (leveldb_iter_value iter vl))
+      (display ", value: ")
+      (display (decode value (unbox vl) integer?))
+
+      (print)
+
+      (leveldb_iter_next iter)
+      (loop)))
+(leveldb_iter_destroy iter)
+
+; done
+(leveldb_close db)
 (print "Ok.")
