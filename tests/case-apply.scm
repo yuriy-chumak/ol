d1bd3b770598be80133a9be08d9f80e4a8d743c6
Yuriy Chumak <yuriy.chumak@mail.com>
2022-04-07 18:20:47 +0300

    case-apply test update

--- tests/case-apply.scm
+++ tests/case-apply.scm
@@ -1,17 +1,17 @@
 (import (otus case-apply))
 
 (define (f0)
-   0)
+   (cons "0" 0))
 (define (f1 x)
-   x)
+   (cons "2" (* x 2)))
 (define (f2 x y)
-   (cons x y))
+   (cons "3" (+ x y)))
 
 (define (test f)
    (case-apply f
       '(1 . (1))
       '(2 . (7 2))))
 
-(print (test f0))
-(print (test f1))
-(print (test f2))
+(print "test arity 0 function: " (test f0))
+(print "test arity 1 function: " (test f1))
+(print "test arity 2 function: " (test f2))
