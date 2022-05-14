670d784a9bde059c7e4835dbc959baed27598960
Yuriy Chumak <yuriy.chumak@mail.com>
2022-05-15 02:59:20 +0300

    small fold cleanup

--- libraries/owl/list.scm
+++ libraries/owl/list.scm
@@ -41,29 +41,30 @@
             (for (op st (car l)) (cdr l) op)))
 
       (define fold
-         (define fold (lambda (f state a)
-            (let loop ((state state) (a a))
-               (if (null? a)
-                  state
-                  (loop (f state (car a)) (cdr a))))))
+         ; (fold (lambda (state a)) state a)
+         ; (fold (lambda (state a b)) state a b)
+         ; (fold (lambda (state a...)) state a...)
          (case-lambda
-            ((f state a)      (fold f state a))
-            ((f state a b)    (let loop ((state state) (a a) (b b)) ; fold2
+            ((f state a)      (let loop ((state state) (a a))
+                                 (if (null? a)
+                                    state
+                                    (loop (f state (car a)) (cdr a)))))
+            ((f state a b)    (let loop ((state state) (a a) (b b))
                                  (if (null? a)
                                     state
                                     (loop (f state (car a) (car b)) (cdr a) (cdr b)))))
-            ((f state a b . c); foldN
-                              (let loop ((state state) (args (cons a (cons b c))))
-                                 (if (null? (car args)) ; закончились
+            ((f state a b . c)
+                              (let loop ((state state) (args (cons* a b c)))
+                                 (if (null? (car args))
                                     state
                                     (loop (apply f (cons state (map car args))) (map cdr args)))))
             ((f state) state)))
 
-      ;(assert (fold + 0)                              ===>  0)
-      ;(assert (fold + 0 '(1 2))                       ===>  3)
-      ;(assert (fold + 0 '(1 2) '(3 4))                ===> 10)
-      ;(assert (fold + 0 '(1 2) '(3 4) '(5 6))         ===> 21)
-      ;(assert (fold + 0 '(1 2) '(3 4) '(5 6) '(7 8))  ===> 36)
+      ;(assert (fold - 9)                              ===>   9)
+      ;(assert (fold - 9 '(1 2))                       ===>   6)
+      ;(assert (fold - 9 '(1 2) '(3 4))                ===>  -1)
+      ;(assert (fold - 9 '(1 2) '(3 4) '(5 6))         ===> -12)
+      ;(assert (fold - 9 '(1 2) '(3 4) '(5 6) '(7 8))  ===> -27)
 
 
       (define (unfold op st end?)
