29df04d29d929030365a6938e5dc6918773ba4ba
Yuriy Chumak <yuriy.chumak@mail.com>
2022-10-05 17:11:33 +0300

    `(lib sqlite)` update with debug colors

--- libraries/lib/sqlite.scm
+++ libraries/lib/sqlite.scm
@@ -587,7 +587,10 @@
    (define RED "\e[0;31m")
    (define GREEN "\e[0;32m")
    (define YELLOW "\e[0;33m")
+   (define BLUE "\e[0;34m")
    (define MAGENTA "\e[0;35m")
+   (define CYAN "\e[0;36m")
+   (define WHITE "\e[0;37m")
    (define END "\e[0;0m"))
 
 (cond-expand
