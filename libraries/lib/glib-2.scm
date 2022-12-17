502fa00137c1c3e24f957b323740ee9f5dfa2ae7
Yuriy Chumak <yuriy.chumak@mail.com>
2022-12-17 04:33:56 +0200

    glib2 and gtk-3 updated

--- libraries/lib/glib-2.scm
+++ libraries/lib/glib-2.scm
@@ -1,7 +1,7 @@
 (define-library (lib glib-2)
    (export
       void
-      gint guint gulong gpointer
+      gint guint gulong gsize gpointer
       gdouble
       gchar* gboolean
 
@@ -21,7 +21,17 @@
       g_object_ref
       g_object_unref
 
+      GValue*
+      make-GValue
+      g_value_init
+      g_value_get_gtype
+      g_value_set_char
+      g_value_get_char
+      g_value_set_int64
+      g_value_get_int64
+
       G_CALLBACK
+      G_TYPE_GTYPE
 
       g_signal_connect
       g_application_run
@@ -29,6 +39,7 @@
    )
    (import
       (scheme core)
+      (owl string)
       (otus ffi))
 
 (begin
@@ -43,8 +54,11 @@
 (define gulong fft-unsigned-long)
 (define gpointer fft-void*)
 (define gdouble fft-double)
+(define gchar fft-char)
 (define gchar* type-string)
+(define gsize fft-unsigned-int)
 (define gboolean gint)
+(define gint64 fft-signed-long-long)
 (define void fft-void)
 
 (define GCallback type-callable) ; void (*GCallback)(void)
@@ -62,11 +76,11 @@
    (load-dynamic-library "libgio-2.0.so")
    (load-dynamic-library "libgio-2.0.so.0")))
 
-(define GObject* fft-void*)
+(define GObject* type-vptr)
 (define g_object_ref (GOBJECT gpointer "g_object_ref" gpointer))
 (define g_object_unref (GOBJECT void "g_object_unref" gpointer))
 
-(define GError* fft-void*)
+(define GError* type-vptr)
 (define g_error_free (GOBJECT void "g_error_free" GError*))
 
 (define g_signal_connect_data (GOBJECT gulong "g_signal_connect_data" gpointer type-string GCallback gpointer GClosureNotify GConnectFlags))
@@ -74,5 +88,51 @@
    (g_signal_connect_data instance detailed_signal c_handler data #false 0))
 (define g_application_run (GIO gint "g_application_run" GApplication* gint (fft* fft-void*)))
 (define g_application_quit (GIO void "g_application_quit" GApplication*))
+
+; --=( GType )=---------------
+(define GType fft-unsigned-long)
+(setq G_TYPE_FUNDAMENTAL_SHIFT 2)
+(setq G_TYPE_MAKE_FUNDAMENTAL (lambda (x)
+   (let* ((hi lo (vm:shl x G_TYPE_FUNDAMENTAL_SHIFT)))
+      lo)))
+;...
+(setq G_TYPE_INT64 (G_TYPE_MAKE_FUNDAMENTAL 10))
+;...
+(setq G_TYPE_STRING (G_TYPE_MAKE_FUNDAMENTAL 16))
+;...
+
+(define G_TYPE_GTYPE ((GIO GType "g_gtype_get_type")))
+
+
+; --=( GValue )=---------
+
+(define GValue* type-vptr)
+(define g_value_init (GIO GValue* "g_value_init" GValue* GType))
+
+(define g_value_get_gtype (GIO GType "g_value_get_gtype" GValue*))
+; gchar
+(define g_value_set_char (GIO void "g_value_set_char" GValue* gchar))
+(define g_value_get_char (GIO gchar "g_value_get_char" GValue*))
+; gint64
+(define g_value_set_int64 (GIO void "g_value_set_int64" GValue* gint64))
+(define g_value_get_int64 (GIO gint64 "g_value_get_int64" GValue*))
+; strings
+(define g_value_set_string (GIO void "g_value_set_string" GValue* gchar*))
+(define g_value_get_string (GIO gchar* "g_value_get_string" GValue*))
+
+(define make-GValue
+   (define (make) (make-bytevector 24)) ; GType(8) + gdouble(8)
+   (case-lambda
+      (()   (make))
+      ((x)  (let ((v (make)))
+               (cond
+                  ((integer? x)
+                     (g_value_init v G_TYPE_INT64)
+                     (g_value_set_int64 v x))
+                  ((string? x)
+                     (g_value_init v G_TYPE_STRING)
+                     (g_value_set_string v x))
+                  (else #f))
+               v))))
 
 ))
---
+++ libraries/lib/gtk-3/list-store.scm
@@ -0,0 +1,38 @@
+(define-library (lib gtk-3 list-store)
+   (export
+      GtkListStore*
+      GTK_TYPE_LIST_STORE
+      gtk_list_store_get_type
+
+      make-GtkTreeIter
+
+      gtk_list_store_set_value
+
+      gtk_list_store_clear
+      ;;gtk_list_store_set_value
+      ;;gtk_list_store_remove
+      ;;gtk_list_store_insert
+      gtk_list_store_append
+
+   )
+   (import
+      (scheme core)
+      (otus ffi)
+      (lib gtk-3 gtk))
+
+(begin
+   (define GtkListStore* type-vptr)
+   (define gtk_list_store_get_type (GTK3 GType "gtk_list_store_get_type"))
+   (define GTK_TYPE_LIST_STORE (gtk_list_store_get_type))
+
+   (define GtkTreeIter* type-vptr)
+   (define (make-GtkTreeIter)
+      (make-bytevector 32)) ; (sizeof GtkTreeIter)
+
+   (define gtk_list_store_set_value (GTK3 void "gtk_list_store_set_value" GtkListStore* GtkTreeIter* gint GValue*))
+
+   (define gtk_list_store_clear (GTK3 void "gtk_list_store_clear" GtkListStore*))
+   (define gtk_list_store_append (GTK3 void "gtk_list_store_append" GtkListStore* GtkTreeIter*))
+
+
+))
