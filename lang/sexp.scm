dc31bf9196f7a3dd3b23dad46c1f02cad85ba20c
Yuriy Chumak <yuriy.chumak@mail.com>
2023-02-12 01:44:44 +0200

    " can be part of symbol name

--- lang/sexp.scm
+++ lang/sexp.scm
@@ -76,7 +76,7 @@
          symbol-lead-chars
          numeric-chars
          (alist->ff
-            (map putT (string->runes "'/"))  ;; we can use ' and / as part of symbol names
+            (map putT (string->runes "'/\""))  ;; we can use "'`/"" as part of symbol names
       )))
 
       (define (symbol-lead-char? n)
