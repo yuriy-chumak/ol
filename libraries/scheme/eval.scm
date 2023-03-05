3efae809fd303f8566eb6ffa3cd2f7ea99cc0d91
Yuriy Chumak <yuriy.chumak@mail.com>
2023-03-05 02:33:52 +0200

    (scheme eval) fix

--- libraries/scheme/eval.scm
+++ libraries/scheme/eval.scm
@@ -1,10 +1,25 @@
 (define-library (scheme eval)
    (export 
-      (exports (scheme core)) )
+      eval)
 
    (import
-      (scheme core))
+      (scheme core)
+      (scheme repl)
+      (prefix (lang eval) lang/)
+      ; default Ol environment:
+      (otus lisp))
 
-   (begin
-      #true
+(begin
+   (define eval
+      (define (eval expr env)
+         (case (lang/eval expr env)
+            (['ok expr env]
+               expr)
+            (['fail reason]
+               (runtime-error "eval failed with" reason))
+            (else
+               (runtime-error "unknown eval fail error") #n)))
+      (case-lambda
+         ((expr env) (eval expr env))
+         ((expr) (eval expr (interaction-environment)))))
 ))
--- tests/eval.scm
+++ tests/eval.scm
@@ -1,15 +1,25 @@
-(import (lang eval))
+(import (scheme eval))
 (import (scheme repl))
 
 ;; normal sexp
+(print (eval '(+ 1 2)))
+
+;; normal sexp using env
 (print (eval '(+ 1 2) (interaction-environment)))
 
 ;; linked code
-(print (eval (list + 1 2) (interaction-environment)))
+(print (eval (list + 1 2)))
 
 ;; nested
-(print (eval (list eval '(+ 1 2) (interaction-environment)) (interaction-environment)))
+(print (eval (list eval '(+ 1 2))))
 
 ;; silly
 (let ((add (fasl-encode +)))
-   (eval (list 'print (list (list 'fasl-decode (list 'quote add) 42) 1 2)) (interaction-environment)))
+   (eval (list 'print (list (list 'fasl-decode (list 'quote add) 42) 1 2))))
+
+;; error handling
+(with-exception-handler
+   (lambda (x)
+      (print "error detected. " (cadr x)))
+   (lambda ()
+      (print (eval '(not-a-func 1 2)))))
