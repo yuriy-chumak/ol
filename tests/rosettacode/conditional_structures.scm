a4f19356d0c7bcd1a326f1fe8cf9a33ba4951a74
Yuriy Chumak <yuriy.chumak@mail.com>
2023-07-16 00:02:41 +0300

    more rosettacode

--- tests/rosettacode/conditional_structures.scm
+++ tests/rosettacode/conditional_structures.scm
@@ -1,30 +1,75 @@
 ; http://www.rosettacode.org/wiki/Conditional_structures
 
-; if-then, the simplest conditional primitive.
-(if (= (* 2 2) 4) (print "if-then: equal"))
-(if (= (* 2 2) 6) (print "if-then: non equal"))
+; if-then, the simplest conditional `if` primitive.
+(if (= (* 2 2) 4)
+   (print "if-then: equal"))
+(if (= (* 2 2) 6)
+   (print "if-then: should not be printed"))
 
-; if-then-else, the full conditional 'if' primitive.
-(if (= (* 2 2) 4) (print "if-then-else: equal") (print "if-then-else: non equal"))
-(if (= (* 2 2) 6) (print "if-then-else: non equal") (print "if-then-else: i don't know"))
+; if-then-else, the full conditional `if` primitive.
+(if (= (* 2 2) 4)
+   (print "if-then-else: equal")
+   (print "if-then-else: non equal"))
+(if (= (* 2 2) 6)
+   (print "if-then-else: equal")
+   (print "if-then-else: non equal"))
 
-; when and unless, the simplification of if
-(when (= (* 2 2) 4) (print "when: equal"))
-(unless (= (* 2 2) 6) (print "unless: not equal"))
+; when and unless, the simplification of `if` without `begin`
+(when (= (* 2 2) 4)
+   (print "when: ..just do something..")
+   (print "when: equal"))
+
+(unless (= (* 2 2) 6)
+   (print "unless: ..just do something..")
+   (print "unless: not equal"))
+
+; if-then-else, extended conditional `if` primitive.
+(if (= (* 2 2) 4)
+   (print "if-then-else*: equal")
+else
+   (print "if-then-else*: ..just do something..")
+   (print "if-then-else*: non equal"))
+
+(if (= (* 2 2) 4)
+then
+   (print "if-then-else*: ..just do something..")
+   (print "if-then-else*: equal")
+else
+   (print "if-then-else*: ..just do something..")
+   (print "if-then-else*: non equal"))
+
+(if (= (* 2 2) 4) ; same as `when`
+then
+   (print "if-then-else*: ..just do something..")
+   (print "if-then-else*: equal"))
+
 
 ; case, the sequence of comparing values.
 (case (* 2 2)
-   (3
+   (3 ; exact number
       (print "case: 3"))
-   (4
+   (4 ; exact number
       (print "case: 4"))
-   ((5 6 7)
+   ((5 6 7) ; list of numbers
       (print "case: 5 or 6 or 7"))
    (else
       (print "case: i don't know")))
 
-; additionally, case can select vectors with variables filling
-(case (vector 'selector 1 2 3)
+; extended case with usable else
+(case (* 2 2)
+   (3 ; exact number
+      (print "case: 3"))
+   (else => (lambda (num)
+      (print "case: real value is " num))))
+
+(case (* 2 2)
+   (3 ; exact number
+      (print "case: 3"))
+   (else is num
+      (print "case: real value is " num)))
+
+; extended case with vectors
+(case ['selector 1 2 3]
    (['case1 x y]
       (print "case: case1 " x ", " y))
    (['selector x y z]
