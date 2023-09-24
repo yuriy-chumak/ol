040e568bf58085ea6f0c08a2d6b717bb3b5faa6d
Yuriy Chumak <yuriy.chumak@mail.com>
2023-09-24 18:21:20 +0300

    opengl 1 libs update

--- libraries/lib/gl-1.scm
+++ libraries/lib/gl-1.scm
@@ -1,7 +1,7 @@
 (define-library (lib gl-1)
 (import
    (scheme core)
-   (lib gl) (OpenGL version-1-4))
+   (lib gl) (OpenGL 1.4))
 (export
    (exports (lib gl))
-   (exports (OpenGL version-1-4))))
+   (exports (OpenGL 1.4))))
