302c989c5a7ff0403ed7c3fc2c6bd485f074955f
Yuriy Chumak <yuriy.chumak@mail.com>
2023-11-20 22:33:42 +0200

    lib gtk-3 updated

---
+++ libraries/lib/gtk-3/combo-box.scm
@@ -0,0 +1,26 @@
+(define-library (lib gtk-3 combo-box)
+   (export
+      GtkComboBox*
+      GTK_TYPE_COMBO_BOX
+      gtk_combo_box_get_type
+
+      gtk_combo_box_get_active_id
+      gtk_combo_box_set_active_id
+   )
+   (import
+      (scheme core)
+      (otus ffi)
+      (lib gtk-3 gtk))
+
+(begin
+   (define GtkComboBox* type-vptr)
+   (define gtk_combo_box_get_type (GTK3 GType "gtk_combo_box_get_type"))
+   (define GTK_TYPE_COMBO_BOX (gtk_combo_box_get_type))
+
+   (define gtk_combo_box_get_active_id (GTK3 type-string "gtk_combo_box_get_active_id" GtkComboBox*))
+   (define gtk_combo_box_set_active_id (GTK3 gboolean "gtk_combo_box_set_active_id" GtkComboBox* type-string))
+
+   (define (GtkComboBox props)
+      ;...
+      #false)
+))
