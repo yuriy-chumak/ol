3d2994326dc3001c4ae5c1372837ddbee93ab358
Yuriy Chumak <yuriy.chumak@mail.com>
2023-12-21 14:30:09 +0200

    (system) now supports regular files, not only pipes

--- libraries/lib/system.scm
+++ libraries/lib/system.scm
@@ -13,6 +13,8 @@
       (() (syscall 22))
       ((flags) (syscall 22 flags))))
 
+   (define pipe? pair?)
+
    (define (close-pipe pp)
       (if (car pp) (close-port (car pp)))
       (if (cdr pp) (close-port (cdr pp))))
@@ -22,27 +24,32 @@
          (define args (map c-string commands))
          (define Pid (syscall 59 (car args) args ports))
 
-         (when in
+         (when (pipe? in) ; pipe?
             (close-port (car in))
             (set-car! in #false))
-         (when out
+         (when (pipe? out)
             (close-port (cdr out))
             (set-cdr! out #false))
-         (when err
+         (when (pipe? err)
             (close-port (cdr err))
             (set-cdr! err #false))
          ; return pid if ok
          Pid)
+      (define (->port in)
+         (cond
+            ((pipe? in) (car in))
+            ((port? in) in)
+            (else #false)))
       ; main
       (case-lambda
          ((commands)
             (system commands #f #f #f #null))
          ((commands in)
-            (system commands in #f #f (list (if in (car in)))))
+            (system commands in #f #f (list (->port in))))
          ((commands in out)
-            (system commands in out #f (list (if in (car in)) (if out (cdr out)))))
+            (system commands in out #f (list (->port in) (->port out))))
          ((commands in out err)
-            (system commands in out err (list (if in (car in)) (if out (cdr out)) (if err (cdr err)))))))
+            (system commands in out err (list (->port in) (->port out) (->port err))))))
 
    (define (waitpid pid)
       (syscall 61 pid))
