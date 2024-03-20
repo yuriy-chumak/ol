285849526b8470acccda4b512804f0d0633dfa93
Yuriy Chumak <yuriy.chumak@mail.com>
2024-03-20 11:37:08 +0200

    gtk glarea update

--- libraries/lib/gtk-3/glarea.scm
+++ libraries/lib/gtk-3/glarea.scm
@@ -12,10 +12,13 @@
       gtk_gl_area_set_required_version
 
       gtk_gl_area_get_error
+
+      ; lisp interface
+      GtkGLArea
    )
    (import
       (scheme core)
-      (otus ffi)
+      (otus ffi) (owl ff)
       (lib gdk-3)
       (lib gtk-3 gtk)
       (lib gtk-3 widget))
@@ -46,7 +49,58 @@
    ; gtk_gl_area_set_error
    (define gtk_gl_area_get_error (GTK3 GError* "gtk_gl_area_get_error" GtkGLArea*))
 
-   (define (GtkGlArea props)
-      ;...
-      #false)
+   ; lisp interface
+   (define GtkGLArea
+      (define (make ptr options)
+         (define base (GtkWidget ptr))
+         (define this (ff-replace base {
+
+            'queue-render (lambda ()
+               (gtk_gl_area_queue_render ptr))
+            'get-context (lambda ()
+               (gtk_gl_area_get_context ptr))
+            'make-current (lambda ()
+               (gtk_gl_area_make_current ptr))
+            'get-error (lambda ()
+               (gtk_gl_area_get_error ptr))
+
+            'set-realize-handler (GtkEventHandler "realize" (widget userdata)
+                     (make widget #empty))
+            'set-render-handler (GtkEventHandler "render" (widget context userdata)
+                     (make widget #empty))
+
+            ; internal
+            'super base
+            'setup (lambda (this options)
+               ((base 'setup) this options)
+
+               (if (options 'on-realize #f)
+                  ((this 'set-realize-handler) (options 'on-realize)))
+               (if (options 'on-render #f)
+                  ((this 'set-render-handler) (options 'on-render)))
+               #true)
+         }))
+
+         ; apply options
+         (when (ff? options)
+            ((this 'setup) this options))
+
+         ; smart object
+         (GObject this))
+
+   ; main
+   (case-lambda
+      ((a1) (cond
+               ((eq? (type a1) type-vptr)
+                  (make a1 #e))
+               ((GObject? a1)
+                  (make (gtk_gl_area_new) #e))
+               (else
+                  (runtime-error "GtkGLArea: invalid argument" a1)) ))
+      ((a1 op) (cond
+               ((and (eq? (type a1) type-vptr) (ff? op))
+                  (make a1 op))
+               (else
+                  (runtime-error "GtkGLArea: invalid arguments" (cons a1 op))) ))
+   ))
 ))
