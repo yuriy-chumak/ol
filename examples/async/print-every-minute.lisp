e469e0930c7c6d45d5b5325f692d9962a6305a36
Yuriy Chumak <yuriy.chumak@mail.com>
2024-10-11 16:44:19 +0300

    "timeout example" updated

--- examples/async/print-every-minute.lisp
+++ examples/async/print-every-minute.lisp
@@ -1,15 +1,10 @@
 #!/usr/bin/env ol
 
-(async (lambda ()
-   (let*((ss ms (clock)))
-      (print "async started at " ss " epoch seconds")
-      (let loop ((ss ss) (ms ms))
-         (let*((ss2 ms2 (clock)))
-            (if (>= (- ss2 ss) 60) ; 1 minute
-            then
-               (print "tick! " ss2 " epoch seconds")
-               (loop ss2 ms2)
-            else
-               (sleep 10)
-               (loop ss ms)))))))
+(import (only (otus syscall) strftime))
 
+(async (lambda ()
+   (print "async started at " (strftime "%c"))
+   (let loop ()
+      (wait 60000)
+      (print "tick! " (strftime "%c"))
+      (loop))))
