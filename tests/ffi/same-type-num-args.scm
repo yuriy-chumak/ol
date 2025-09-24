8032f7954977c84cad1b4494a23a3f2fc25e5d1a
Yuriy Chumak <yuriy.chumak@mail.com>
2025-09-24 18:57:45 +0300

    long long added to ffi tests

--- tests/ffi/same-type-num-args.scm
+++ tests/ffi/same-type-num-args.scm
@@ -18,8 +18,8 @@ void v_cc..c(n)(type a1, type a2, .., type aN)
    printf('{{ %u %u .. %u(n) }}', a1, a2, .., aN); fflush(stdout);
 }, n = (1 .. " MAX-ARGS-COUNT ")")
 
-(let ((N MAX-ARGS-COUNT))
 (for-each (lambda (index typename Sn)
+      (define N (if (m/L|l/ index) (/ MAX-ARGS-COUNT 2) MAX-ARGS-COUNT))
       (for-each (lambda (n)
             (define name (|v_cc..c(n)| index n))
             (define rtty (repeat typename n)) ; args types
@@ -31,41 +31,41 @@ void v_cc..c(n)(type a1, type a2, .., type aN)
 
          (iota N 1)))
    ;      unsigned types      signed types                                 floating points
-   (list "C"    "S"    "I"    "c"    "c"     "s"    "s"     "i"    "i"     "f"    "f"     "d"    "d"    )
-   (list uchar  ushort uint   char   char    short  short   int    int     float  float   double double )
-   (list iota24 primes primes iota24 -iota24 primes -primes primes -primes primes -primes primes -primes)
-))
+   (list "C"    "S"    "I"    "L"    "c"    "c"     "s"    "s"     "i"    "i"     "l"    "l"     "f"    "f"     "d"    "d"    )
+   (list uchar  ushort uint   ullong char   char    short  short   int    int     llong  llong   float  float   double double )
+   (list iota24 primes primes primes iota24 -iota24 primes -primes primes -primes primes -primes primes -primes primes -primes)
+)
 
-(let ((N MAX-ARGS-COUNT))
-(for-each (lambda (name n)
-   (print name)
-   (for-each (lambda (index typename)
-         (define name (|v_cc..c(n)| index N))
-         (define rtty (repeat typename N)) ; args types
-         (define function (apply this (cons* void name rtty)))
+(for-each (lambda (comment n)
+      (print comment)
+      (for-each (lambda (index typename)
+            (define N (if (m/L|l/ index) (/ MAX-ARGS-COUNT 2) MAX-ARGS-COUNT))
+            (define name (|v_cc..c(n)| index N))
+            (define rtty (repeat typename N)) ; args types
+            (define function (apply this (cons* void name rtty)))
 
-         (define args (repeat n N))
-         (try name function args))
-      ;      unsigned types                                        signed types                                                                                     floating points
-      (list "C"            "S"               ); "I"               "c"             "c"             "s"              "s"              "i"            "i"             "f"       "f"       "d"        "d"       )
-      (list uchar          ushort            ) ));  uint  char char fft-signed-short fft-signed-short fft-signed-int fft-signed-int  fft-float fft-float fft-double fft-double)
+            (define args (repeat n N))
+            (try name function args))
+         ;      unsigned types   signed types   floating points
+         (list "C"   "S"    "I"  "L"    "c"  "s"   "i" "l"   "f"   "d"   )
+         (list uchar ushort uint ullong char short int llong float double) ))
    '("zeroes:" "defaults (#f):")
    '(0          #false)
-))
+)
 
 (print "limits:")
-(let ((N MAX-ARGS-COUNT))
 (for-each (lambda (index typename n)
+      (define N (if (m/L|l/ index) (/ MAX-ARGS-COUNT 2) MAX-ARGS-COUNT))
       (define name (|v_cc..c(n)| index N))
       (define rtty (repeat typename N)) ; args types
       (define function (apply this (cons* void name rtty)))
 
       (try name function (repeat n N)))
    ;      unsigned types                          signed types
-   (list "C"       "S"        "I"        "c"      "c"      "s"       "s"       "i"       "i"      )
-   (list uchar     ushort     uint       char     char     short     short     int       int      )
-   (list UINT8_MAX UINT16_MAX UINT32_MAX INT8_MIN INT8_MAX INT16_MIN INT16_MAX INT32_MIN INT32_MAX)
-))
+   (list "C"       "S"        "I"        "L"        "c"      "c"      "s"       "s"       "i"       "i"       "l"       "l"      )
+   (list uchar     ushort     uint       ullong     char     char     short     short     int       int       llong     llong    )
+   (list UINT8_MAX UINT16_MAX UINT32_MAX UINT64_MAX INT8_MIN INT8_MAX INT16_MIN INT16_MAX INT32_MIN INT32_MAX INT64_MIN INT64_MAX)
+)
 
 ;; ; special case:
 ;; "L"          
