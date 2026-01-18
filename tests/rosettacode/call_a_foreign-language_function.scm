a7d37325e26ec1b8cdd493090479b34bf0f72d61
Yuriy Chumak <yuriy.chumak@mail.com>
2026-01-19 01:29:41 +0200

    rosettacode examples updated

--- tests/rosettacode/call_a_foreign-language_function.scm
+++ tests/rosettacode/call_a_foreign-language_function.scm
@@ -2,7 +2,34 @@
 
 (import (otus ffi))
 
-(define self (load-dynamic-library (if (has? *features* 'Windows) "shlwapi.dll" #f)))
-(define strdup (self type-string (if (has? *features* 'Windows) "StrDupA" "strdup") type-string))
+(define libc (or
+   (load-dynamic-library "libc.so") ; General Posix
+   (load-dynamic-library "libc.so.6") ; Linux
+   (load-dynamic-library "libc.so.7") ; Latest *BSD
+   (load-dynamic-library "libSystem.B.dylib") ; Mac
+   (load-dynamic-library "shlwapi.dll") )) ; Windows
+
+(define strdup (or
+   (libc type-string "strdup" type-string) ; Posix
+   (libc type-string "StrDupA" type-string) )) ; Win
+
+(print (strdup "Hello World!"))
+
+; strdup with free
+(define lib2
+   (load-dynamic-library "kernel32.dll")) ; Windows
+
+(define strdup 
+   (let ((strdup (or
+            (libc type-vptr "strdup" type-string) ; Posix
+            (libc type-vptr "StrDupA" type-string))) ; Win
+         (free (or 
+            (libc fft-void "free" type-vptr) ; Posix
+            (lib2 fft-void "LocalFree" type-vptr)))) ; Win
+      (lambda (str)
+         (let*((dupped (strdup str))
+               (result (vptr->string dupped)))
+            (free dupped)
+            result))))
 
 (print (strdup "Hello World!"))
--- tests/rosettacode/call_a_function_in_a_shared_library.scm
+++ tests/rosettacode/call_a_function_in_a_shared_library.scm
@@ -3,22 +3,19 @@
 (import (otus ffi))
 
 (define self (load-dynamic-library #f))
+
 (define strdup
    (let ((strdup (self type-vptr "strdup" type-string))
-         (free   (self fft-void "free" type-vptr)))
+         (free (self fft-void "free" type-vptr)))
       (if (and strdup free)
+         ; we have both native functions
          (lambda (str)
             (let*((dupped (strdup str))
                   (result (vptr->string dupped)))
                (free dupped)
                result))
+         ; windows doesn't have strdup and free, so...
          (lambda (str)
             (list->string (string->list str))))))
 
-(define strduX (or
-   (self type-string "strduX" type-string)
-   (lambda (str)
-      (list->string (string->list str)))))
-
 (print (strdup "Hello World!"))
-(print (strduX "Hello World!"))
