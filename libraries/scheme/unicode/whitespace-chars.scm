233190bcc46f13bdd6e768a95a56a3a6336ba5df
Yuriy Chumak <yuriy.chumak@mail.com>
2026-01-23 03:27:59 +0200

    Unicode support updated to 17.0 version

--- libraries/scheme/unicode/whitespace-chars.scm
+++ libraries/scheme/unicode/whitespace-chars.scm
@@ -1,10 +1,10 @@
 (define whitespace-chars-alist '(
 ; White_Space
-#x9 #xA #xB #xC #xD #x0020
+#x9 #xa #xb #xc #xd #x0020
 #x0085
 #x00A0
 #x1680
-#x2000 #x2001 #x2002 #x2003 #x2004 #x2005 #x2006 #x2007 #x2008 #x2009 #x200A #x2028
+#x2000 #x2001 #x2002 #x2003 #x2004 #x2005 #x2006 #x2007 #x2008 #x2009 #x200a #x2028
 #x2029
 #x202F
 #x205F
