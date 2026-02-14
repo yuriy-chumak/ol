8cefbc5b45f7d0ef43e5592efaa89ede63c2bc9d
Yuriy Chumak <yuriy.chumak@mail.com>
2026-02-14 19:37:53 +0200

    webkit2gtk4 lib updated

--- libraries/lib/webkit2gtk-4.scm
+++ libraries/lib/webkit2gtk-4.scm
@@ -2,12 +2,14 @@
    (export
 
       webkit_web_view_new
+      webkit_web_view_new_with_context
       webkit_web_view_load_uri
       webkit_web_view_load_html
 
       webkit_web_view_evaluate_javascript
       webkit_web_view_get_user_content_manager
       webkit_user_content_manager_register_script_message_handler
+      webkit_web_view_stop_loading
 
       ; javascript support
       webkit_javascript_result_get_global_context
@@ -17,6 +19,14 @@
       JSStringGetMaximumUTF8CStringSize
       JSStringGetUTF8CString
       JSStringRelease
+
+      ; context
+      WebKitWebContext*
+      webkit_web_context_get_default
+      webkit_web_context_register_uri_scheme
+
+      ; WebKitURIRequest
+      webkit_uri_request_get_uri
    )
 
    (import
@@ -28,6 +38,7 @@
 (begin
    (define WEBKIT2 (or
       (load-dynamic-library "libwebkit2gtk-4.0.so")
+      (load-dynamic-library "libwebkit2gtk-4.1.so")
       (load-dynamic-library "libwebkit2gtk-4.0.so.37")))
 
    (define WebKitWebView* type-vptr)
@@ -36,6 +47,7 @@
    (define GAsyncReadyCallback GtkCallback)
 
    (define webkit_web_view_new (WEBKIT2 GtkWidget* "webkit_web_view_new"))
+   (define webkit_web_view_new_with_context (WEBKIT2 GtkWidget* "webkit_web_view_new_with_context" type-vptr))
    (define webkit_web_view_load_uri (WEBKIT2 fft-void "webkit_web_view_load_uri" WebKitWebView* type-string))
    (define webkit_web_view_load_html (WEBKIT2 fft-void "webkit_web_view_load_html" WebKitWebView* type-string type-string))
    ; webkit_web_view_is_loading
@@ -55,6 +67,7 @@
    (define WebKitUserContentManager* type-vptr)
    (define webkit_web_view_get_user_content_manager (WEBKIT2 WebKitUserContentManager* "webkit_web_view_get_user_content_manager" WebKitWebView*))
    (define webkit_user_content_manager_register_script_message_handler (WEBKIT2 gboolean "webkit_user_content_manager_register_script_message_handler" WebKitUserContentManager* type-string))
+   (define webkit_web_view_stop_loading (WEBKIT2 void "webkit_web_view_stop_loading" WebKitWebView*))
 
    ; javascript
    (define JSValueRef type-vptr)
@@ -73,5 +86,13 @@
    (define JSStringGetUTF8CString (WEBKIT2 fft-size_t "JSStringGetUTF8CString" JSStringRef fft-void* fft-size_t))
    (define JSStringRelease (WEBKIT2 fft-void "JSStringRelease" JSStringRef))
 
+   ; context
+   (define WebKitWebContext* type-vptr)
+   (define WebKitURISchemeRequestCallback* type-callable)
+   (define webkit_web_context_get_default (WEBKIT2 WebKitWebContext* "webkit_web_context_get_default"))
+   (define webkit_web_context_register_uri_scheme (WEBKIT2 fft-void "webkit_web_context_register_uri_scheme" WebKitWebContext* gchar* WebKitURISchemeRequestCallback* gpointer gpointer))
 
+   ; WebKitURIRequest
+   (define WebKitURIRequest* type-vptr)
+   (define webkit_uri_request_get_uri (WEBKIT2 gchar* "webkit_uri_request_get_uri" WebKitURIRequest*))
 ))
