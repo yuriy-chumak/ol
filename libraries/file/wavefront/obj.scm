05b95a64ac0d9bc16b5b98c373a1bc0d3c675383
Yuriy Chumak <yuriy.chumak@mail.com>
2026-04-08 11:38:56 +0300

    wavefront obj parser moved to new data model

--- libraries/file/wavefront/obj.scm
+++ libraries/file/wavefront/obj.scm
@@ -1,157 +1,182 @@
 (define-library (file wavefront obj)
 
 (export
-   wavefront-obj-parser)
+   wavefront-obj-parser
+   read-wavefront-obj
+   read-wavefront-obj-file
+
+   read-wavefront-obj-port
+   read-wavefront-obj-stream
+)
 
 (import
    (otus lisp)
-   (file parser)
-   (data s-exp))
+   (data parse)
+   (data s-exp)
+   (only (scheme file) call-with-input-file))
 (begin
 
-(define get-rest-of-line
-   (let-parses
-      ((chars (get-greedy* (get-byte-if (lambda (x) (not (eq? x 10))))))
-         (skip (get-imm 10))) ;; <- note that this won't match if line ends to eof
-      chars))
+   (define get-rest-of-line
+      (let-parse* (
+            (chars (greedy* (byte (lambda (x) (not (eq? x 10))))))
+            ( -- (byte 10))) ;; <- note that this won't match if line ends to eof
+         chars))
 
-(define get-inexact
-   (let-parse* (
-         (number get-number))
-      (inexact number)))
+   (define get-inexact
+      (let-parse* (
+            (number get-number))
+         (inexact number)))
 
-(define get-integer
-   (let-parse* (
-         (numbers (get-greedy+ (get-byte-if (lambda (x) (<= #\0 x #\9))))))
-      (list->number numbers 10)))
+   (define get-integer
+      (let-parse* (
+            (numbers (greedy+ (byte (lambda (x) (<= #\0 x #\9))))))
+         (list->number numbers 10)))
 
-(define get-comment
-   (let-parses(
-         (sign (get-imm #\#))
-         (comment get-rest-of-line))
-      #true))
-(define get-mtllib
-   (either
-      (let-parses(
-            (skip (get-word "mtllib " #t))
+   (define get-comment
+      (let-parse* (
+            (sign (byte #\#))
+            (comment get-rest-of-line))
+         #true))
+   (define get-mtllib
+      (either
+         (let-parse* (
+               ( -- (bytes "mtllib "))
+               (name get-rest-of-line))
+            (bytes->string name))
+         (epsilon #false)))
+   (define get-usemtl
+      (either
+         (let-parse* (
+               ( -- (bytes "usemtl "))
+               (name get-rest-of-line))
+            (bytes->string name))
+         (epsilon #false)))
+
+   (define get-g
+      (let-parse* (
+            ( -- (bytes "g "))
             (name get-rest-of-line))
-         (bytes->string name))
-      (epsilon #false)))
-(define get-usemtl
-   (either
-      (let-parses(
-            (skip (get-word "usemtl " #t))
+         name))
+   (define get-o
+      (let-parse* (
+            ( -- (bytes "o "))
             (name get-rest-of-line))
-         (bytes->string name))
-      (epsilon #false)))
+         name))
+   (define get-s
+      (let-parse* (
+            ( -- (bytes "s "))
+            (group get-rest-of-line))
+         group))
+   (define get-l
+      (let-parse* (
+            ( -- (bytes "l "))
+            (group get-rest-of-line))
+         group))
+   (define get-v
+      (let-parse* (
+            ( -- (bytes "v "))
+            (x get-inexact)
+            ( -- (byte #\space))
+            (y get-inexact)
+            ( -- (byte #\space))
+            (z get-inexact)
+            ( -- (byte #\newline)))
+         [x y z]))
+   (define get-vt
+      (let-parse* (
+            ( -- (bytes "vt "))
+            (x get-inexact)
+            ( -- (byte #\space))
+            (y get-inexact)
+            ( -- (byte #\newline)))
+         [x y]))
+   (define get-vn
+      (let-parse* (
+            ( -- (bytes "vn "))
+            (x get-inexact)
+            ( -- (byte #\space))
+            (y get-inexact)
+            ( -- (byte #\space))
+            (z get-inexact)
+            ( -- (byte #\newline)))
+         [x y z]))
+   (define get-v-vt-vn
+      (let-parse* (
+            (a (either get-integer (epsilon #f)))
+            ( -- (either (byte #\/) (epsilon #false)))
+            (b (either get-integer (epsilon #f)))
+            ( -- (either (byte #\/) (epsilon #false)))
+            (c (either get-integer (epsilon #f))) )
+         [a b c]))
 
-(define get-g
-   (let-parses(
-         (skip (get-word "g " #t))
-         (name get-rest-of-line))
-      name))
-(define get-o
-   (let-parses(
-         (skip (get-word "o " #t))
-         (name get-rest-of-line))
-      name))
-(define get-s
-   (let-parses(
-         (skip (get-word "s " #t))
-         (group get-rest-of-line))
-      group))
-(define get-l
-   (let-parses(
-         (skip (get-word "l " #t))
-         (group get-rest-of-line))
-      group))
-(define get-v
-   (let-parses(
-         (skip (get-word "v " #t))
-         (x get-inexact)
-         (skip (get-imm #\space))
-         (y get-inexact)
-         (skip (get-imm #\space))
-         (z get-inexact)
-         (skip (get-imm #\newline)))
-      [x y z]))
-(define get-vt
-   (let-parses(
-         (skip (get-word "vt " #t))
-         (x get-inexact)
-         (skip (get-imm #\space))
-         (y get-inexact)
-         (skip (get-imm #\newline)))
-      [x y]))
-(define get-vn
-   (let-parses(
-         (skip (get-word "vn " #t))
-         (x get-inexact)
-         (skip (get-imm #\space))
-         (y get-inexact)
-         (skip (get-imm #\space))
-         (z get-inexact)
-         (skip (get-imm #\newline)))
-      [x y z]))
-(define get-v-vt-vn
-   (let-parses(
-         (a (either get-integer (epsilon #f)))
-         (skip (either (get-imm #\/) (epsilon #false)))
-         (b (either get-integer (epsilon #f)))
-         (skip (either (get-imm #\/) (epsilon #false)))
-         (c (either get-integer (epsilon #f))) )
-      [a b c]))
+   (define get-f
+      (let-parse* (
+            ( -- (bytes "f "))
+            (a get-v-vt-vn)
+            ( -- (byte #\space))
+            (b get-v-vt-vn)
+            ( -- (byte #\space))
+            (c get-v-vt-vn)
+            ( -- (byte #\newline)))
+         [a b c]))
 
-(define get-f
-   (let-parses(
-         (skip (get-word "f " #t))
-         (a get-v-vt-vn)
-         (skip (get-imm #\space))
-         (b get-v-vt-vn)
-         (skip (get-imm #\space))
-         (c get-v-vt-vn)
-         (skip (get-imm #\newline)))
-      [a b c]))
+   (define facegroup-parser
+      (let-parse* (
+            (usemtl get-usemtl)
+            ( -- (greedy* get-s))
+            (faces (greedy+ get-f))
+            ( -- (greedy* get-l)))
+         (cons
+            usemtl
+            faces)))
 
-(define facegroup-parser
-   (let-parses (
-         (usemtl get-usemtl)
-         (skip (get-greedy* get-s))
-         (faces (get-greedy+ get-f))
-         (skip (get-greedy* get-l)))
-      (cons
-         usemtl
-         faces)))
+   ; main
+   (define wavefront-obj-parser
+      (let-parse* (
+            (comments (greedy* get-comment))
+            (mtllib get-mtllib)
+            (objects (greedy+ (let-parse* (
+                  (name (either get-g get-o))
+                  (v (greedy+ get-v))
+                  (vt (greedy* get-vt))
+                  (vn (greedy* get-vn))
+                  (facegroups (greedy+ facegroup-parser)) )
+               {
+                  'name  (bytes->string name)
+                  'v  v
+                  'vt vt
+                  'vn vn
+                  'facegroups facegroups
+               }))))
+         {
+            'mtllib  mtllib
+            'v  (foldr append '() (map (lambda (o) (o 'v  '())) objects))
+            'vt (foldr append '() (map (lambda (o) (o 'vt '())) objects))
+            'vn (foldr append '() (map (lambda (o) (o 'vn '())) objects))
+            'o (map (lambda (o) {
+                     'name (o 'name)
+                     'facegroups (o 'facegroups)
+                     })
+                  objects)
+         }))
 
-; main
-(define wavefront-obj-parser
-   (let-parses (
-         (comments (get-greedy* get-comment))
-         (mtllib get-mtllib)
-         (objects (greedy+ (let-parses (
-               (name (get-either get-g get-o))
-               (v (greedy+ get-v))
-               (vt (get-greedy* get-vt))
-               (vn (get-greedy* get-vn))
-               (facegroups (get-greedy+ facegroup-parser))
-               )
-            {
-               'name  (bytes->string name)
-               'v  v
-               'vt vt
-               'vn vn
-               'facegroups facegroups
-            }))))
-      {
-         'mtllib  mtllib
-         'v  (foldr append '() (map (lambda (o) (o 'v  '())) objects))
-         'vt (foldr append '() (map (lambda (o) (o 'vt '())) objects))
-         'vn (foldr append '() (map (lambda (o) (o 'vn '())) objects))
-         'o (map (lambda (o) {
-                  'name (o 'name)
-                  'facegroups (o 'facegroups)
-                  })
-               objects)
-      }))
+
+   (define (read-wavefront-obj-stream stream)
+      (when stream
+         (parse wavefront-obj-parser stream)))
+
+   (define (read-wavefront-obj-port port)
+      (when port
+         (read-wavefront-obj-stream (port->bytestream port))))
+
+   (define read-wavefront-obj (case-lambda
+      (() (read-wavefront-obj-port stdin))
+      ((source) (cond
+         ((port? source) (read-wavefront-obj-port source))
+         ((pair? source) (read-wavefront-obj-stream source))))))
+
+   (define (read-wavefront-obj-file filename)
+      (call-with-input-file filename (lambda (port)
+         (read-wavefront-obj-port port))))
 
 ))
